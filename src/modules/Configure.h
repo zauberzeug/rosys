393d1d2cb9ec6b1a76a0e8e39817f749915e3dfc
Falko Schindler <mail@falkoschindler.de>
2020-07-21 15:27:39 +0200

    instantiate before applying settings

--- src/modules/Configure.h
+++ src/modules/Configure.h
@@ -19,39 +19,46 @@ public:
     {
         printf("Reading configuration from persistent storage...\n");
 
-        nvs_iterator_t it = nvs_entry_find("nvs", NULL, NVS_TYPE_ANY);
+        nvs_iterator_t it;
         nvs_entry_info_t info;
+
+        it = nvs_entry_find("nvs", NULL, NVS_TYPE_ANY);
         while (it != NULL)
         {
             nvs_entry_info(it, &info);
             it = nvs_entry_next(it);
 
             std::string namespace_ = std::string(info.namespace_name);
+            if (namespace_ != "configure")
+                continue;
+
             std::string key = std::string(info.key);
             std::string value = storage::get(namespace_, key);
-            if (namespace_ == "configure")
-            {
-                printf("+ Module %s: %s\n", key.c_str(), value.c_str());
-                std::string type = cut_first_word(value, ':');
-                if (type == "led")
-                {
-                    modules[key] = new Led(new Port((gpio_num_t)atoi(value.c_str())));
-                }
-                else if (type == "button")
-                {
-                    modules[key] = new Button(new Port((gpio_num_t)atoi(value.c_str())));
-                }
-                else
-                {
-                    printf("Unknown module type: %s\n", type.c_str());
-                    continue;
-                }
-            }
+            printf("+ Module %s: %s\n", key.c_str(), value.c_str());
+
+            std::string type = cut_first_word(value, ':');
+            if (type == "led")
+                modules[key] = new Led(new Port((gpio_num_t)atoi(value.c_str())));
+            else if (type == "button")
+                modules[key] = new Button(new Port((gpio_num_t)atoi(value.c_str())));
             else
-            {
-                printf("+ Setting %s.%s=%s\n", namespace_.c_str(), key.c_str(), value.c_str());
-                modules[namespace_]->handleMsg("set " + key + "=" + value);
-            }
+                printf("Unknown module type: %s\n", type.c_str());
+        };
+
+        it = nvs_entry_find("nvs", NULL, NVS_TYPE_ANY);
+        while (it != NULL)
+        {
+            nvs_entry_info(it, &info);
+            it = nvs_entry_next(it);
+
+            std::string namespace_ = std::string(info.namespace_name);
+            if (namespace_ == "configure")
+                continue;
+
+            std::string key = std::string(info.key);
+            std::string value = storage::get(namespace_, key);
+            printf("+ Setting %s.%s=%s\n", namespace_.c_str(), key.c_str(), value.c_str());
+            modules[namespace_]->handleMsg("set " + key + "=" + value);
         };
     }
 
