890c4af4947dd40b236f65a5d03d539095d34222
Falko Schindler <mail@falkoschindler.de>
2020-07-23 21:07:34 +0200

    more robust storage implementation inspired from m5camera code

--- src/storage.h
+++ src/storage.h
@@ -5,6 +5,8 @@
 
 namespace storage
 {
+    std::map<std::string, std::string> dict;
+
     void init()
     {
         nvs_flash_init();
@@ -13,24 +15,61 @@ namespace storage
     void put(std::string namespace_, std::string key, std::string value)
     {
         nvs_handle handle;
-        nvs_open(namespace_.c_str(), NVS_READWRITE, &handle);
-        nvs_set_str(handle, key.c_str(), value.c_str());
-        nvs_commit(handle);
+        if (nvs_open(namespace_.c_str(), NVS_READWRITE, &handle) != ESP_OK)
+            return;
+
+        if (nvs_set_str(handle, key.c_str(), value.c_str()) != ESP_OK)
+        {
+            nvs_close(handle);
+            return;
+        }
+
+        if (nvs_commit(handle) != ESP_OK)
+        {
+            nvs_close(handle);
+            return;
+        }
+
+        dict[key] = std::string(value);
         nvs_close(handle);
     }
 
     std::string get(std::string namespace_, std::string key)
     {
+        if (dict.count(key) == 1)
+            return dict[key];
+
         nvs_handle handle;
-        nvs_open(namespace_.c_str(), NVS_READWRITE, &handle);
+        if (nvs_open(namespace_.c_str(), NVS_READWRITE, &handle) != ESP_OK)
+        {
+            return "";
+        }
+
         size_t size = 0;
-        nvs_get_str(handle, key.c_str(), NULL, &size);
-        char *buffer = (char *)malloc(size);
-        nvs_get_str(handle, key.c_str(), buffer, &size);
-        std::string value = std::string(buffer);
-        free(buffer);
+        if (nvs_get_str(handle, key.c_str(), NULL, &size) != ESP_OK)
+        {
+            nvs_close(handle);
+            return "";
+        }
+
+        char *value = (char *)malloc(size);
+        if (size > 0)
+        {
+            if (nvs_get_str(handle, key.c_str(), value, &size) != ESP_OK)
+            {
+                free(value);
+                nvs_close(handle);
+                return "";
+            }
+        }
+
+        std::string result = std::string(value);
+        free(value);
+
+        dict[key] = result;
         nvs_close(handle);
-        return value;
+
+        return result;
     }
 
     void erase()
