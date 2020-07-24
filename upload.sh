79825dd0eb6d767998f3871e00a93db8dce432e5
zauberzeug@j24 <development@perpetual-mobile.de>
2020-07-24 14:10:57 +0200

    adjustments for jetson

--- upload.sh
+++ upload.sh
@@ -5,7 +5,26 @@ then
     export IDF_TOOLS_PATH="$HOME/esp/esp-tools_4.0.1"
     export IDF_PATH="$HOME/esp/esp-idf_4.0.1"
     . $IDF_PATH/export.sh
-    make -j4 flash
+    
+    if lshw -C system 2>/dev/null | grep -q "NVIDIA Jetson"
+    then
+        en=216
+        g0=50
+        sudo echo $en > /sys/class/gpio/export
+        sudo echo $g0 > /sys/class/gpio/export
+        sudo echo out > /sys/class/gpio/gpio$en/direction
+        sudo echo out > /sys/class/gpio/gpio$g0/direction
+        sudo echo 1 > /sys/class/gpio/gpio$en/value
+        sudo echo 1 > /sys/class/gpio/gpio$g0/value
+        sudo echo 0 > /sys/class/gpio/gpio$en/value
+        make -j4 flash
+        sudo echo 1 > /sys/class/gpio/gpio$en/value
+        sudo echo 0 > /sys/class/gpio/gpio$en/value
+        sudo echo $en > /sys/class/gpio/unexport
+        sudo echo $g0 > /sys/class/gpio/unexport
+    else
+        make -j4 flash
+    fi
 else
     $HOME/.platformio/penv/bin/pio run --target upload
 fi
