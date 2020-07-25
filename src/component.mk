6dd92b65f5a5a5d446fa0a9aeea276e3cc75908a
jetsonnano@j3 <development@perpetual-mobile.de>
2020-07-25 07:46:31 +0200

    fix component makefile on jetson

--- src/component.mk
+++ src/component.mk
@@ -4,4 +4,4 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 CPPFLAGS += -D_GLIBCXX_USE_C99 # https://github.com/espressif/esp-idf/issues/1445#issuecomment-354456491
-COMPONENT_SRCDIRS := . modules/*
+COMPONENT_SRCDIRS := . modules/bluetooth modules/roboclaw
