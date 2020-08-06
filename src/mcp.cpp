9cd9a61611d69519d7df0f084ff84ab20f15c2f4
zauberzeug@j24 <development@perpetual-mobile.de>
2020-08-06 14:15:31 +0200

    initialize mcp bank A as inputs

--- src/mcp.cpp
+++ src/mcp.cpp
@@ -13,6 +13,8 @@ void mcp::init()
         .port = I2C_NUM_0,
         .sda_pin = 21,
         .scl_pin = 22,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
     };
 
     gpio_reset_pin(GPIO_NUM_14);
@@ -20,6 +22,9 @@ void mcp::init()
     gpio_set_level(GPIO_NUM_14, 1);
 
     ESP_ERROR_CHECK(mcp23017_init(&config));
+
+    mcp23017_write_register(&config, MCP23017_IODIR, GPIOA, 0xFF);
+    mcp23017_write_register(&config, MCP23017_IODIR, GPIOB, 0x00);
 }
 
 void mcp::write_bit(int bank, int number, int level)
