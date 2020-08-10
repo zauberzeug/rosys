58304a7f4caf8fedb09c3f308bb8e037e189e7f3
zauberzeug@j24 <development@perpetual-mobile.de>
2020-08-10 13:27:42 +0200

    split timing in cpp and h

---
+++ src/utils/timing.cpp
@@ -0,0 +1,24 @@
+#include "timing.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void delay(unsigned int duration_ms)
+{
+    vTaskDelay(duration_ms / portTICK_PERIOD_MS);
+}
+
+unsigned long int IRAM_ATTR micros()
+{
+    return (unsigned long)(esp_timer_get_time());
+}
+
+unsigned long int IRAM_ATTR millis()
+{
+    return (unsigned long)(esp_timer_get_time() / 1000ULL);
+}
+
+unsigned long millisSince(unsigned long time)
+{
+    return millis() - time;
+}
--- src/utils/timing.h
+++ src/utils/timing.h
@@ -1,24 +1,11 @@
 #pragma once
 
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
-void delay(unsigned int duration_ms)
-{
-    vTaskDelay(duration_ms / portTICK_PERIOD_MS);
-}
+void delay(unsigned int duration_ms);
 
-unsigned long int IRAM_ATTR micros()
-{
-    return (unsigned long)(esp_timer_get_time());
-}
+unsigned long int IRAM_ATTR micros();
 
-unsigned long int IRAM_ATTR millis()
-{
-    return (unsigned long)(esp_timer_get_time() / 1000ULL);
-}
+unsigned long int IRAM_ATTR millis();
 
-unsigned long millisSince(unsigned long time)
-{
-    return millis() - time;
-}
+unsigned long millisSince(unsigned long time);
