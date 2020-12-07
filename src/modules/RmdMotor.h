8b61a1437abef1551e41440d8708c8696cf70e43
Falko Schindler <mail@falkoschindler.de>
2020-12-07 15:59:08 +0100

    rmd: refactoring, states and output

--- src/modules/RmdMotor.h
+++ src/modules/RmdMotor.h
@@ -11,6 +11,7 @@
 class RmdMotor : public Module
 {
 private:
+    bool output = false;
     double ratio = 1.0;
     double speed = 10.0;
     double minAngle = 0.0;
@@ -20,6 +21,16 @@ private:
 
     Can *can;
 
+    double target = 0;
+
+    enum State
+    {
+        IDLE = 0,
+        MOVE = 1,
+        HOME = 2,
+        STOP = 3,
+    };
+
 public:
     RmdMotor(std::string name, Can *can) : Module(name)
     {
@@ -27,27 +38,25 @@ public:
         this->can->subscribe(this->id, this);
     }
 
+    void loop()
+    {
+        if (output)
+            printf("%s %d\n", this->name.c_str(), this->state);
+    }
+
     void handleMsg(std::string msg)
     {
         std::string command = cut_first_word(msg);
 
-        if (command == "turn")
+        if (command == "move")
         {
-            double requested_angle = atof(cut_first_word(msg, ',').c_str());
-            double clipped_angle = std::max(std::min(requested_angle, maxAngle), minAngle);
-            uint16_t speed = this->speed * this->ratio;
-            uint32_t angle = clipped_angle * this->ratio * 100;
-            uint8_t data[8] = {
-                0xa4,
-                0,
-                *((uint8_t *)(&speed) + 0),
-                *((uint8_t *)(&speed) + 1),
-                *((uint8_t *)(&angle) + 0),
-                *((uint8_t *)(&angle) + 1),
-                *((uint8_t *)(&angle) + 2),
-                *((uint8_t *)(&angle) + 3),
-            };
-            this->can->send(this->id, data);
+            this->target = atof(cut_first_word(msg, ',').c_str());
+            this->move();
+        }
+        else if (command == "home")
+        {
+            this->target = 0;
+            this->move();
         }
         else if (command == "zero")
         {
@@ -58,10 +67,9 @@ public:
         {
             this->stop();
         }
-        else if (command == "off")
+        else if (command == "get")
         {
-            uint8_t data[8] = {0x80, 0, 0, 0, 0, 0, 0, 0};
-            this->can->send(this->id, data);
+            printf("%s get %d\n", this->name.c_str(), this->state);
         }
         else
         {
@@ -76,7 +84,11 @@ public:
 
     void set(std::string key, std::string value)
     {
-        if (key == "ratio")
+        if (key == "output")
+        {
+            output = value == "1";
+        }
+        else if (key == "ratio")
         {
             ratio = atof(value.c_str());
         }
@@ -98,9 +110,29 @@ public:
         }
     }
 
+    void move()
+    {
+        double clipped_angle = std::max(std::min(this->target, maxAngle), minAngle);
+        uint16_t speed = this->speed * this->ratio;
+        uint32_t angle = clipped_angle * this->ratio * 100;
+        uint8_t data[8] = {
+            0xa4,
+            0,
+            *((uint8_t *)(&speed) + 0),
+            *((uint8_t *)(&speed) + 1),
+            *((uint8_t *)(&angle) + 0),
+            *((uint8_t *)(&angle) + 1),
+            *((uint8_t *)(&angle) + 2),
+            *((uint8_t *)(&angle) + 3),
+        };
+        this->can->send(this->id, data);
+        this->state = MOVE;
+    }
+
     void stop()
     {
         uint8_t data[8] = {0x81, 0, 0, 0, 0, 0, 0, 0};
         this->can->send(this->id, data);
+        this->state = STOP;
     }
 };
