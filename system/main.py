9837d8b5ad8a7f3f9f22165a56676310828b59ca
Rodja Trappe <rodja@perpetual-mobile.de>
2021-02-12 13:42:51 +0100

    adding first socketio consuming event

--- system/main.py
+++ system/main.py
@@ -5,16 +5,21 @@ app = FastAPI()
 sio = SocketManager(app=app)
 
 
+@sio.on('drive_power')
+def drive_power(sid, data):
+    print(f'{sid} sends {data}', flush=True)
+
+
 @app.on_event("startup")
 async def startup():
 
-    print("---- startup")
+    print("---- startup", flush=True)
 
 
 @app.on_event("shutdown")
 async def shutdown():
 
-    print("---- stop")
+    print("---- stop", flush=True)
 
 
 @app.get("/api")
