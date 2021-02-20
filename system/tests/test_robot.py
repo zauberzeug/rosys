e23b2a90d58aa243dc4a137f51d71b2f15056033
Rodja Trappe <rodja@perpetual-mobile.de>
2021-02-20 09:04:03 +0100

    system: fixed white spaces

--- system/tests/test_robot.py
+++ system/tests/test_robot.py
@@ -12,12 +12,12 @@ async def test_drive(world):
 
     drive(1.0, deg_per_s=0)
     await world.simulate(seconds=1.0)
-    assert_pose(1.0, 0,  yaw=0)
+    assert_pose(1.0, 0, yaw=0)
 
     drive(0.0, deg_per_s=90)
     await world.simulate(seconds=0.5)
-    assert_pose(1.0, 0,  yaw=45)
+    assert_pose(1.0, 0, yaw=45)
 
     drive(1.0, deg_per_s=0)
     await world.simulate(seconds=np.sqrt(2))
-    assert_pose(2.0, 1.0,  yaw=45, linear_tolerance=0.1)
+    assert_pose(2.0, 1.0, yaw=45, linear_tolerance=0.1)
