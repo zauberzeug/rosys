e58b1c918263454292a96c4e21bd1d9873b57fa0
Falko Schindler <mail@falkoschindler.de>
2021-02-20 06:32:18 +0100

    system: minor cleanup

--- system/tests/test_clock.py
+++ system/tests/test_clock.py
@@ -1,11 +1,8 @@
 import pytest
-from world.clock import Clock
-from world.robot import Robot
-from world.world import World
 
 
 @pytest.mark.asyncio
-async def test_intervals(world):
+async def test_clock(world):
 
     await world.simulate(seconds=1.0)
     assert world.clock.time > 1.0
