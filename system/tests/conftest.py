17e8adb10ea69f209768d247f5e088d5da8caa9f
Rodja Trappe <rodja@perpetual-mobile.de>
2021-02-20 09:12:50 +0100

    system: enabled pytest rewriting of assert messages for helper methods

--- system/tests/conftest.py
+++ system/tests/conftest.py
@@ -1,9 +1,9 @@
-import pytest
-from world.clock import Clock
-from world.robot import Robot
-from world.world import World
 from typing import Generator
-from tests.helper import set_global_world
+from world.world import World
+from world.robot import Robot
+from world.clock import Clock
+import pytest
+pytest.register_assert_rewrite("tests.helper")
 
 
 @pytest.fixture
@@ -12,5 +12,6 @@ def world() -> Generator:
     clock = Clock(interval=0.1)
     robot = Robot(width=0.5)
     world = World(clock=clock, robot=robot)
+    from tests.helper import set_global_world
     set_global_world(world)
     yield world
