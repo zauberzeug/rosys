ad8df165e039712f4f3c89c344e3046b478ce2bb
Falko Schindler <mail@falkoschindler.de>
2021-02-21 21:08:33 +0100

    system: broadcast pose in separate loop

--- system/main.py
+++ system/main.py
@@ -15,33 +15,37 @@ robot = Robot(width=0.5)
 world = World(clock=clock, robot=robot)
 
 
-@sio.on('connect')
-async def on_connect(sid, _):
-    await sio.emit('robot_pose', jsonable_encoder(world.robot.pose), to=sid)
-    return True
-
-
 @sio.on('drive_power')
 def on_drive_power(sid, data):
     print(f'{sid} received drive_power {data}', flush=True)
     world.robot.power(data['left'], data['right'])
 
 
+async def do_updates():
+    while True:
+        await sio.emit('robot_pose', jsonable_encoder(world.robot.pose))
+        await asyncio.sleep(0.1)
+
+
 running_world = None
+client_updates = None
 
 
 @app.on_event("startup")
 async def startup():
     global running_world
+    global client_updates
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
     running_world = task_logger.create_task(world.run())
+    client_updates = task_logger.create_task(do_updates())
 
 
 @app.on_event("shutdown")
 async def shutdown():
     running_world.cancel()
+    client_updates.cancel()
 
 
 @app.get("/api")
