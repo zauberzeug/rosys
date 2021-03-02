977f7c24f941a074f2adb0889b735091caebc21f
jetsonnano@j3 <development@perpetual-mobile.de>
2021-03-02 17:11:55 +0100

    system: read velocity from serial (loop is too slow)

--- system/world/machine.py
+++ system/world/machine.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, Optional
+from world.velocity import Velocity
 from pydantic.main import BaseModel
 import aioserial
 
@@ -13,7 +14,7 @@ class Machine(BaseModel):
         super().__init__(*args, **kwargs)
         self.aioserial_instance = aioserial.AioSerial(self.port, baudrate=115200)
 
-    async def read(self):
+    async def read(self) -> Optional[Velocity]:
 
         line = (await self.aioserial_instance.readline_async()).decode().strip()
         if '^' in line:
@@ -23,6 +24,6 @@ class Machine(BaseModel):
                 checksum ^= ord(c)
             if checksum != int(check):
                 return
-        print(line, flush=True)
 
-        # TODO: extract status information from line
+        words = line.split()
+        return Velocity(linear=float(words[1]), angular=float(words[2]))
--- system/world/robot.py
+++ system/world/robot.py
@@ -17,23 +17,27 @@ class Robot(BaseModel):
     velocity: Velocity = Velocity()
 
     def drive(self, linear: float, angular: float):
+
         self.velocity.linear = linear
         self.velocity.angular = angular
 
     def power(self, left: float, right: float):
 
-        self.velocity.linear = (left + right) / 2.0
-        self.velocity.angular = (right - left) / self.width / 2.0
+        if self.machine:
+            self.machine.send('pw %.3f,%.3f' % (left, right))
+        else:
+            self.velocity.linear = (left + right) / 2.0
+            self.velocity.angular = (right - left) / self.width / 2.0
 
     async def loop(self, dt: float):
 
         if self.machine:
-            await self.machine.read()
-            # self.velocity = self.machine.velocity
+            self.velocity = (await self.machine.read()) or self.velocity
 
         self.pose.x += dt * self.velocity.linear * np.cos(self.pose.yaw)
         self.pose.y += dt * self.velocity.linear * np.sin(self.pose.yaw)
         self.pose.yaw += dt * self.velocity.angular
+        print(self.pose, flush=True)
 
     def automate(_, coro: Coroutine):
         task_logger.create_task(coro)
