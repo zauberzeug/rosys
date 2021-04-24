99f3ddabe4f1589c27ff07010a10cf25ad91ed45
jetsonnano@j3 <development@perpetual-mobile.de>
2021-04-24 11:49:24 +0200

    show mode in ui

--- frontend/src/app/robot.service.ts
+++ frontend/src/app/robot.service.ts
@@ -6,12 +6,14 @@ import { WrappedSocket } from "./socket-io/socket-io.service";
   providedIn: "root",
 })
 export class RobotService {
+  mode?: number;
   time: number = 0;
   pose?: Pose;
   width: number = 1.0;
 
   constructor(private socket: WrappedSocket) {
     socket.on("world", (data: any) => {
+      this.mode = data.mode;
       this.width = data.robot.width;
       this.pose = Pose.from_dict(data.robot.pose);
       this.time = data.time;
