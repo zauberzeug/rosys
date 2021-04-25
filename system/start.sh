f6a6f599e7d9d27ba1769e2d488a8f4fd1397b20
Rodja Trappe <rodja@zauberzeug.com>
2021-04-25 04:22:25 +0200

    adding profiling capabilities

--- system/start.sh
+++ system/start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ $1 = "dev" ]]; then
+if [[ $1 = "debug" ]]; then
    python3 -m debugpy --listen 5678 /app/main.py
+elif [[ $1 = "profile" ]]; then
+    kernprof /app/main.py
 else
     # only use one worker because we do not have a message quque (https://trello.com/c/qpgsAx9y/46-deployment-auf-learning-loopai#comment-5fe6d7d9ef84285e09cd10dc)
     export MAX_WORKERS=1
