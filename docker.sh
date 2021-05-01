7ed6f71e54916c4187c82b1aa2e5895775d9ed48
Rodja Trappe <rodja@zauberzeug.com>
2021-05-01 05:10:32 +0200

    began overriding compose file for nvidia worksataions

--- docker.sh
+++ docker.sh
@@ -32,6 +32,7 @@ os=`uname`
 case $os in
     Linux)
         [ -f /etc/nv_tegra_release ] && compose_args="$compose_args -f docker-compose.yml -f docker-compose.jetson.yml"
+        ( nvidia-smi > /dev/null 2>&1 ) && compose_args="$compose_args -f docker-compose.yml -f docker-compose.nvidia.yml"
         ;;
     Darwin)
         compose_args="$compose_args -f docker-compose.yml -f docker-compose.mac.yml"
