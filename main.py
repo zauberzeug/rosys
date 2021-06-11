f25a36b0bed762209697851671132101a89542a2
Falko Schindler <falko@zauberzeug.com>
2021-06-11 11:09:49 +0200

    move three element into rosys library

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from rosys.runtime import Runtime
 from rosys.world.mode import Mode
 from rosys.ui.joystick import Joystick
+from rosys.ui.three import Three
 
 has_esp = os.path.exists('/dev/esp') and os.stat('/dev/esp').st_gid > 0
 runtime = Runtime(Mode.REAL if has_esp else Mode.SIMULATION)
@@ -17,6 +18,9 @@ ui.timer(0.1, lambda: state.set_text(f'''
 '''))
 
 Joystick(size=50, color='blue', on_drive=lambda linear, angular: jp.run_task(runtime.esp.drive(linear, angular)))
+
+three = Three(runtime.world.robot.pose)
+ui.timer(0.1, lambda: three.move_robot(runtime.world.robot.pose))
 
 
 @jp.app.on_event('startup')
