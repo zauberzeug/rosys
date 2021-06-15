958c138992caad227c458e1acc4c63160f07baf6
jetsonnano@j28 <development@perpetual-mobile.de>
2021-06-15 14:45:25 +0200

    show inputs

--- hardware-test/main.py
+++ hardware-test/main.py
@@ -2,8 +2,8 @@
 from nicegui import ui
 import justpy as jp
 import aioserial
-import asyncio
 from collections import namedtuple
+import time
 
 port = aioserial.AioSerial('/dev/ttyTHS1', baudrate=115200)
 
@@ -18,6 +18,7 @@ sockets = [
     Socket(['MCP_A5', 'MCP_A4'], ['MCP_B2', 'MCP_B3']),
     Socket(['MCP_A7', 'MCP_A6'], ['MCP_B0', 'MCP_B1']),
 ]
+inputs = {}
 
 def send(text):
 
@@ -29,12 +30,14 @@ def configure():
 
     send('esp erase')
     for s, socket in enumerate(sockets):
-        for i, input_ in enumerate(socket.outputs):
+        for i, input_ in enumerate(socket.inputs):
             send(f'+new button IN_{s+1}_{i+1} {input_}')
         for o, output in enumerate(socket.outputs):
             send(f'+new led OUT_{s+1}_{o+1} {output}')
+        time.sleep(0.1)
     send('+set esp.ready=1')
     send('+set esp.24v=1')
+    send('+set esp.outputModules=' + ','.join(inputs.keys()))
     send('esp restart')
 
 async def read():
@@ -45,7 +48,6 @@ async def read():
         line = await port.readline_async()
         if line:
             line = line.decode().strip()
-            print(line)
             if '^' in line:
                 line, check = line.split('^')
                 checksum = 0
@@ -55,22 +57,45 @@ async def read():
                     line = line + '^' + check
             line = line.replace('[0;32mI ', '<span style="color:green">')
             line = line.replace('[0m', '</span>')
+            print(line)
             lines += [line]
             lines = lines[-20:]
-    
-message_input = ui.input(placeholder="Send message...", on_change=lambda e: send(e.value))
 
-ui.button('Configure', on_click=configure)
+            if line.startswith('esp get '):
+                words = line.split()
+                for i, (name, icon) in enumerate(inputs.items()):
+                    icon.visible = words[3+i] == '0'
+    
+with ui.row():
+    with ui.card():
+        ui.markdown('#### READY')
+        ui.toggle({0: 'off', 1: 'on'}, on_change=lambda e: send(f'set esp.ready={e.value}'))
+    with ui.card():
+        ui.markdown('#### 24V')
+        ui.toggle({0: 'off', 1: 'on'}, on_change=lambda e: send(f'set esp.24v={e.value}'))
+    ui.button('Configure', on_click=configure)
 
 with ui.row():
     for s, socket in enumerate(sockets):
         with ui.card():
-            ui.label(f'Socket {s+1}')
+            ui.markdown(f'#### Socket {s+1}')
             for o, output in enumerate(socket.outputs):
                 name = f'OUT_{s+1}_{o+1}'
-                with ui.row():
+                with ui.row().style('align-items:center'):
                     ui.label(name)
                     ui.toggle(['on', 'off', 'pulse'], on_change=lambda e,name=name: send(f'{name} {e.value}'))
+            for i, input_ in enumerate(socket.inputs):
+                name = f'IN_{s+1}_{i+1}'
+                with ui.row():
+                    ui.label(name)
+                    inputs[name] = ui.icon('lens')
+                    inputs[name].visible = False
+    def read_modules():
+        send('esp get')
+    timer = ui.timer(0.5, read_modules)
+    ui.checkbox('Auto-reload inputs').bind_value_to(timer.active)
+
+message_input = ui.input(placeholder="Send message...", on_change=lambda e: send(e.value))
 
 with ui.card():
     lines = []
