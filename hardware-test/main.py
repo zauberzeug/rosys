3d4fdfeac8f1c06b8134edd1c46c484d7dd1ecc8
jetsonnano@j28 <development@perpetual-mobile.de>
2021-06-15 10:49:07 +0200

    use aioserial

--- hardware-test/main.py
+++ hardware-test/main.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
 from nicegui import ui
-import serial
-from line_reader import LineReader
+import justpy as jp
+import aioserial
+import asyncio
 
-port = serial.Serial("/dev/ttyTHS1", baudrate=115200, timeout=0.1)
-line_reader = LineReader(port)
+port = aioserial.AioSerial('/dev/ttyTHS1', baudrate=115200)
 
 def send(text):
 
     print(text)
-
+    message_input.value = ''
     port.write((text + '\n').encode())
-    
-text = ui.input()
-ui.button('Send', on_click=lambda: send(text.value))
-
-lines = []
-output = ui.label().style('white-space:pre;font-family:monospace')
 
 def configure():
 
@@ -27,26 +21,40 @@ def configure():
     send('+led on')
     send('esp restart')
 
-ui.button('Configure', on_click=configure)
-
-def read():
+async def read():
 
     global lines
 
-    line = line_reader.readline()
-    if line:
-        line = line.decode().strip()
-        if '^' in line:
-            line, check = line.split('^')
-            checksum = 0
-            for c in line:
-                checksum ^= ord(c)
-            if checksum != int(check):
-                line = line + '^' + check
-        line = line.replace('[0;32mI ', '<span style="color:green">')
-        line = line.replace('[0m', '</span>')
-        lines += [line]
-        lines = lines[-20:]
-        output.view.inner_html = '\n'.join(lines)
+    while True:
+        line = await port.readline_async()
+        if line:
+            line = line.decode().strip()
+            print(line)
+            if '^' in line:
+                line, check = line.split('^')
+                checksum = 0
+                for c in line:
+                    checksum ^= ord(c)
+                if checksum != int(check):
+                    line = line + '^' + check
+            line = line.replace('[0;32mI ', '<span style="color:green">')
+            line = line.replace('[0m', '</span>')
+            lines += [line]
+            lines = lines[-20:]
+        await asyncio.sleep(0)
+    
+message_input = ui.input(placeholder="Send message...", on_change=lambda e: send(e.value))
 
-ui.timer(0.01, lambda: read())
+ui.button('Configure', on_click=configure)
+
+with ui.card():
+    lines = []
+    output = ui.label().style('white-space:pre;font-family:monospace')
+    def update_output():
+        output.view.inner_html = '\n'.join(lines)
+    ui.timer(0.1, update_output)
+
+@jp.app.on_event('startup')
+def startup():
+    jp.run_task(read())
+
