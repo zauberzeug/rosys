7dbf67e9915ebc0dbe911c08c6313a80da88324e
Falko Schindler <falko@zauberzeug.com>
2021-06-23 11:27:22 +0200

    renaming

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ ui.timer(0.1, lambda: state.set_text(f'''
 Joystick(size=50, color='blue', steerer=runtime.steerer)
 
 three = Three(runtime.world.robot.pose)
-ui.timer(0.05, lambda: three.move_robot(runtime.world.robot.pose))
+ui.timer(0.05, lambda: three.set_robot_pose(runtime.world.robot.pose))
 
 ui.on_startup(runtime.run())
 ui.on_shutdown(runtime.stop())
