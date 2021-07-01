302171a3735b6603b9c0d22bb6bc4bd4642b6937
Rodja Trappe <rodja@zauberzeug.com>
2021-07-01 08:43:31 +0200

    first shot at displaying images

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
+from controller.esptool import run
 from nicegui import ui
 import os
 from rosys.runtime import Runtime
 from rosys.world.mode import Mode
 from rosys.ui.joystick import Joystick
 from rosys.ui.three import Three
+import base64
+
 import icecream
 icecream.install()
 
-# has_esp = os.path.exists('/dev/esp') and os.stat('/dev/esp').st_gid > 0
-# runtime = Runtime(Mode.REAL if has_esp else Mode.SIMULATION)
-runtime = Runtime(Mode.SIMULATION)
+has_esp = os.path.exists('/dev/esp') and os.stat('/dev/esp').st_gid > 0
+runtime = Runtime(Mode.REAL if has_esp else Mode.SIMULATION)
+#runtime = Runtime(Mode.SIMULATION)
 
 state = ui.label()
 ui.timer(0.1, lambda: state.set_text(f'''
@@ -21,8 +24,26 @@ ui.timer(0.1, lambda: state.set_text(f'''
 
 ui.button('Download images', on_click=lambda: runtime.world.download_queue.extend(runtime.world.cameras.keys()))
 
+ui.label('Cameras')
+
 cams = ui.label()
 ui.timer(1, lambda: cams.set_text(f'cams: {runtime.world.cameras}'))
+
+cam_image = ui.image().style('width:300px;height:200px')
+
+
+def update_camera_images():
+    if len(runtime.world.images) > 0:
+        image = runtime.world.images[0]
+    else:
+        return
+    data = runtime.world.image_data.get(image.id, None)
+    if data is not None:
+        encoded = base64.b64encode(data).decode("utf-8")
+        cam_image.source = 'data:image/jpeg;base64,' + encoded
+
+
+ui.timer(1, lambda: update_camera_images())
 
 
 def set_height(height):
@@ -34,9 +55,6 @@ def set_height(height):
 
 
 ui.number('Height [m]', on_change=lambda e: set_height(e.value))
-
-detections = ui.label()
-ui.timer(1, lambda: detections.set_text(f'images: {runtime.world.images}'))
 
 Joystick(size=50, color='blue', steerer=runtime.steerer)
 
