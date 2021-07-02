5d51b19855b3d257242de143d6d6590c1620678e
Rodja Trappe <rodja@zauberzeug.com>
2021-07-02 07:22:48 +0000

    improve layout to reduce timer-related traffic

--- main.py
+++ main.py
@@ -15,58 +15,62 @@ has_esp = os.path.exists('/dev/esp') and os.stat('/dev/esp').st_gid > 0
 runtime = Runtime(Mode.REAL if has_esp else Mode.SIMULATION)
 #runtime = Runtime(Mode.SIMULATION)
 
-state = ui.label()
-ui.timer(0.1, lambda: state.set_text(f'''
-    {runtime.world.time:.3f} s
-    (x={runtime.world.robot.pose.x:.3f},
-    y={runtime.world.robot.pose.y:.3f})
-'''))
+with ui.card():
 
-ui.button('Download images', on_click=lambda: runtime.world.download_queue.extend(runtime.world.cameras.keys()))
+    state = ui.label()
+    ui.timer(0.1, lambda: state.set_text(f'''
+        {runtime.world.time:.3f} s
+        (x={runtime.world.robot.pose.x:.3f},
+        y={runtime.world.robot.pose.y:.3f})
+    '''))
 
-ui.label('Cameras')
+    Joystick(size=50, color='blue', steerer=runtime.steerer)
+    three = Three(runtime.world.robot.pose)
+    ui.timer(0.05, lambda: three.set_robot_pose(runtime.world.robot.pose))
 
-cams = ui.label()
-ui.timer(1, lambda: cams.set_text(f'cams: {runtime.world.cameras}'))
+with ui.card():
+
+    ui.label('Cameras')
+
+    cams = ui.label()
+    ui.timer(1, lambda: cams.set_text(f'cams: {runtime.world.cameras}'))
+
+    def set_height(height):
+        for camera in runtime.world.cameras.values():
+            try:
+                camera.calibration.extrinsics.translation[2] = height
+            except AttributeError:
+                pass
+
+    ui.number('Height [m]', on_change=lambda e: set_height(e.value))
 
 with ui.card().style('width:600px'):
+
+    ui.button('Download images', on_click=lambda: runtime.world.download_queue.extend(runtime.world.cameras.keys()))
+
     cam_image = ui.image()
 
+    def update_camera_images():
 
-def update_camera_images():
-    if len(runtime.world.images) > 0:
+        if not any(runtime.world.images):
+            cam_image.source = None
+            cam_image.id = None
+            return
+
         image = runtime.world.images[0]
-        if hasattr(cam_image, 'id') and cam_image.id == image.id:
-            return  # we do not need to update the same image again
-    else:
-        cam_image.source = None
-        cam_image.id = None
-        return
-    data = runtime.world.image_data.get(image.id, None)
-    if data is not None:
+        if cam_image.id == image.id:
+            return False
+
+        data = runtime.world.image_data.get(image.id, None)
+        if data is None:
+            return False
+
         encoded = base64.b64encode(data).decode("utf-8")
         cam_image.source = 'data:image/jpeg;base64,' + encoded
         cam_image.id = image.id
         ic(image.detections)
 
-
-ui.timer(1, lambda: update_camera_images())
-
-
-def set_height(height):
-    for camera in runtime.world.cameras.values():
-        try:
-            camera.calibration.extrinsics.translation[2] = height
-        except AttributeError:
-            pass
-
-
-ui.number('Height [m]', on_change=lambda e: set_height(e.value))
-
-Joystick(size=50, color='blue', steerer=runtime.steerer)
-
-three = Three(runtime.world.robot.pose)
-ui.timer(0.05, lambda: three.set_robot_pose(runtime.world.robot.pose))
+    ui.timer(1, lambda: update_camera_images())
 
 ui.on_startup(runtime.run())
 ui.on_shutdown(runtime.stop())
