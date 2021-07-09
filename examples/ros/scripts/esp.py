9ca8090a9430825a882744ab137c02c6f5b3144f
Zauberzeug on Edge Device <development@zauberzeug.com>
2021-07-09 11:20:17 +0200

    ros: show odometry and status information in ui

--- examples/ros/scripts/esp.py
+++ examples/ros/scripts/esp.py
@@ -1,20 +1,61 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist, Vector3
 import serial
+from operator import ixor
+from functools import reduce
+import json
 
 def handle_command(data):
 
-    print('command: %.3f, %.3f' % (data.linear.x, data.angular.z))
+    line = f'wheels drive {data.linear.x:3f},{data.angular.z:.3f}'
+    print('<', line)
+    line = f'{line}^{reduce(ixor, map(ord, line))}\n'
+    port.write(line.encode())
 
 if __name__ == '__main__':
 
     rospy.init_node('esp')
+
+    publish_odometry = rospy.Publisher('/odometry', Twist, queue_size=1).publish
+    publish_status = rospy.Publisher('/status', String, queue_size=1).publish
 
     rospy.Subscriber('/command', Twist, handle_command, queue_size=1)
 
     with serial.Serial('/dev/serial', 115200) as port:
 
         while not rospy.is_shutdown():
-            line = port.readline()
-            print(line)
+            try:
+                line = port.readline().decode()
+            except UnicodeDecodeError:
+                continue
+
+            if '^' in line:
+                line, checksum = line.split('^')
+                if reduce(ixor, map(ord, line)) != int(checksum):
+                    continue
+
+            words = line.split()
+            if not any(words):
+                continue
+            if words.pop(0) != 'esp':
+                continue
+            print('>', line)
+            time = int(words.pop(0))
+            linear_speed = float(words.pop(0))
+            angular_speed = float(words.pop(0))
+            temperature = float(words.pop(0))
+            battery = float(words.pop(0))
+            state = words.pop(0).lower()
+
+            publish_odometry(Twist(
+                Vector3(linear_speed, 0, 0),
+                Vector3(0, 0, angular_speed),
+            ))
+            publish_status(json.dumps({
+                'time': time,
+                'temperature': temperature,
+                'battery': battery,
+                'state': state,
+            }))
--- examples/ros/scripts/ui.py
+++ examples/ros/scripts/ui.py
@@ -1,21 +1,63 @@
 #!/usr/bin/env python3
 import rospy
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Vector3
 from nicegui import ui
+import json
+
+def send(x, y):
+
+    linear = Vector3(x, 0, 0)
+    angular = Vector3(0, 0, y)
+    publish(Twist(linear, angular))
+
+def handle_status(data):
+
+    msg = json.loads(data.data)
+    time.text = f'Timestamp: {msg["time"]} ms'
+    temperature.text = f'Temperature: {msg["temperature"]} C'
+    battery.text = f'Battery: {msg["battery"]} V'
+    state.text = f'State: {msg["state"]}'
+
+def handle_odometry(data):
+
+    linear.value = data.linear.x
+    angular.value = data.angular.z
+
+with ui.row():
+
+    with ui.card() as status:
+        ui.markdown('### Status')
+        time = ui.label()
+        temperature = ui.label()
+        battery = ui.label()
+        state = ui.label()
+
+    with ui.card() as odometry:
+        ui.markdown('### Odometry')
+        ui.label('linear')
+        linear = ui.slider(min=-1.0, max=1.0, step=0.01).props('label-always readonly')
+        ui.label('angular')
+        angular = ui.slider(min=-1.0, max=1.0, step=0.01).props('label-always readonly')
+
+    with ui.card():
+        ui.markdown('### Control')
+        ui.joystick(
+            color='blue',
+            size=50,
+            on_move=lambda msg: send(msg.data.vector.x, msg.data.vector.y),
+            on_end=lambda _: send(0, 0),
+        )
+
+    ui.timer(0.1, lambda: None) # NOTE: update ui
+
 
 rospy.init_node('ui')
 
 publish = rospy.Publisher('/command', Twist, queue_size=1).publish
 
-def send(x, y):
-    linear = Vector3(x, 0, 0)
-    angular = Vector3(0, 0, y)
-    publish(Twist(linear, angular))
+rospy.Subscriber('/status', String, handle_status, queue_size=1)
+rospy.Subscriber('/odometry', Twist, handle_odometry, queue_size=1)
 
-ui.joystick(
-    color='blue',
-    size=50,
-    on_move=lambda msg: send(msg.data.vector.x, msg.data.vector.y),
-    on_end=lambda _: send(0, 0))
 
 ui.run(reload=False, show=False)
