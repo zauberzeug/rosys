15996d39c7355da11d7ae2197d7cdee75c733038
Zauberzeug on Edge Device <development@zauberzeug.com>
2021-07-16 15:11:25 +0200

    improve measure helper

--- rosys/helpers.py
+++ rosys/helpers.py
@@ -4,15 +4,16 @@ import traceback
 import sys
 
 
-def measure(reset: bool = False):
+def measure(*, reset: bool = False, ms: bool = False):
 
     global t
     if 't' in globals() and not reset:
         dt = time.time() - t
         line = inspect.stack()[1][0].f_lineno
-        print(f'{inspect.stack()[1].filename}:{line}', "%6.3f s" % (dt), flush=True)
+        output = f'{dt * 1000:7.3f} ms' if ms else f'{dt:7.3f} s'
+        print(f'{inspect.stack()[1].filename}:{line}', output, flush=True)
     if reset:
-        print('------------ starting new measurements -----------', flush=True)
+        print('------------', flush=True)
     t = time.time()
 
 
