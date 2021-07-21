e06d377ac3b15b88aa2c9328cf74a2dc4dc59f2a
Falko Schindler <falko@zauberzeug.com>
2021-07-21 13:40:49 +0200

    handle missing backup file

--- rosys/persistence.py
+++ rosys/persistence.py
@@ -6,6 +6,8 @@ from .world.camera import Camera
 from .world.robot import RobotParameters
 from .world.spline import Spline
 
+filepath = '/data/backup/world.json'
+
 
 def backup(world: World):
 
@@ -15,18 +17,22 @@ def backup(world: World):
         'path': [spline.dict() for spline in world.path],
         'robot': {'parameters': world.robot.parameters.dict()}
     }
-    os.makedirs('/data/backup', exist_ok=True)
-    with open('/data/backup/world.json', 'w') as f:
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'w') as f:
         json.dump(dict_, f)
 
 
 def restore(world: World):
 
+    if not os.path.exists(filepath):
+        logging.warning('No backup file found.')
+        return
+
     try:
-        with open('/data/backup/world.json', 'r') as f:
+        with open(filepath, 'r') as f:
             dict_ = json.load(f)
         world.cameras = {mac: Camera.parse_obj(camera) for mac, camera in dict_['cameras'].items()}
         world.path = [Spline.parse_obj(spline) for spline in dict_['path']]
         world.robot.parameters = RobotParameters.parse_obj(dict_['robot']['parameters'])
     except:
-        logging.exception('could not load from backup')
+        logging.exception('Could not load from backup')
