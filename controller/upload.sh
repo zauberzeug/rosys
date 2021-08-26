32a9e3921f39fca746f95b7c2018f274547141b2
Falko Schindler <falko@zauberzeug.com>
2021-08-26 16:46:55 +0200

    use esp-idf docker image for building controller binary on mac

--- controller/upload.sh
+++ controller/upload.sh
@@ -27,5 +27,19 @@ then
         make -j4 flash
     fi
 else
-    $HOME/.platformio/penv/bin/pio run --target upload
+    docker run --rm -v $PWD:/project -w /project espressif/idf:v4.2 make -j4
+    ./esptool.py \
+        --chip esp32 \
+        --port /dev/tty.SLAB_USBtoUART \
+        --baud 921600 \
+        --before default_reset \
+        --after hard_reset \
+        write_flash \
+        -z \
+        --flash_mode dio \
+        --flash_freq 40m \
+        --flash_size detect \
+        0x1000 build/bootloader/bootloader.bin \
+        0x8000 build/partitions_singleapp.bin \
+        0x10000 build/zauberzeug-robot-brain.bin
 fi
