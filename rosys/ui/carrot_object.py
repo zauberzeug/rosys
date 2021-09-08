7d75c7d6667419719c885eca1830765948a3e869
Falko Schindler <falko@zauberzeug.com>
2021-09-08 15:21:23 +0200

    fix drawbot navigation

--- rosys/ui/carrot_object.py
+++ rosys/ui/carrot_object.py
@@ -9,12 +9,16 @@ class CarrotObject(Object3D):
         super().__init__('group')
         self.robot = robot
         with self:
-            self.sphere = Sphere(0.05)
+            self.sphere0 = Sphere(0.03)
+            self.sphere1 = Sphere(0.05).move(robot.parameters.carrot_offset)
         self.update()
 
     def update(self):
         if self.robot.carrot is None:
-            self.sphere.material('#ff8800', 0)
+            self.sphere0.material('#ff8800', 0)
+            self.sphere1.material('#ff8800', 0)
         else:
-            self.sphere.material('#ff8800', 1)
+            self.sphere0.material('#ff8800', 1)
+            self.sphere1.material('#ff8800', 1)
             self.move(self.robot.carrot.x, self.robot.carrot.y)
+            self.rotate(0, 0, self.robot.carrot.yaw)
--- rosys/ui/robot_object.py
+++ rosys/ui/robot_object.py
@@ -5,14 +5,15 @@ from rosys.world.robot import Robot
 
 class RobotObject(Object3D):
 
-    def __init__(self, robot: Robot, *, show_hook: bool = False):
+    def __init__(self, robot: Robot, *, show_hook: bool = False, wireframe: bool = False):
         super().__init__('group')
         self.robot = robot
         with self:
             outline = list(map(list, robot.shape.outline))
-            Extrusion(outline, robot.shape.height).material('#4488ff', 0.5)
+            Extrusion(outline, robot.shape.height, wireframe=wireframe).material('#4488ff', 0.5)
             if show_hook:
-                Sphere(0.05).move(robot.parameters.hook_offset).material('#4488ff', 0.5)
+                Sphere(0.03).material('#4488ff', 0.5)
+                Sphere(0.05).material('#4488ff', 0.5).move(robot.parameters.hook_offset)
         self.update()
 
     def update(self):
