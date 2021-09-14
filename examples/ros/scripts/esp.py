41fdbf66ed2483e0f10dad1782ada5c7345ed452
Rodja Trappe <rodja@zauberzeug.com>
2021-09-14 05:17:49 +0200

    formatting

--- examples/ros/scripts/esp.py
+++ examples/ros/scripts/esp.py
@@ -8,17 +8,17 @@ from functools import reduce
 import json
 import os.path
 
-def send(line):
 
+def send(line):
     line = f'{line}^{reduce(ixor, map(ord, line))}\n'
     port.write(line.encode())
 
-def handle_command(data):
 
+def handle_command(data):
     send(f'drive speed {data.linear.x:3f},{data.angular.z:.3f}')
 
-def handle_configure(data):
 
+def handle_configure(data):
     with open(os.path.dirname(__file__) + '/../config.txt') as f:
         send('esp erase')
         for line in f.read().splitlines():
@@ -27,10 +27,9 @@ def handle_configure(data):
                 send('+' + line)
         send('esp restart')
 
+
 if __name__ == '__main__':
-
     rospy.init_node('esp')
-
     publish_odometry = rospy.Publisher('/odometry', Twist, queue_size=1).publish
     publish_status = rospy.Publisher('/status', String, queue_size=1).publish
 
@@ -38,13 +37,11 @@ if __name__ == '__main__':
     rospy.Subscriber('/configure', Empty, handle_configure, queue_size=1)
 
     with serial.Serial('/dev/esp', 115200) as port:
-
         while not rospy.is_shutdown():
             try:
                 line = port.readline().decode()
             except UnicodeDecodeError:
                 continue
-
             if '^' in line:
                 line, checksum = line.split('^')
                 if reduce(ixor, map(ord, line)) != int(checksum):
