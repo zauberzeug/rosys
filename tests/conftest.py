bc35411e8697aa221aaff2e8ffb78b88a99825d8
Rodja Trappe <rodja@zauberzeug.com>
2021-10-10 07:34:08 +0200

    sticky cams: enabled vscode pytests and logging

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,45 @@ from rosys.world.world import World, WorldState
 from rosys.world.robot import Robot
 from rosys.world.marker import Marker
 import asyncio
+import logging
+import logging.config
 
 import icecream
 icecream.install()
+
+config = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'level': 'INFO',
+            'stream': 'ext://sys.stdout'
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['console'],
+            'level': 'WARN'
+        },
+        'rosys': {
+            'level': 'DEBUG'
+        },
+        'rosys.ota': {
+            'level': 'WARN'
+        }
+    },
+}
+
+logging.config.dictConfig(config)
+
 
 pytest.register_assert_rewrite("tests.helper")
 
