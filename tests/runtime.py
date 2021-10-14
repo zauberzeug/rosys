300dc5b9e63b572136faada1b122dd3dfa00d20b
Falko Schindler <falko@zauberzeug.com>
2021-10-14 17:31:47 +0200

    introduce custom run_in_executor() that does not advance time in test mode

--- tests/runtime.py
+++ tests/runtime.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Callable
 from rosys.runtime import Runtime
 from rosys.world.marker import Marker
 from rosys.world.mode import Mode
@@ -7,6 +8,7 @@ from rosys.world.world import World, WorldState
 
 
 class TestRuntime(Runtime):
+    is_time_running: bool = True
 
     def __init__(self):
         robot = Robot(marker=Marker(points={'front': (0.12, 0), 'back': (-0.12, 0)}, height=0.58))
@@ -20,6 +22,7 @@ class TestRuntime(Runtime):
 
         for actor in self.actors:
             actor.sleep = self.sleep
+            actor.run_in_executor = self.run_in_executor
 
     async def forward(self, seconds, dt=0.01):
         # NOTE we start runtime here because this makes it easy in the tests to prepare it beforehand
@@ -28,10 +31,21 @@ class TestRuntime(Runtime):
 
         end_time = self.world.time + seconds
         while self.world.time <= end_time:
-            self.world.set_time(self.world.time + dt)
-            await asyncio.sleep(0)
+            if self.is_time_running:
+                self.world.set_time(self.world.time + dt)
+                await asyncio.sleep(0)
+            else:
+                await asyncio.sleep(0.01)
 
     async def sleep(self, seconds: float):
         sleep_end_time = self.world.time + seconds
         while self.world.time <= sleep_end_time:
             await asyncio.sleep(0)
+
+    async def run_in_executor(self, callback: Callable):
+        try:
+            self.is_time_running = False
+            loop = asyncio.get_running_loop()
+            return await loop.run_in_executor(None, callback)
+        finally:
+            self.is_time_running = True
