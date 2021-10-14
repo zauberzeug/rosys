fc506231e5f701c8d8ecdf077521660e5516a40e
Falko Schindler <falko@zauberzeug.com>
2021-10-14 10:55:30 +0200

    minor cleanup

--- rosys/factory.py
+++ rosys/factory.py
@@ -1,9 +1,8 @@
-import os
-from rosys.world.world import World
 from .actors.esp import Esp
 from .actors.serial_esp import SerialEsp
 from .actors.mocked_esp import MockedEsp
 from .actors.web_esp import WebEsp
+from .world.world import World
 import logging
 
 log = logging.getLogger(__name__)
