840e0fa1e1fdf273ac5e4ca2a348dd5aa9b00238
Falko Schindler <falko@zauberzeug.com>
2021-10-14 10:14:26 +0200

    improve logging format for unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,53 @@
 from typing import Generator
 import pytest
+import asyncio
+import logging
+import logging.config
+import os
+import sys
 from rosys.runtime import Runtime, Mode
 from rosys.world.world import World, WorldState
 from rosys.world.robot import Robot
 from rosys.world.marker import Marker
-import asyncio
-import logging
-import logging.config
 
 import icecream
 icecream.install()
+
+
+class PackagePathFilter(logging.Filter):
+    # https://stackoverflow.com/a/52582536/3419103
+    def filter(self, record):
+        pathname = record.pathname
+        record.relativepath = None
+        abs_sys_paths = map(os.path.abspath, sys.path)
+        for path in sorted(abs_sys_paths, key=len, reverse=True):  # longer paths first
+            if not path.endswith(os.sep):
+                path += os.sep
+            if pathname.startswith(path):
+                record.relativepath = os.path.relpath(pathname, path)
+                break
+        return True
+
 
 config = {
     'version': 1,
     'disable_existing_loggers': True,
     'formatters': {
         'default': {
-            'format': '%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s: %(message)s',
+            'format': '%(asctime)s.%(msecs)03d [%(levelname)s] %(relativepath)s:%(lineno)d: %(message)s',
             'datefmt': '%Y-%m-%d %H:%M:%S',
         },
+    },
+    'filters': {
+        'package_path_filter': {
+            '()': PackagePathFilter,
+        }
     },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
             'formatter': 'default',
+            'filters': ['package_path_filter'],
             'level': 'INFO',
             'stream': 'ext://sys.stdout'
         },
@@ -34,9 +58,11 @@ config = {
             'level': 'WARN'
         },
         'rosys': {
+            'handlers': ['console'],
             'level': 'DEBUG'
         },
         'rosys.ota': {
+            'handlers': ['console'],
             'level': 'WARN'
         }
     },
