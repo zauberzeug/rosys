1c3aef02ce525b7a729423c6137c0e8431f01f70
Rodja Trappe <rodja@zauberzeug.com>
2021-10-31 06:28:28 +0100

    removed supressing of warnings ... seems not to be neccessary and gc collection takes to much time on event loop

--- rosys/actors/automator.py
+++ rosys/actors/automator.py
@@ -7,18 +7,6 @@ import warnings
 import gc
 
 
-@contextlib.contextmanager
-def silence_coro():
-    '''Supress warning about coroutine is never awaited.
-
-    This is ok because the Automator uses coro.send(None) to only step one chunck at a time.
-    Similar code is used in  https://github.com/python/cpython/pull/5410'''
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        yield
-        gc.collect()
-
-
 class Automator(Actor):
     interval: float = 0.1
 
@@ -27,13 +15,11 @@ class Automator(Actor):
         self.routines = []
 
     def add(self, coro: Coroutine):
-        with silence_coro():
-            self.routines.append(coro)
+        self.routines.append(coro)
 
     def replace(self, coro: Coroutine):
-        with silence_coro():
-            self.routines.clear()
-            self.add(coro)
+        self.routines.clear()
+        self.add(coro)
 
     async def step(self, world: World):
         if world.state != WorldState.RUNNING:
