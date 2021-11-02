92e9143b42ccd1d85ca3d594bd061d33ff9bcfab
Falko Schindler <falko@zauberzeug.com>
2021-11-02 17:08:36 +0100

    code review and refactoring

--- rosys/ui/keyboard_control.py
+++ rosys/ui/keyboard_control.py
@@ -1,61 +1,53 @@
+import numpy as np
 from nicegui.ui import Ui
+from nicegui.events import KeyEventArguments
 from rosys.world.point import Point
 from rosys.actors.steerer import Steerer
 
 
-class KeyboardControl():
+class KeyboardControl:
 
     # these will be set by rosys.ui.configure
     steerer: Steerer = None
     ui: Ui = None
 
-    def __init__(self, *, default_speed=2) -> None:
-        self.ui.keyboard(self.handle_keys)
+    def __init__(self, *, default_speed: float = 2.0):
+        self.ui.keyboard(on_key=self.handle_keys)
         self.direction = Point(x=0, y=0)
         self.speed = default_speed
 
-    def adapt_direction(self, e, modifier):
-        if e.key.left:
-            self.direction.y = - modifier
-        elif e.key.right:
-            self.direction.y = + modifier
-        elif e.key.up:
-            self.direction.x = + modifier
-        elif e.key.down:
-            self.direction.x = - modifier
-
-    def adjust_speed(self, modifier):
-        if self.direction.y < 0:
-            self.direction.y = - modifier/2
-        elif self.direction.y > 0:
-            self.direction.y = + modifier/2
-
-        if self.direction.x > 0:
-            self.direction.x = + modifier
-        elif self.direction.x < 0:
-            self.direction.x = - modifier
-
-    def handle_keys(self, e):
-        if e.key.repeat:
+    def handle_keys(self, e: KeyEventArguments):
+        if e.action.repeat:
             return
 
-        if e.action.keydown and e.key.numberkey:
-            self.speed = int(e.key.numberkey)
-            self.adjust_speed(self.speed)
+        # change speed via number key
+        if e.action.keydown and e.key.number is not None:
+            self.speed = e.key.number
+            self.direction.y = self.speed * np.sign(self.direction.y)
+            self.direction.x = self.speed * np.sign(self.direction.x)
             self.steerer.update(self.direction.y, self.direction.x)
 
-        if e.key.shift and e.action.keyup:
-            self.direction.x = self.direction.y = 0
-            self.steerer.stop()
+        # update individual speed components via arrow keys
+        if e.modifiers.shift and e.key.is_cursorkey:
+            if e.action.keydown and self.direction.x == 0 and self.direction.y == 0:
+                self.steerer.start()
 
-        elif e.modifiers.shiftkey and e.key.is_cursorkey:
-            if e.action.keydown:
-                if self.direction.x == 0 and self.direction.y == 0:
-                    self.steerer.start()
-                self.adapt_direction(e, self.speed)
-            elif e.action.keyup:
-                self.adapt_direction(e, 0)
-
+            new_speed = self.speed if e.action.keydown else 0
+            if e.key.arrow_left:
+                self.direction.y = -new_speed
+            elif e.key.arrow_right:
+                self.direction.y = +new_speed
+            elif e.key.arrow_up:
+                self.direction.x = +new_speed
+            elif e.key.arrow_down:
+                self.direction.x = -new_speed
             self.steerer.update(self.direction.y, self.direction.x)
-            if self.direction.x == 0 and self.direction.y == 0:
+
+            if e.action.keyup and self.direction.x == 0 and self.direction.y == 0:
                 self.steerer.stop()
+
+        # stop when releasing shift key
+        if e.key.shift and e.action.keyup:
+            self.direction.y = 0
+            self.direction.x = 0
+            self.steerer.stop()
