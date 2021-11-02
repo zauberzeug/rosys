611a8ebcf44a71cc056846bc347985345848fc1c
Zauberzeug on Edge Device <development@zauberzeug.com>
2021-11-02 03:31:50 +0100

    starting rosys with highest prio

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $1 = "debug" ]]; then
-   python3 -m debugpy --listen 5678 main.py
+   nice -n -19 python3 -m debugpy --listen 5678 main.py
 else
-   python3 main.py
+   nice -n -19 python3 main.py
 fi
