8ec84f67c882df5e6a966e911fb35ddab3249be2
Rodja Trappe <rodja@zauberzeug.com>
2021-11-07 07:03:16 +0100

    making code profileable with py-spy

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 if [[ $1 = "debug" ]]; then
    nice -n -19 python3 -m debugpy --listen 5678 main.py
+elif [[ $1 = "profile" ]]; then
+   nice -n -19 py-spy record -o profile.svg -- python3 main.py
 else
    nice -n -19 python3 main.py
 fi
