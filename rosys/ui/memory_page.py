e4d470056764e64a13ee9744e90790d0cfe7e950
Rodja Trappe <rodja@zauberzeug.com>
2021-11-26 14:48:19 +0100

    showing image graph for object

--- rosys/ui/memory_page.py
+++ rosys/ui/memory_page.py
@@ -6,6 +6,8 @@ from outside_in_tracking import World
 from rosys import event
 from datetime import datetime
 import objgraph
+import random
+import base64
 
 
 class MemoryPage:
@@ -16,3 +18,19 @@ class MemoryPage:
             self.ui.label('Analyze Memory')
             self.ui.button('log most common types', on_click=lambda: objgraph.show_most_common_types(limit=20))
             self.ui.button('log growth', on_click=lambda: objgraph.show_growth(limit=20))
+
+            self.ui.input('Search Object', on_change=self.update_graph)
+            self.graph = self.ui.row().style('width:40%')
+            self.graph.visible = False
+
+    def update_graph(self, event):
+        objgraph.show_chain(objgraph.find_backref_chain(
+            random.choice(objgraph.by_type(event.value)),  # just pick one of the found objects
+            objgraph.is_proper_module),
+            filename='/tmp/rosys_objgraph.png')
+        with open('/tmp/rosys_objgraph.png', "rb") as f:
+            image = base64.b64encode(f.read()).decode('utf-8')
+        self.graph.clear()
+        self.graph.visible = True
+        with self.graph:
+            self.ui.image('data:image/png;base64,'+str(image))
