d737f01e45eb873e16ce77fdd73d83705de8b51a
Falko Schindler <falko@zauberzeug.com>
2021-12-14 11:28:49 +0100

    simplify proxy, use pyserial instead of aioserial

--- rosys/communication/proxy.py
+++ rosys/communication/proxy.py
@@ -2,56 +2,39 @@
 import socketio
 import uvicorn
 import logging
-import aioserial
+import serial
 from checksum import augment, check
 
-aioserial = aioserial.AioSerial('/dev/ttyTHS1', baudrate=115200)
-buffer = ''
-
 sio = socketio.AsyncServer(async_mode='asgi')
-sio.connected = False
-
-stop_requested = False
 
 
 @sio.event
-def connect(sid, environ, auth):
-    sio.connected = True
-
-
-@sio.event
-def disconnect(sid):
-    sio.connected = False
-
-
-@sio.event
-async def write(sid, line):
-     await aioserial.write_async(f'{augment(line)}\n'.encode())
+def write(sid, line):
+    serial.write(f'{augment(line)}\n'.encode())
 
 
 async def receive():
     global buffer
     try:
         while not stop_requested:
-            if not sio.connected:
-                await sio.sleep(0.1)
-                continue
             try:
-                buffer += aioserial.read_all().decode()
+                buffer += serial.read_all().decode()
                 await sio.sleep(0)
-            except UnicodeDecodeError:
-                logging.exception('could not decode')
-            if '\n' not in buffer:
-                continue
-            line, buffer = buffer.split('\n', 1)
-            await sio.emit('read', line)
+            except UnicodeDecodeError as e:
+                logging.exception('could not decode', exc_info=e)
+            if '\n' in buffer:
+                line, buffer = buffer.split('\n', 1)
+                await sio.emit('read', check(line.rstrip('\r')))
     except:
         logging.exception('could not read')
 
 
 if __name__ == '__main__':
+    serial = serial.Serial('/dev/ttyTHS1', 115200)
+    buffer = ''
+    app = socketio.ASGIApp(sio, on_startup=lambda: sio.start_background_task(receive))
+    stop_requested = False
     try:
-        app = socketio.ASGIApp(sio, on_startup=lambda: sio.start_background_task(receive))
         uvicorn.run(app, host='0.0.0.0', port=8081)
     except KeyboardInterrupt:
         stop_requested = True
