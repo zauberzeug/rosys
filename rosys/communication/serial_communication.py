38e8629a805efaa6a4f739447e1cc3dbb2281eee
Falko Schindler <falko@zauberzeug.com>
2021-12-14 11:18:32 +0100

    use pyserial instead of aioserial

--- rosys/communication/serial_communication.py
+++ rosys/communication/serial_communication.py
@@ -1,6 +1,7 @@
 from typing import Optional
+import asyncio
 import os
-import aioserial
+import serial
 from .communication import Communication
 from .checksum import augment, check
 
@@ -10,7 +11,7 @@ class SerialCommunication(Communication):
     baudrate: int = 115200
 
     def __init__(self):
-        self.aioserial = aioserial.AioSerial(self.device_path, baudrate=self.baudrate)
+        self.serial = serial.Serial(self.device_path, self.baudrate)
         self.buffer = ''
 
     @classmethod
@@ -18,18 +19,19 @@ class SerialCommunication(Communication):
         return os.path.exists(cls.device_path) and os.stat(cls.device_path).st_gid > 0
 
     def connect(self):
-        if not self.aioserial.isOpen():
-            self.aioserial.open()
+        if not self.serial.isOpen():
+            self.serial.open()
 
     def disconnect(self):
-        if self.aioserial.isOpen():
-            self.aioserial.close()
+        if self.serial.isOpen():
+            self.serial.close()
 
     async def read(self) -> Optional[str]:
-        self.buffer += self.aioserial.read_all().decode()
+        self.buffer += self.serial.read_all().decode()
         if '\n' in self.buffer:
             line, self.buffer = self.buffer.split('\r\n', 1)
             return check(line)
 
     async def send_async(self, line: str):
-        await self.aioserial.write_async(f'{augment(line)}\n'.encode())
+        self.serial.write(f'{augment(line)}\n'.encode())
+        await asyncio.sleep(0)
