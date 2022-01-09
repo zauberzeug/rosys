991f17b4b9be9aa5d7d404ceb3e88dcc528c5e70
Rodja Trappe <rodja@zauberzeug.com>
2022-01-09 16:09:01 +0100

    triggering NEW_CAMERA event in simulation

--- rosys/actors/image_capture_simulation.py
+++ rosys/actors/image_capture_simulation.py
@@ -1,5 +1,6 @@
 from .actor import Actor
 from ..world.camera import Camera, Frame
+from .. import event
 
 
 class ImageCaptureSimulation(Actor):
@@ -7,11 +8,13 @@ class ImageCaptureSimulation(Actor):
 
     def __init__(self) -> None:
         super().__init__()
-        for i in range(2):
-            uid = f'simulated_cam_{i}'
-            self.world.cameras[uid] = Camera(id=uid)
 
     async def step(self):
         await super().step()
+        while len(self.world.cameras) < 2:
+            uid = f'simulated_cam_{len(self.world.cameras)}'
+            self.world.cameras[uid] = Camera(id=uid)
+            await event.call(event.Id.NEW_CAMERA, self.world.cameras[uid])
+
         for camera in self.world.cameras.values():
             camera.frames.append(Frame.create_placeholder(f'{camera.id}: {self.world.time:.2f}', time=self.world.time))
