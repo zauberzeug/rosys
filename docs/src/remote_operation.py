c5325073d78036307bd496568e3ddfdb14113002
Rodja Trappe <rodja@zauberzeug.com>
2022-01-09 14:30:45 +0000

    polish

--- docs/src/remote_operation.py
+++ docs/src/remote_operation.py
@@ -11,11 +11,11 @@ rosys.ui.configure(ui, runtime)
 
 
 async def add_main_camera(camera: Camera):
-    camera_card.clear()  # remove label
+    camera_card.clear()  # remove "seeking cam" label
     with camera_card:
         maincam = ui.image()
         ui.timer(1, lambda: maincam.set_source(camera.latest_frame_uri))
-    rosys.event.unregister(rosys.event.Id.NEW_CAMERA, add_main_camera)
+    rosys.event.unregister(rosys.event.Id.NEW_CAMERA, add_main_camera)  # we only show the first cam
 
 with ui.card().tight().style('width:30em;') as camera_card:
     ui.label('seeking main camera').style('margin:1em')
@@ -27,4 +27,4 @@ with ui.card().tight().style('width:30em;'):
 rosys.ui.keyboard_control()
 rosys.event.register(rosys.event.Id.NEW_CAMERA, add_main_camera)
 
-ui.run(title='RoSys', port=80, show=False)
+ui.run(title='RoSys', port=8080)
