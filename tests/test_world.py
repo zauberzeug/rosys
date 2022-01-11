5aef7e9956d96671588d23aeedea61bdc8d2aabf
Falko Schindler <falko@zauberzeug.com>
2022-01-11 12:02:04 +0100

    cleanup

--- tests/test_world.py
+++ tests/test_world.py
@@ -6,9 +6,9 @@ from rosys.test import TestRuntime
 
 def test_dumping(runtime: TestRuntime):
     data = Persistence(runtime.world).dump()
-    assert 'robot' in data.keys()
-    assert 'obstacles' in data.keys()
-    assert 'automation_state' not in data.keys()
+    assert 'robot' in data
+    assert 'obstacles' in data
+    assert 'automation_state' not in data
 
 
 @pytest.mark.asyncio
@@ -17,10 +17,9 @@ async def test_json_view(runtime: TestRuntime):
     await runtime.forward(1)
     serialized = runtime.world.json()
     data = json.loads(serialized)
-    assert 'robot' in data.keys()
-    assert 'automation_state' in data.keys()
-    assert 'obstacles' in data.keys()
-    assert 'notifications' in data.keys()
-    assert 'cameras' in data.keys()
-    ic(data['cameras'])
-    assert 'frames' not in data['cameras']['simulated_cam_0'].keys()
+    assert 'robot' in data
+    assert 'automation_state' in data
+    assert 'obstacles' in data
+    assert 'notifications' in data
+    assert 'cameras' in data
+    assert 'frames' not in data['cameras']['simulated_cam_0']
