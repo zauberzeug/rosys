67e166cc3e046f16003143ae16c0a9e0b6c5449f
Rodja Trappe <rodja@zauberzeug.com>
2022-01-16 06:00:58 +0100

    added some logging to find root cause of delayed machine communication

--- rosys/actors/lizard.py
+++ rosys/actors/lizard.py
@@ -1,6 +1,7 @@
 from .. import event
 from ..hardware import Hardware
 from . import Actor
+import time
 
 
 class Lizard(Actor):
@@ -15,7 +16,11 @@ class Lizard(Actor):
     async def step(self):
         await self.ensure_responsiveness()
         await self.hardware.update()
+        t = time.time()
         await event.call(event.Id.NEW_MACHINE_DATA)
+        dt = time.time() - t
+        if dt > 0.02:
+            self.log.warning(f'computing machine data took {dt:.2f} s')
 
     async def _handle_pause(self, reason: str):
         await self.hardware.stop()
@@ -23,9 +28,9 @@ class Lizard(Actor):
     async def ensure_responsiveness(self):
         dt = self.world.time - self.last_step if self.last_step is not None else 0
         if dt > 1:
-            msg = 'esp serial communication can not be guaranteed (>= 1 sec)'
+            msg = f'esp serial communication can not be guaranteed ({dt:.2f} s since last call)'
             self.log.error(msg + '; aborting automations')
             await self.pause_automations(because=msg)
         elif dt > 0.1:
-            self.log.warning('esp serial communication is slow (>= 100 ms)')
+            self.log.warning(f'esp serial communication is slow ({dt:.2f} s since last call)')
         self.last_step = self.world.time
