3f19cbe5bc23ab27f9b92bc7b525a10d48dbfd58
Rodja Trappe <rodja@zauberzeug.com>
2022-01-31 06:46:23 +0000

    fixing usb_camera

--- rosys/actors/camera_capture.py
+++ rosys/actors/camera_capture.py
@@ -1,8 +1,10 @@
 import cv2
 import re
 import shutil
+
+from rosys.world.image import ImageSize
 from .actor import Actor
-from ..world.camera import Camera, Image
+from ..world import UsbCamera, Image
 from .. import event
 
 
@@ -17,11 +19,12 @@ class CameraCapture(Actor):
     async def step(self):
         await super().step()
         await self.update_device_list()
-        for uid, camera in self.world.cameras.items():
+        for uid, camera in self.world.usb_cameras.items():
             if not camera.capture:
                 return
             bytes = await self.run_io_bound(self.capture_image, uid)
-            camera.images.append(Image(camera_id=uid, data=bytes, time=self.world.time))
+            camera.images.append(Image(camera_id=uid, data=bytes, time=self.world.time,
+                                 size=ImageSize(width=camera.resolution.width, height=camera.resolution.height)))
         self.purge_old_images()
 
     def capture_image(self, id) -> bytes:
@@ -38,24 +41,28 @@ class CameraCapture(Actor):
             return
         self.last_scan = self.world.time
         output = await self.run_sh(['v4l2-ctl', '--list-devices'])
-        for line in output.splitlines():
-            if 'Cannot open device' in line:
-                break
-            if 'Camera' in line:
-                uid = re.search('\((.*)\)', line).group(1)
-                if uid not in self.world.cameras:
-                    self.world.cameras[uid] = Camera(id=uid)
-                    self.log.info(f'adding camera {uid}')
-                    await event.call(event.Id.NEW_CAMERA, self.world.cameras[uid])
-            if '/dev/video' in line:
-                num = int(line.strip().lstrip('/dev/video'))
-                if uid not in self.devices:
-                    device = self.get_capture_device(num)
-                    if device is None:
-                        if uid in self.world.cameras:
-                            del self.world.cameras[uid]
-                    else:
-                        self.devices[uid] = device
+        for infos in output.split('\n\n'):
+            if 'Cannot open device' in infos:
+                continue
+            match = re.search('\((.*)\)', infos)
+            if match is None:
+                continue
+            uid = match.group(1)
+            if uid not in self.world.cameras:
+                self.world.cameras[uid] = UsbCamera(id=uid)
+                self.log.info(f'adding camera {uid}')
+                await event.call(event.Id.NEW_CAMERA, self.world.cameras[uid])
+            lines = infos.splitlines()
+            num = int(lines[1].strip().lstrip('/dev/video'))
+            if uid not in self.devices:
+                device = self.get_capture_device(num)
+                if device is None:
+                    if uid in self.world.cameras:
+                        del self.world.cameras[uid]
+                else:
+                    self.devices[uid] = device
+            if uid in self.devices:
+                await self.update_parameters(uid, num)
 
     def get_capture_device(self, index: int):
         try:
@@ -75,3 +82,8 @@ class CameraCapture(Actor):
     @staticmethod
     def is_operable() -> bool:
         return shutil.which('v4l2-ctl') is not None
+
+    async def update_parameters(self, uid: str, dev_num: int):
+        output = await self.run_sh(['v4l2-ctl', '--all', '-d', str(dev_num)])
+        size = re.search('Width/Height.*: (\d*)/(\d*)', output)
+        self.world.cameras[uid].resolution = ImageSize(width=int(size.group(1)), height=int(size.group(2)))
--- rosys/world/usb_camera.py
+++ rosys/world/usb_camera.py
@@ -1,8 +1,11 @@
-from typing import Literal
-from .camera import Camera
+from typing import Literal, Optional
+from pydantic import Field
+from rosys.world.image import ImageSize
+from rosys.world.camera import Camera
 
 
 class UsbCamera(Camera):
     type: Literal['usb_camera'] = 'usb_camera'
     capture: bool = True
     detect: bool = False
+    resolution: Optional[ImageSize] = Field(None, exclude=True)
