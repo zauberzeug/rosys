5a10b84bfba705bc288e3b7fddab0ea6d3821919
Falko Schindler <falko@zauberzeug.com>
2022-02-08 11:45:30 +0100

    resolve automator.step method

--- rosys/actors/automator.py
+++ rosys/actors/automator.py
@@ -7,21 +7,16 @@ from . import Actor
 
 
 class Automator(Actor):
-    interval: float = 0.1
-
     def __init__(self, default_automation: Coroutine = None) -> None:
         super().__init__()
         self.automations: list[Automation] = []
         self.default_automation = default_automation
         event.register(event.Id.PAUSE_AUTOMATIONS, self._handle_pause_event)
 
-    async def step(self):
-        if self.automations:
-            await asyncio.gather(*self.automations)
-            self.stop('the last one has completed')
-
     def start(self, coro: Optional[Coroutine] = None):
         self.automations = [Automation(coro or self.default_automation)]
+        [a.stop() for a in self.automations]
+        asyncio.gather(*self.automations)
         self.world.automation_state = AutomationState.RUNNING
 
     def pause(self, because: Optional[str] = None):
