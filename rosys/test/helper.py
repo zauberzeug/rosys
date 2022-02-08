68ec569a34985be82f47f50ddbd98e54ebb84eaf
Rodja Trappe <rodja@zauberzeug.com>
2022-02-08 16:21:02 +0100

    fixed helper

--- rosys/test/helper.py
+++ rosys/test/helper.py
@@ -35,9 +35,7 @@ def assert_point(actual: Union[Point, Point3d], expected: Union[Point, Point3d],
 
 
 async def automate_drive_to(x: float, y: float):
-    global_runtime.automator.replace(drive_to(global_runtime.world, global_runtime.hardware, Point(x=x, y=y)))
-    await global_runtime.automator.step()  # NOTE the step will update the automation_state so we can "resume"
-    await global_runtime.resume()
+    global_runtime.automator.start(drive_to(global_runtime.world, global_runtime.hardware, Point(x=x, y=y)))
 
 
 def approx(o1: Any, o2: Any) -> None:
