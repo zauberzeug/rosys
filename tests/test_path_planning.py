52fb8a171c8c75ef7f29ef0266f872b19ea08bb7
Falko Schindler <falko@zauberzeug.com>
2022-03-16 17:13:11 +0100

    fix tests

--- tests/test_path_planning.py
+++ tests/test_path_planning.py
@@ -7,6 +7,7 @@ from rosys.test import TestRuntime
 
 @pytest.mark.asyncio
 async def test_driving_to_planned_point(runtime: TestRuntime):
+    await runtime.forward(1.0)
     path = await runtime.path_planner.search_async(goal=Pose(x=5, y=2), timeout=3.0)
     runtime.automator.start(drive_path(runtime.world, runtime.hardware, path))
     await runtime.forward(x=5, y=2, tolerance=0.15)
@@ -14,11 +15,13 @@ async def test_driving_to_planned_point(runtime: TestRuntime):
 
 @pytest.mark.asyncio
 async def test_planning_to_problematic_location(runtime: TestRuntime):
+    await runtime.forward(1.0)
     await runtime.path_planner.search_async(goal=Pose(x=2.250, y=1.299, yaw=np.deg2rad(-60.0)), timeout=3.0)
 
 
 @pytest.mark.asyncio
 async def test_not_finding_a_path(runtime: TestRuntime):
+    await runtime.forward(1.0)
     id = 'o1'
     p = Point(x=3, y=0)
     runtime.world.obstacles[id] = Obstacle(id=id, outline=[
