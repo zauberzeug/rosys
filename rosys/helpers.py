c60bb970283e7fc4204bfab85ac3ed8135d7a99f
Falko Schindler <falko@zauberzeug.com>
2022-03-21 12:07:03 +0100

    use perfcounter for performance measurements

--- rosys/helpers.py
+++ rosys/helpers.py
@@ -9,13 +9,13 @@ from contextlib import contextmanager
 def measure(*, reset: bool = False, ms: bool = False):
     global t
     if 't' in globals() and not reset:
-        dt = time.time() - t
+        dt = time.perf_counter() - t
         line = inspect.stack()[1][0].f_lineno
         output = f'{dt * 1000:7.3f} ms' if ms else f'{dt:7.3f} s'
         print(f'{inspect.stack()[1].filename}:{line}', output, flush=True)
     if reset:
         print('------------', flush=True)
-    t = time.time()
+    t = time.perf_counter()
 
 
 def angle(yaw0: float, yaw1: float) -> float:
