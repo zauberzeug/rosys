3a361be96521ad5e144319b47bed4a174b20daaf
Rodja Trappe <falko@zauberzeug.com>
2022-04-02 12:01:28 +0000

    shrinking image needs to run as seperate process

--- rosys/ui/routes.py
+++ rosys/ui/routes.py
@@ -13,29 +13,34 @@ from starlette.routing import Route
 log = logging.getLogger('rosys.routes')
 
 
+def shrink(factor, array) -> bytes:
+    img = cv2.imdecode(array, cv2.IMREAD_COLOR)[::factor, ::factor]
+    jpeg = cv2.imencode('.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 60])[1].tobytes()
+    return jpeg
+
+
 def setup(ui: Ui, runtime: rosys.Runtime):
     ui.add_route(Route('/world', lambda request, **_: Response(content=runtime.world.json(), media_type='text/json')))
     ui.add_route(Route('/export', lambda request, **_: JSONResponse(content=runtime.persistence.dump())))
 
     @lru_cache(maxsize=100)
-    def try_get_jpeg(cam_id: str, timestamp: str, shrink: int) -> Optional[bytes]:
+    async def try_get_jpeg(cam_id: str, timestamp: str, shrink_factor: int) -> Optional[bytes]:
         camera = runtime.world.cameras.get(cam_id)
         if camera is None:
             return
         for image in reversed(camera.images):
             if str(image.time) == timestamp:
                 jpeg = image.data
-                if shrink != 1:
+                if shrink_factor != 1:
                     array = np.frombuffer(image.data, dtype=np.uint8)
-                    img = cv2.imdecode(array, cv2.IMREAD_COLOR)[::shrink, ::shrink]
-                    jpeg = cv2.imencode('.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 60])[1].tobytes()
+                    jpeg = await rosys.run.cpu_bound(shrink, shrink_factor, array)
                 return jpeg
 
-    def get_image(request: Request, **_):
+    async def get_image(request: Request, **_):
         try:
-            jpeg = try_get_jpeg(request.path_params['id'],
-                                request.path_params['timestamp'],
-                                int(request.query_params.get('shrink', 1)))
+            jpeg = await try_get_jpeg(request.path_params['id'],
+                                      request.path_params['timestamp'],
+                                      int(request.query_params.get('shrink', 1)))
             if jpeg:
                 return Response(content=jpeg, headers={'cache-control': 'max-age=7776000'}, media_type='image/jpeg')
             else:
