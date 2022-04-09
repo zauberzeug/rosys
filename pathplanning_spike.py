f34d9cefbd238fb4d08aba01d49b7bc82caee578
Falko Schindler <falko@zauberzeug.com>
2022-04-09 14:43:51 +0200

    pathplanning: improve terminal segments and smoothing

--- pathplanning_spike.py
+++ pathplanning_spike.py
@@ -11,7 +11,7 @@ from scipy import spatial
 from rosys.actors.pathplanning import PlannerProcess
 from rosys.actors.pathplanning import plot_tools as pt
 from rosys.actors.pathplanning.robot_renderer import RobotRenderer
-from rosys.actors.pathplanning.steps import Path, Step
+from rosys.helpers import angle
 from rosys.world import Area, Obstacle, PathSegment, Point, Pose, Spline
 
 robot_outline = [(-0.5, -0.5), (0.5, -0.5), (0.75, 0), (0.5, 0.5), (-0.5, 0.5)]
@@ -50,19 +50,19 @@ def run_old():
 def run_new():
     t = time.time()
     planner.update_obstacle_map(areas, obstacles, [start, goal], deadline=time.time()+10.0)
-    planner.update_distance_map(goal, deadline=time.time()+10.0)
+    obstacle_map = planner.state.obstacle_map
     dt0 = time.time() - t
 
     t = time.time()
 
-    min_x, min_y, size_x, size_y = planner.state.obstacle_map.grid.bbox
+    min_x, min_y, size_x, size_y = obstacle_map.grid.bbox
     points = []
     while True:
         for _ in range(100):
-            planner.state.obstacle_map.grid.bbox
+            obstacle_map.grid.bbox
             point = Point(x=min_x + size_x * np.random.rand(), y=min_y + size_y * np.random.rand())
-            row, col = planner.state.obstacle_map.grid.to_grid(point.x, point.y)
-            if all(planner.state.obstacle_map.stack[int(np.round(row)), int(np.round(col)), :]):
+            row, col = obstacle_map.grid.to_grid(point.x, point.y)
+            if all(obstacle_map.stack[int(np.round(row)), int(np.round(col)), :]):
                 continue
             if any(p.distance(point) < 2.0 for p in points):
                 continue
@@ -104,37 +104,27 @@ def run_new():
         for p, (pose, g_) in enumerate(zip(group.poses, group.neighbor_indices)):
             for p_, pose_ in enumerate(pose_groups[g_].poses):
                 spline = Spline.from_poses(pose, pose_)
-                if not planner.state.obstacle_map.test_spline(spline):
+                if not obstacle_map.test_spline(spline):
                     G.add_edge((g, p), (g_, p_))
 
     dt1 = time.time() - t
 
     t = time.time()
 
-    def find_first_segment() -> tuple[PathSegment, int, int]:
-        start_point = start.point
-        group_distances = [g.point.distance(start_point) for g in pose_groups]
+    def find_terminal_segment(terminal_pose: Pose, first: bool) -> tuple[PathSegment, int, int]:
+        terminal_point = terminal_pose
+        group_distances = [g.point.distance(terminal_point) for g in pose_groups]
         group_indices = np.argsort(group_distances)
         for g, group in zip(group_indices, np.array(pose_groups)[group_indices]):
             for p, pose in enumerate(group.poses):
                 for backward in [False, True]:
-                    spline = Spline.from_poses(start, pose, backward=backward)
-                    if not planner.state.obstacle_map.test_spline(spline):
+                    poses = (terminal_pose, pose) if start else (pose, terminal_pose)
+                    spline = Spline.from_poses(*poses, backward=backward)
+                    if not obstacle_map.test_spline(spline):
                         return PathSegment(spline=spline, backward=backward), g, p
 
-    def find_last_segment() -> tuple[PathSegment, int, int]:
-        goal_point = goal.point
-        group_distances = [g.point.distance(goal_point) for g in pose_groups]
-        group_indices = np.argsort(group_distances)
-        for g, group in zip(group_indices, np.array(pose_groups)[group_indices]):
-            for p, pose in enumerate(group.poses):
-                for backward in [False, True]:
-                    spline = Spline.from_poses(pose, goal, backward=backward)
-                    if not planner.state.obstacle_map.test_spline(spline):
-                        return PathSegment(spline=spline, backward=backward), g, p
-
-    first_segment, g, p = find_first_segment()
-    last_segment, g_, p_ = find_last_segment()
+    first_segment, g, p = find_terminal_segment(start, True)
+    last_segment, g_, p_ = find_terminal_segment(goal, False)
 
     path: list[PathSegment] = []
     path.append(first_segment)
@@ -146,22 +136,47 @@ def run_new():
         last_pose = next_pose
     path.append(last_segment)
 
-    steps = Path()
-    last_step = None
-    for segment in path:
-        pose = segment.spline.pose(1)
-        step = Step((pose.x, pose.y, pose.yaw), last_step)
-        steps.append(step)
-        last_step = step
-    steps.smooth(planner.state.obstacle_map)
-
     dt2 = time.time() - t
+
+    t = time.time()
+
+    def is_healthy(spline: Spline, curvature_limit: float = 10.0):
+        dir_ = spline.start.direction(spline.end)
+        yaw0 = spline.start.direction(spline.control1)
+        yaw1 = spline.control2.direction(spline.end)
+        return \
+            np.abs(angle(dir_, yaw0)) < np.pi / 2 and \
+            np.abs(angle(dir_, yaw1)) < np.pi / 2 and \
+            np.abs(spline.max_curvature()) < curvature_limit  # NOTE: max_curvature can be NaN
+
+    while True:
+        for s in range(1, len(path) - 1):
+            if path[s].backward != path[s+1].backward:
+                continue
+
+            backward = path[s].backward
+            new_spline = Spline.from_poses(path[s].spline.pose(0), path[s+1].spline.pose(1))
+            if not is_healthy(new_spline):
+                continue
+            new_distance = obstacle_map.get_minimum_spline_distance(new_spline, backward=backward)
+            if new_distance == 0:
+                continue
+            old_distance1 = obstacle_map.get_minimum_spline_distance(path[s].spline, backward)
+            old_distance2 = obstacle_map.get_minimum_spline_distance(path[s+1].spline, backward)
+            if new_distance < min(old_distance1, old_distance2):
+                continue
+            path[s] = PathSegment(spline=new_spline, backward=path[s].backward)
+            del path[s+1]
+            break  # restart while loop
+        else:
+            break  # exit while loop
+
+    dt3 = time.time() - t
 
     with plot:
         pl.clf()
-        pl.title(f'{dt0:.3f} s + {dt1:.3f} s + {dt2:.3f} s')
-        pt.show_distance_map(planner.state.distance_map)
-        pt.show_obstacle_map(planner.state.obstacle_map)
+        pl.title(f'map: {dt0:.3f} s, graph: {dt1:.3f} s, path: {dt2:.3f} s, smoothing: {dt3:.3f} s')
+        pt.show_obstacle_map(obstacle_map)
         pl.gca().invert_yaxis()
         pl.autoscale(False)
         pl.triplot(points[:, 0], points[:, 1], tri.simplices, lw=0.1)
@@ -172,7 +187,7 @@ def run_new():
             pl.plot([pose_groups[g].poses[p].x, pose_groups[g_].poses[p_].x],
                     [pose_groups[g].poses[p].y, pose_groups[g_].poses[p_].y], 'C0-', lw=1)
 
-        pt.plot_path(path, 'C0')
+        pt.plot_path(path, 'C1')
         robot_renderer = RobotRenderer(robot_outline)
         for step in path:
             for t in [0, 1]:
