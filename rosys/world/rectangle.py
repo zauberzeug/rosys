cf02980a3edc92381b388c3739c748a5781c114c
Falko Schindler <falko@zauberzeug.com>
2022-04-29 07:39:38 +0200

    code review

--- rosys/world/rectangle.py
+++ rosys/world/rectangle.py
@@ -16,9 +16,8 @@ class Rectangle(BaseModel):
         return (self.x, self.y, self.w, self.h)
 
     def contains(self, point: Point) -> bool:
-        x2: int = self.x + self.width
-        y2: int = self.y + self.height
-        return self.x <= point.x and x2 >= point.x and self.y <= point.y and y2 >= point.y
+        return (self.x <= point.x <= self.x + self.width and
+                self.y <= point.y <= self.y + self.height)
 
     def __str__(self) -> str:
         return f'Rectangle({round(self.x, 2)}, {round(self.y, 2)}, {round(self.w, 2)}, {round(self.h, 2)})'
