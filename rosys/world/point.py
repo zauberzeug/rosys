e342fb68acc4637a6d1910580742c6c4ce5dbef3
Rodja Trappe <rodja@zauberzeug.com>
2022-05-20 16:48:58 +0200

    fixing evil operand error

--- rosys/world/point.py
+++ rosys/world/point.py
@@ -46,7 +46,7 @@ class Point(BaseModel):
         )
 
     def __add__(self, other: Point) -> Point:
-        return Point(x=self.x + other.x, y=self.y * other.y)
+        return Point(x=self.x + other.x, y=self.y + other.y)
 
     def __mul__(self, factor) -> Point:
         return Point(x=self.x * factor, y=self.y * factor)
