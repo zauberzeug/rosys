073aa54ab77c0416e39ef4cd3702a8f87fa31e55
Rodja Trappe <rodja@zauberzeug.com>
2022-05-21 15:22:03 +0200

    removed detector disconnect on timeout -- does not help

--- rosys/actors/detector.py
+++ rosys/actors/detector.py
@@ -113,8 +113,7 @@ class Detector(Actor):
                 point_detections = [PointDetection.parse_obj(d) for d in result.get('point_detections', [])]
                 image.detections = Detections(boxes=box_detections, points=point_detections)
             except socketio.exceptions.TimeoutError:
-                self.log.exception(f'detection for {image.id} took to long; trying to reset connection...')
-                await self.disconnect()
+                self.log.exception(f'detection for {image.id} took to long')
             except:
                 self.log.exception(f'could not detect {image.id}')
             else:
