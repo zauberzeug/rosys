3f067d0175bb872d17798da05dc1c1a0d22e1cf1
Rodja Trappe <rodja@zauberzeug.com>
2022-05-28 17:40:31 +0200

    create cover for timelapse videos

--- rosys/actors/timelapse_recorder.py
+++ rosys/actors/timelapse_recorder.py
@@ -7,6 +7,7 @@ import shutil
 from asyncio import Task
 from datetime import datetime
 from glob import glob
+from pathlib import Path
 
 import rosys
 from PIL import Image, ImageDraw, ImageFont, ImageStat
@@ -25,10 +26,8 @@ class TimelapsRecorder(Actor):
         # NOTE as long as we develop this feature we cleanup on start
         self.clear_jpegs()
         rosys_dir = os.path.dirname(os.path.dirname(__file__))
-        self.font = ImageFont.truetype(f'{rosys_dir}/RobotoMono-Medium.ttf', 12)
-
-    async def step(self):
-        await super().step()
+        self.image_font = ImageFont.truetype(f'{rosys_dir}/RobotoMono-Medium.ttf', 12)
+        self.cover_font = ImageFont.truetype(f'{rosys_dir}/RobotoMono-Medium.ttf', 110)
 
     async def save(self, image: rosys.Image) -> None:
         def _save(image: rosys.world.Image):
@@ -38,24 +37,41 @@ class TimelapsRecorder(Actor):
             y = img.height - 18
             text = f'{datetime.utcfromtimestamp(image.time).strftime("%Y-%m-%d %H:%M:%S")}, cam {image.camera_id}'
             # shadow
-            draw.text((x - 1, y - 1), text, font=self.font, fill=(0, 0, 0))
-            draw.text((x + 1, y - 1), text, font=self.font, fill=(0, 0, 0))
-            draw.text((x - 1, y + 1), text, font=self.font, fill=(0, 0, 0))
-            draw.text((x + 1, y + 1), text, font=self.font, fill=(0, 0, 0))
-            draw.text((x, y), text, font=self.font, fill=(255, 255, 255))
-            dest = self.storage_path + f'/{image.time}.jpg'
+            draw.text((x - 1, y - 1), text, font=self.image_font, fill=(0, 0, 0))
+            draw.text((x + 1, y - 1), text, font=self.image_font, fill=(0, 0, 0))
+            draw.text((x - 1, y + 1), text, font=self.image_font, fill=(0, 0, 0))
+            draw.text((x + 1, y + 1), text, font=self.image_font, fill=(0, 0, 0))
+            draw.text((x, y), text, font=self.image_font, fill=(255, 255, 255))
+            dest = self.storage_path + f'/{int(image.time)}.jpg'
             img.save(dest, "JPEG")
         await rosys.run.io_bound(_save, image)
 
     async def compress_video(self) -> None:
-        now = datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
-        target_dir = self.storage_path + '/' + now
+        jpgs = glob(f'{self.storage_path}/*.jpg')
+        if len(jpgs) < 2:
+            return
+        start = datetime.utcfromtimestamp(int(Path(jpgs[0]).stem))
+        end = datetime.utcfromtimestamp(int(Path(jpgs[-1]).stem))
+        subtitle = f'{start.strftime("%H:%M:%S")} - {end.strftime("%H:%M:%S")}'
+        for i in range(10):
+            self.create_cover(start.strftime("%d.%m.%Y"), subtitle, f'{self.storage_path}/0_cover_{i}.jpg')
+        id = start.strftime('%Y-%m-%d_%H_%M_%S')
+        target_dir = self.storage_path + '/' + id
         os.mkdir(target_dir)
         await rosys.run.sh(['mv', self.storage_path + '/*.jpg', target_dir])
-        cmd = f'nice -n 19 ffmpeg -hide_banner -r 10 -pattern_type glob -i "{target_dir}/*.jpg" -s 1600x1200 -vcodec libx264 -crf 18 -preset slow -pix_fmt yuv420p -y {target_dir}/timelapse_{now}.mp4; mv {target_dir}/*mp4 {self.storage_path}; rm -r {target_dir};'
+        cmd = f'nice -n 19 ffmpeg -hide_banner -r 10 -pattern_type glob -i "{target_dir}/*.jpg" -s 1600x1200 -vcodec libx264 -crf 18 -preset slow -pix_fmt yuv420p -y {target_dir}/timelapse_{id}.mp4; mv {target_dir}/*mp4 {self.storage_path}; rm -r {target_dir};'
         rosys.task_logger.create_task(rosys.run.sh(cmd, timeout=None))
 
     def clear_jpegs(self):
         files = glob(f'{self.storage_path}/**/*.jpg', recursive=True)
         for f in files:
             os.remove(f)
+
+    def create_cover(self, title: str, subtilte: str, dest: str):
+        img = Image.new('RGB', (1600, 1200), 'black')
+        draw = ImageDraw.Draw(img)
+        w, h = draw.textsize(title, font=self.cover_font)
+        draw.text((img.width / 2 - w / 2, img.height / 2 - h / 2 - 200), title, font=self.cover_font, fill='white')
+        w, h = draw.textsize(subtilte, font=self.cover_font)
+        draw.text((img.width / 2 - w / 2, img.height / 2 - h / 2 + 100), subtilte, font=self.cover_font, fill='white')
+        img.save(dest)
