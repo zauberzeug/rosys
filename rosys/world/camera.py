0664beed7e2ebd6fc8accda6a5d64d49b18dc1c5
Rodja Trappe <rodja@zauberzeug.com>
2022-05-28 11:23:39 +0200

    provide recent images getter for cameras

--- rosys/world/camera.py
+++ rosys/world/camera.py
@@ -38,6 +38,9 @@ class Camera(BaseModel, abc.ABC):
         if images:
             return images[-1]
 
+    def get_recent_images(self, current_time: float, timespan: float = 10.0) -> list[Image]:
+        return [i for i in self.captured_images if i.time > current_time - timespan]
+
     def set_perfect_calibration(
         self,
         x: float = 0, y: float = 0, z: float = 1,
