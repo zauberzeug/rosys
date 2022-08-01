9c720c43aab95ce996d58bd92b261655e2e74a8d
Falko Schindler <falko@zauberzeug.com>
2022-08-01 16:24:28 +0200

    introduce import and export buttons for persistence

--- rosys/persistence.py
+++ rosys/persistence.py
@@ -3,11 +3,16 @@ import logging
 import os
 import sys
 from dataclasses import fields
-from typing import Any, Protocol, TypeVar
+from typing import Any, Callable, Optional, Protocol, TypeVar
 
 import numpy as np
 from dataclasses_json import Exclude, config
 from dataclasses_json.core import _asdict, _decode_dataclass
+from nicegui import ui
+from nicegui.events import UploadEventArguments
+from starlette.responses import FileResponse
+
+from .helpers import invoke
 
 exclude = config(exclude=Exclude.ALWAYS)
 
@@ -50,19 +55,19 @@ is_test = 'pytest' in sys.modules
 class PersistentActor(Protocol):
     needs_backup: bool
 
-    def backup(self, data: dict[str, Any]) -> None:
+    def backup(self) -> dict[str, Any]:
         ...
 
-    def restore(self) -> dict[str, Any]:
+    def restore(self, data: dict[str, Any]) -> None:
         ...
 
 
-actors: list[PersistentActor] = []
+actors: dict[str, PersistentActor] = {}
 
 
 def register(actor: PersistentActor) -> None:
     if not is_test:
-        actors.append(actor)
+        actors[actor.__module__] = actor
 
 
 class Encoder(json.JSONEncoder):
@@ -73,22 +78,45 @@ class Encoder(json.JSONEncoder):
 
 
 def backup(force: bool = False) -> None:
-    for actor in actors:
+    for name, actor in actors.items():
         if not actor.needs_backup and not force:
             continue
         if not os.path.exists(backup_path):
             os.makedirs(backup_path)
-        filepath = f'{backup_path}/{actor.__module__}.json'
+        filepath = f'{backup_path}/{name}.json'
         with open(filepath, 'w') as f:
             json.dump(actor.backup(), f, indent=4, cls=Encoder)
         actor.needs_backup = False
 
 
 def restore() -> None:
-    for actor in actors:
-        filepath = f'{backup_path}/{actor.__module__}.json'
+    for name, actor in actors.items():
+        filepath = f'{backup_path}/{name}.json'
         if not os.path.exists(filepath):
             log.warning(f'Backup file "{filepath}" not found.')
             continue
         with open(filepath) as f:
             actor.restore(json.load(f))
+
+
+def export_button(title: str = 'Export', route: str = '/export', tmp_filepath: str = '/tmp/export.json') -> ui.button:
+    @ui.get(route)
+    def get_export() -> FileResponse:
+        with open(tmp_filepath, 'w') as f:
+            json.dump({name: actor.backup() for name, actor in actors.items()}, f, indent=4)
+        return FileResponse(tmp_filepath, filename='export.json')
+    ui.button(title, on_click=lambda e: ui.open(route, e.socket))
+
+
+def import_button(title: str = 'Import', after_import: Optional[Callable] = None) -> ui.button:
+    async def restore_from_file(e: UploadEventArguments) -> None:
+        all_data = json.loads(e.files[0].decode())
+        assert isinstance(all_data, dict)
+        for name, data in all_data.items():
+            actors[name].restore(data)
+        backup(force=True)
+        dialog.close()
+        await invoke(after_import)
+    with ui.dialog() as dialog, ui.card():
+        ui.upload(on_upload=restore_from_file, upload_button_text='Import')
+    return ui.button(title, on_click=dialog.open)
