90522dcd7bf2e4e26991882f0e86b1487aa78de0
Falko Schindler <falko@zauberzeug.com>
2022-08-03 15:50:26 +0200

    tiny fix

--- rosys/driving/__init__.py
+++ rosys/driving/__init__.py
@@ -2,7 +2,7 @@ from .drivable import Drivable
 from .driver import Driver
 from .joystick import Joystick
 from .keyboard_control import KeyboardControl
-from .odometer import Odometer
+from .odometer import Odometer, VelocityProvider
 from .path_segment import PathSegment
 from .robot_object import RobotObject
 from .steerer import Steerer
