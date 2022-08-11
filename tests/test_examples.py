01ffa156674a7b2fa25156e3b6763585f2eefb37
Falko Schindler <falko@zauberzeug.com>
2022-08-11 16:14:34 +0200

    add a main guard to keep pytest out

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -44,20 +44,21 @@ def check(path: str, *, timeout: float = 15.0) -> bool:
     return True
 
 
-success = True
-success &= check('../main.py')
-success &= check('../docs/src/scene_on_click.py')
-success &= check('../docs/src/scene_on_click_with_automation_controls.py')
-success &= check('../docs/src/geofence.py')
-success &= check('../docs/src/path_planning.py')
-success &= check('../docs/src/robot_shape.py')
-success &= check('../docs/src/show_captured_images.py')
-success &= check('../docs/src/remote_operation.py')
-success &= check('../docs/src/logging_config.py')
-success &= check('../docs/src/logging_to_file.py')
-success &= check('../examples/hello_bot/main.py')
-success &= check('../examples/obstacles/main.py')
-success &= check('../rosys/pathplanning/planner_demo.py', timeout=30.0)
+if __name__ == '__main__':
+    success = True
+    success &= check('../main.py')
+    success &= check('../docs/src/scene_on_click.py')
+    success &= check('../docs/src/scene_on_click_with_automation_controls.py')
+    success &= check('../docs/src/geofence.py')
+    success &= check('../docs/src/path_planning.py')
+    success &= check('../docs/src/robot_shape.py')
+    success &= check('../docs/src/show_captured_images.py')
+    success &= check('../docs/src/remote_operation.py')
+    success &= check('../docs/src/logging_config.py')
+    success &= check('../docs/src/logging_to_file.py')
+    success &= check('../examples/hello_bot/main.py')
+    success &= check('../examples/obstacles/main.py')
+    success &= check('../rosys/pathplanning/planner_demo.py', timeout=30.0)
 
-if not success:
-    sys.exit(1)
+    if not success:
+        sys.exit(1)
