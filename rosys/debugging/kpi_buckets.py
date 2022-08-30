d1372c7a3e12d90438ca60bc0fd8ab6c23a77c61
Falko Schindler <falko@zauberzeug.com>
2022-08-30 08:36:36 +0200

    introduce KPIs

---
+++ rosys/debugging/kpi_buckets.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from collections import Counter, defaultdict
+from dataclasses import dataclass, field
+
+
+@dataclass(slots=True, kw_only=True)
+class TimeBucket:
+    date: str
+    incidents: dict[str, int] = field(default_factory=lambda: defaultdict(int))
+
+    @classmethod
+    def from_buckets(cls, sources: list[TimeBucket]) -> TimeBucket:
+        counter = Counter()
+        for source in sources:
+            counter.update(source.incidents)
+        return cls(date=sources[0].date, incidents=defaultdict(int, **counter))
+
+
+@dataclass(slots=True, kw_only=True)
+class Day(TimeBucket):
+    pass
+
+
+@dataclass(slots=True, kw_only=True)
+class Week(TimeBucket):
+    pass
+
+
+@dataclass(slots=True, kw_only=True)
+class Month(TimeBucket):
+    pass
---
+++ rosys/debugging/kpi_logger.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from datetime import date, datetime
+from itertools import groupby
+from typing import Any
+
+import rosys
+from rosys import persistence
+
+from .kpi_buckets import Day, Month
+
+
+def date_to_str(date: date) -> str: return date.isoformat()
+def str_to_date(date: str) -> date: return datetime.fromisoformat(date)
+
+
+class KpiLogger:
+
+    def __init__(self) -> None:
+        self.states: dict[str, bool] = defaultdict(bool)
+
+        self.days: list[Day] = []
+        self.months: list[Month] = []
+
+        self.needs_backup: bool = False
+        persistence.register(self)
+
+    def backup(self) -> dict:
+        self.pack()
+        return {
+            'days': persistence.to_dict(self.days),
+            'months': persistence.to_dict(self.months),
+        }
+
+    def restore(self, data: dict[str, Any]) -> None:
+        persistence.replace_list(self.days, Day, data.get('days', []))
+        persistence.replace_list(self.months, Month, data.get('months', []))
+
+    def pack(self) -> None:
+        '''pack old days into months (as long as there are at least 3 unpacked months)'''
+        while True:
+            month_groups = [list(days) for _, days in groupby(self.days, key=lambda d: d.date[:-3])]
+            if len(month_groups) <= 4:
+                break
+            month = Month.from_buckets(month_groups[0])
+            self.months.append(month)
+            for day in month_groups[0]:
+                self.days.remove(day)
+            self.needs_backup = True
+
+    def today(self) -> Day:
+        date = date_to_str(datetime.utcfromtimestamp(rosys.time()).date())
+        if not self.days or self.days[-1].date != date:
+            self.days.append(Day(date=date))
+        return self.days[-1]
+
+    def increment(self, key: str) -> None:
+        self.today().incidents[key] += 1
+        self.needs_backup = True
+
+    def increment_on_rising_edge(self, key: str, new_state: bool) -> None:
+        if new_state and not self.states[key]:
+            self.increment(key)
+        self.states[key] = new_state
