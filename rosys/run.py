40b261f6431ac39f33f14d78ffa117fd1c6cfb98
Falko Schindler <falko@zauberzeug.com>
2022-10-06 10:04:33 +0200

    use more specific early exits in run.py to fix pytests downstream

--- rosys/run.py
+++ rosys/run.py
@@ -24,7 +24,7 @@ log = logging.getLogger('rosys.run')
 
 
 async def io_bound(callback: Callable, *args: any, **kwargs: any):
-    if nicegui_globals.state != nicegui_globals.State.STARTED:
+    if nicegui_globals.state == nicegui_globals.State.STOPPING:
         return
     loop = asyncio.get_running_loop()
     try:
@@ -45,7 +45,7 @@ def awaitable(func: Callable) -> Callable:
 
 
 async def cpu_bound(callback: Callable, *args: any):
-    if nicegui_globals.state != nicegui_globals.State.STARTED:
+    if nicegui_globals.state == nicegui_globals.State.STOPPING:
         return
     with cpu():
         try:
@@ -101,7 +101,7 @@ async def sh(command: list[str] | str, timeout: Optional[float] = 1, shell: bool
         _kill(proc)
         running_sh_processes.remove(proc)
         return stdout.decode('utf-8')
-    if nicegui_globals.state != nicegui_globals.State.STARTED:
+    if nicegui_globals.state == nicegui_globals.State.STOPPING:
         return
     return await io_bound(popen)
 
