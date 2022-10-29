80f76d5eb19bf6626712512a495d35cbdf284f54
Rodja Trappe <rodja@zauberzeug.com>
2022-10-29 15:23:13 +0200

    Providing an example for custom hardware. Using the Robot Brain is now a follow up to this example.

---
+++ docs/src/example_robot_brain_hardware.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from nicegui import ui
+from rosys.driving import Odometer, Steerer, joystick, keyboard_control
+from rosys.hardware import RobotBrain, SerialCommunication, WheelsHardware, WheelsSimulation, communication
+
+is_real = SerialCommunication.is_possible()
+if is_real:
+    communication = SerialCommunication()
+    robot_brain = RobotBrain(communication)
+    wheels = WheelsHardware(robot_brain)
+else:
+    wheels = WheelsSimulation()
+odometer = Odometer(wheels)
+steerer = Steerer(wheels)
+
+keyboard_control(steerer)
+joystick(steerer)
+
+if is_real:
+    communication.debug_ui()
+    robot_brain.developer_ui()
+
+ui.run(title='RoSys')
