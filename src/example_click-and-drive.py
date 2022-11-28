09dd4d00263109692bb921a08d4c878314300bf4
rodja <rodja@users.noreply.github.com>
2022-11-28 03:39:16 +0000

    Deploying to gh-pages from @ zauberzeug/rosys@52e884326430a8aebdaf79f704d00ae22cd43b47 🚀

--- src/example_click-and-drive.py
+++ src/example_click-and-drive.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 from nicegui import ui
 
-from rosys.automation import Automator, automation_controls
-from rosys.driving import Driver, Odometer, robot_object
-from rosys.geometry import Point, Prism
-from rosys.hardware import WheelsSimulation
+import rosys
 
-shape = Prism.default_robot_shape()
-wheels = WheelsSimulation()
-odometer = Odometer(wheels)
-driver = Driver(wheels, odometer)
-automator = Automator(wheels, None)
+wheels = rosys.hardware.WheelsSimulation()
+odometer = rosys.driving.Odometer(wheels)
+driver = rosys.driving.Driver(wheels, odometer)
+automator = rosys.automation.Automator(wheels, None)
 
 
 async def handle_click(msg):
     for hit in msg.hits:
         if hit.object_id == 'ground':
-            target = Point(x=hit.point.x, y=hit.point.y)
+            target = rosys.geometry.Point(x=hit.point.x, y=hit.point.y)
             automator.start(driver.drive_to(target))
 
 with ui.scene(on_click=handle_click):
-    robot_object(shape, odometer, debug=True)
+    shape = rosys.geometry.Prism.default_robot_shape()
+    rosys.driving.robot_object(shape, odometer, debug=True)
 ui.label('click into the scene to drive the robot')
 with ui.row():
-    automation_controls(automator)
+    rosys.automation.automation_controls(automator)
 ui.label('you can also pause/resume or stop the running automation')
 
 ui.run(title='RoSys')
