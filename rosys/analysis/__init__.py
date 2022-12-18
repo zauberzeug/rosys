bf01b97d6adb4ab1d5ef49d14b86ac1f1eefbe5d
Rodja Trappe <rodja@zauberzeug.com>
2022-12-18 09:43:49 +0100

    allow profiling in docker container again new packages allow compatibility with python 3.11

--- rosys/analysis/__init__.py
+++ rosys/analysis/__init__.py
@@ -4,11 +4,7 @@ from .kpi_logger import KpiLogger, date_to_str, str_to_date
 from .kpi_page_ import KpiPage as kpi_page
 from .legacy.asyncio_monitor import AsyncioMonitor
 from .legacy.network_monitor import NetworkMonitor, NetworkStats
+from .profile_button_ import ProfileButton as profile_button
 from .timelapse_recorder import TimelapseRecorder
 from .track import track
 from .videos_page_ import VideosPage as videos_page
-
-try:
-    from .profile_button_ import ProfileButton as profile_button
-except:
-    pass
