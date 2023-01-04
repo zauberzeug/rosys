6b76aa0d8d0dfade49a80f0585dddf91ba5aa6d8
Falko Schindler <falko@zauberzeug.com>
2023-01-04 14:51:55 +0100

    transform objgraph_page into function

--- rosys/analysis/__init__.py
+++ rosys/analysis/__init__.py
@@ -4,6 +4,7 @@ from .kpi_logger import KpiLogger, date_to_str, str_to_date
 from .kpi_page_ import kpi_page
 from .legacy.asyncio_monitor import AsyncioMonitor
 from .legacy.network_monitor import NetworkMonitor, NetworkStats
+from .legacy.objgraph_page import objgraph_page
 from .memory import MemoryMiddleware
 from .profile_button_ import ProfileButton as profile_button
 from .timelapse_recorder import TimelapseRecorder
--- rosys/analysis/legacy/objgraph_page.py
+++ rosys/analysis/legacy/objgraph_page.py
@@ -15,72 +15,72 @@ log = logging.getLogger('rosys.objgraph_page')
 SVG_FILE = Path('/tmp/rosys_objgraph.svg')
 
 
-class ObjgraphPage:
+def objgraph_page() -> None:
 
-    def __init__(self) -> None:
-        @ui.page('/objgraph')
-        def objgraph_page() -> None:
-            t = ui.timer(10, self.refresh_stats, active=False)
-            ui.switch('track objects (every 10 s)').bind_value_to(t, 'active')
-            with ui.column():
-                self.growth = ui.label()
-                self.leaking = ui.label()
-                self.overall = ui.label()
-                self.counts = ui.markdown()
+    @ui.page('/objgraph')
+    def page() -> None:
+        async def update_graph() -> None:
+            class_search_result.content = await create_graph(class_search.value)
 
-            self.class_search = ui.input('Search Class', value='Client')
-            ui.button('Update Graph', on_click=self.update_graph)
-            self.class_search_result = ui.html()
-            self.most_common_search_result = ui.html()
+        async def refresh_stats() -> None:
+            loop = asyncio.get_running_loop()
+            growth, leaking, overall, counts = await loop.run_in_executor(None, get_objgraph_stats)
+            await update_graph()
+            growth.set_text(growth)
+            leaking.set_text(leaking)
+            overall.set_text(overall)
+            highest = [f'{c[1]}: {c[0]}' for c in counts.most_common()[:5]]
+            counts.set_content('new obj occurrence: ' + '\n'.join(highest))
+            most_common_search_result.content = SVG_FILE.read_text() if counts else ''
 
-    async def update_graph(self) -> None:
+        t = ui.timer(10, refresh_stats, active=False)
+        ui.switch('track objects (every 10 s)').bind_value_to(t, 'active')
+        with ui.column():
+            growth = ui.label()
+            leaking = ui.label()
+            overall = ui.label()
+            counts = ui.markdown()
 
-        @rosys.run.awaitable
-        def search() -> str:
-            gc.collect()
-            objects = objgraph.by_type(self.class_search.value)
-            content = ''
-            for obj in objects:
-                chain = objgraph.find_backref_chain(obj, objgraph.is_proper_module)
-                objgraph.show_chain(chain, filename=str(SVG_FILE))
-                content += SVG_FILE.read_text()
-            return content
+        class_search = ui.input('Search Class', value='Client')
+        ui.button('Update Graph', on_click=update_graph)
+        class_search_result = ui.html()
+        most_common_search_result = ui.html()
 
-        self.class_search_result.content = await search()
 
-    def get_objgraph_stats(self) -> tuple[str, str, str, Optional[Counter[str]]]:
-        gc.collect()
-        growth_objs = objgraph.growth(4)
-        growth = 'object growth every 10 sec: '
-        for obj in growth_objs:
-            growth += f'{obj[0]}: +{obj[2]}  '
-        leaking = 'obj without refs: '
-        for o, c in list(objgraph.typestats(objgraph.get_leaking_objects()).items())[:4]:
-            leaking += f'{o}: {c}  '
-        overall = 'overall obj count: '
-        for o, c in list(objgraph.typestats().items())[:4]:
-            overall += f'{o}: {c}  '
-        if growth_objs:
-            biggest_growth = growth_objs[0][0]
-            new = objgraph.at_addrs(objgraph.get_new_ids()[biggest_growth])
-            counts = Counter(str(i) for i in new)
-            if counts:
-                most_common = counts.most_common()[0][0]
-                unique = {str(i): i for i in new}
-                chain = objgraph.find_backref_chain(unique[most_common], objgraph.is_proper_module)
-                objgraph.show_chain(chain, filename=str(SVG_FILE))
-        else:
-            counts = None
+@rosys.run.awaitable
+def create_graph(search_term: str) -> str:
+    gc.collect()
+    objects = objgraph.by_type(search_term)
+    content = ''
+    for obj in objects:
+        chain = objgraph.find_backref_chain(obj, objgraph.is_proper_module)
+        objgraph.show_chain(chain, filename=str(SVG_FILE))
+        content += SVG_FILE.read_text()
+    return content
 
-        return growth, leaking, overall, counts
 
-    async def refresh_stats(self) -> None:
-        loop = asyncio.get_running_loop()
-        growth, leaking, overall, counts = await loop.run_in_executor(None, self.get_objgraph_stats)
-        await self.update_graph()
-        self.growth.set_text(growth)
-        self.leaking.set_text(leaking)
-        self.overall.set_text(overall)
-        highest = [f'{c[1]}: {c[0]}' for c in counts.most_common()[:5]]
-        self.counts.set_content('new obj occurrence: ' + '\n'.join(highest))
-        self.most_common_search_result.content = SVG_FILE.read_text() if counts else ''
+def get_objgraph_stats() -> tuple[str, str, str, Optional[Counter[str]]]:
+    gc.collect()
+    growth_objs = objgraph.growth(4)
+    growth = 'object growth every 10 sec: '
+    for obj in growth_objs:
+        growth += f'{obj[0]}: +{obj[2]}  '
+    leaking = 'obj without refs: '
+    for o, c in list(objgraph.typestats(objgraph.get_leaking_objects()).items())[:4]:
+        leaking += f'{o}: {c}  '
+    overall = 'overall obj count: '
+    for o, c in list(objgraph.typestats().items())[:4]:
+        overall += f'{o}: {c}  '
+    if growth_objs:
+        biggest_growth = growth_objs[0][0]
+        new = objgraph.at_addrs(objgraph.get_new_ids()[biggest_growth])
+        counts = Counter(str(i) for i in new)
+        if counts:
+            most_common = counts.most_common()[0][0]
+            unique = {str(i): i for i in new}
+            chain = objgraph.find_backref_chain(unique[most_common], objgraph.is_proper_module)
+            objgraph.show_chain(chain, filename=str(SVG_FILE))
+    else:
+        counts = None
+
+    return growth, leaking, overall, counts
