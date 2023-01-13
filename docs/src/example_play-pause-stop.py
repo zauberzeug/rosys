9558e848537305fc7910efe76c647c6be79a12f9
Rodja Trappe <rodja@zauberzeug.com>
2023-01-13 09:22:41 +0100

    cleanup: using more general "import rosys"

--- docs/src/example_play-pause-stop.py
+++ docs/src/example_play-pause-stop.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 from nicegui import ui
 
-from rosys.automation import Automator, automation_controls
-from rosys.driving import Driver, Odometer, robot_object
-from rosys.geometry import Point, Prism
-from rosys.hardware import WheelsSimulation
+import rosys
+from rosys.geometry import Point
 
 
 async def run() -> None:
@@ -12,16 +10,17 @@ async def run() -> None:
         await driver.drive_to(c)
 
 checkpoints: list[Point] = [Point(x=-3, y=1), Point(x=3, y=3), Point(x=2, y=-2)]
-wheels = WheelsSimulation()
-odometer = Odometer(wheels)
-driver = Driver(wheels, odometer)
-automator = Automator(wheels, None, default_automation=run)
+wheels = rosys.hardware.WheelsSimulation()
+odometer = rosys.driving.Odometer(wheels)
+driver = rosys.driving.Driver(wheels, odometer)
+automator = rosys.automation.Automator(wheels, None, default_automation=run)
 
 with ui.scene(width=600).classes('drop-shadow-lg') as scene:
-    robot_object(Prism.default_robot_shape(), odometer)
+    rosys.driving.robot_object(rosys.geometry.Prism.default_robot_shape(), odometer)
     for i, point in enumerate(checkpoints):
         scene.text(f'{i+1}').move(x=point.x, y=point.y)
+
 with ui.row():
-    automation_controls(automator)
+    rosys.automation.automation_controls(automator)
 
 ui.run(title='RoSys')
