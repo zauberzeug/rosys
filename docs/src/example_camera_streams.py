29ad1b28df17b831f3cee38e62348d117a46f0ba
Rodja Trappe <rodja@zauberzeug.com>
2023-01-23 05:34:05 +0100

    cleanup

--- docs/src/example_camera_streams.py
+++ docs/src/example_camera_streams.py
@@ -11,29 +11,21 @@ from nicegui import ui
 
 import rosys
 
-logging.basicConfig(level=logging.INFO)
-
-
-icecream.install()
 camera_provider = rosys.vision.RtspCameraProviderHardware()
 
 
 def refresh() -> None:
     for uid, camera in camera_provider.cameras.items():
-        if uid not in feeds:
-            with cameras:
+        if uid not in streams:
+            with camera_grid:
                 with ui.card().tight():
-                    feeds[uid] = ui.interactive_image()
+                    streams[uid] = ui.interactive_image()
                     ui.label(uid).classes('m-2')
-                ic('creating new feed', uid)
-        url = camera_provider.get_latest_image_url(camera)
-        feeds[uid].set_source(url)
+        streams[uid].set_source(camera_provider.get_latest_image_url(camera))
 
 
-feeds = {}
-warnings = rosys.analysis.asyncio_warnings.AsyncioWarnings()
-cameras = ui.row()
+streams = {}
+camera_grid = ui.row()
 ui.timer(0.01, refresh)
-rosys.on_startup(warnings.activate)
 
 ui.run(title='RoSys')
