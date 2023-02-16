e7c94829c66c668bc03bb3e984256d52e7ada00c
Miguel <angelom93@web.de>
2023-02-16 16:41:49 +0100

    added expander prefix to can module

--- rosys/hardware/can.py
+++ rosys/hardware/can.py
@@ -1,3 +1,4 @@
+from .expander import ExpanderHardware
 from .module import ModuleHardware
 from .robot_brain import RobotBrain
 
@@ -5,10 +6,11 @@ from .robot_brain import RobotBrain
 class CanHardware(ModuleHardware):
 
     def __init__(self, robot_brain: RobotBrain, *,
+                 expander: ExpanderHardware = None,
                  name: str = 'can',
                  rx_pin: int = 32,
                  tx_pin: int = 33,
                  baud: int = 1_000_000) -> None:
         self.name = name
-        lizard_code = f'{name} = Can({rx_pin}, {tx_pin}, {baud})'
+        lizard_code = f'{name} = {expander.name + "." if expander != None else ""}Can({rx_pin}, {tx_pin}, {baud})'
         super.__init__(robot_brain=robot_brain, lizard_code=lizard_code)
