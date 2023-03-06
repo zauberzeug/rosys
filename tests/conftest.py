7e6fa9adc5cacc392962a08ef95b7368d87ef881
Falko Schindler <falko@zauberzeug.com>
2023-03-06 13:44:16 +0100

    fix pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,7 +51,7 @@ async def driver(wheels: Wheels, odometer: Odometer, integration: None) -> Drive
 
 @pytest.fixture
 async def automator(wheels: Wheels, integration: None) -> Automator:
-    helpers.automator = Automator(wheels, None)
+    helpers.automator = Automator(None, on_interrupt=wheels.stop)
     return helpers.automator
 
 
