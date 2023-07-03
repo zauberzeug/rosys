e6897b2196e5371092087ca0e215ab9233a68088
Miguel <angelom93@web.de>
2023-07-03 13:22:04 +0200

    added battery control and imu hardware

---
+++ rosys/hardware/imu.py
@@ -0,0 +1,9 @@
+from .module import ModuleHardware
+from .robot_brain import RobotBrain
+
+
+class ImuHardware(ModuleHardware):
+
+    def __init__(self, robot_brain: RobotBrain) -> None:
+        lizard_code = f'imu = Imu()'
+        super().__init__(robot_brain, lizard_code=lizard_code)
