7a05a1b6ea72a61c24038f0b5c708a9c1056bd5d
Falko Schindler <falko@zauberzeug.com>
2023-08-21 16:42:35 +0200

    allow parallelizing coroutines with the automator

--- rosys/automation/__init__.py
+++ rosys/automation/__init__.py
@@ -2,4 +2,5 @@ from .app_controls_ import AppButton
 from .app_controls_ import AppControls as app_controls
 from .automation_controls_ import AutomationControls as automation_controls
 from .automator import Automator
+from .parallelize import parallelize
 from .schedule import Schedule
