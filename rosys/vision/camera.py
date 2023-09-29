85290ed11de1e5be12528c1b04a6b1a334d32ad5
NiklasNeugebauer <niklas@neugebauer.dev>
2023-09-29 09:01:56 +0200

    make is_connected non-abstract

--- rosys/vision/camera.py
+++ rosys/vision/camera.py
@@ -26,11 +26,11 @@ class Camera(abc.ABC):
             self.name = self.id
 
     @property
-    @abc.abstractmethod
     def is_connected(self) -> bool:
         '''
         to be interpreted as "ready to capture images"
         '''
+        return True
 
     @property
     def captured_images(self) -> list[Image]:
