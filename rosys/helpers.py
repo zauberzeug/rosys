7f13b6f61f1bd731b2c40738f9c348de95fbbec8
Falko Schindler <falko@zauberzeug.com>
2023-10-11 15:33:16 +0200

    move PackagePathFilter into helpers module

--- rosys/helpers.py
+++ rosys/helpers.py
@@ -1,8 +1,12 @@
+import functools
 import inspect
+import logging
+import logging.config
+import os
 import sys
 import time
 from contextlib import contextmanager
-from typing import Any, Awaitable, Callable, Generator
+from typing import Any, Awaitable, Callable, Generator, Optional
 
 import numpy as np
 from nicegui import globals as nicegui_globals
@@ -94,3 +98,19 @@ def is_stopping() -> bool:
 
 def is_test() -> bool:
     return 'pytest' in sys.modules
+
+
+class PackagePathFilter(logging.Filter):
+    ABS_SYS_PATHS = sorted((os.path.abspath(p) + os.sep for p in sys.path), key=len, reverse=True)
+
+    # https://stackoverflow.com/a/52582536/3419103
+    @functools.lru_cache(maxsize=100)
+    def find_relative_path(self, pathname: str) -> Optional[str]:
+        for path in self.ABS_SYS_PATHS:
+            if pathname.startswith(path):
+                return os.path.relpath(pathname, path)
+        return None
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        record.relative_path = self.find_relative_path(record.pathname)
+        return True
