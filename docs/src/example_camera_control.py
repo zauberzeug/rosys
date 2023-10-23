c335c5599a0467ec567c404931d2614e8ec0a3c9
NiklasNeugebauer <niklas@neugebauer.dev>
2023-10-23 16:00:32 +0200

    move code and correct parameter display

--- docs/src/example_camera_control.py
+++ docs/src/example_camera_control.py
@@ -4,9 +4,23 @@ from nicegui import ui
 import rosys
 from rosys.vision import Camera, ConfigurableCameraMixin, RtspCamera
 
-rtsp_camera_provider = rosys.vision.RtspCameraProvider()
-usb_camera_provider = rosys.vision.UsbCameraProvider()
-simulated_camera_provider = rosys.vision.SimulatedCameraProvider()
+
+def add_card(camera: Camera) -> None:
+    uid = camera.id
+    camera_card = ui.card().tight()
+    camera_cards[uid] = camera_card
+    if uid not in streams:
+        with camera_grid:
+            with camera_card:
+                streams[uid] = ui.interactive_image()
+                ui.label(uid).classes('m-2')
+                with ui.row():
+                    ui.switch('stream').bind_value(camera, 'streaming')
+                    ui.button('capture', on_click=camera.capture_image)
+                if isinstance(camera, ConfigurableCameraMixin):
+                    create_camera_settings_panel(camera)
+
+    streams[uid].set_source(camera.get_latest_image_url())
 
 
 def create_camera_settings_panel(camera: ConfigurableCameraMixin) -> None:
@@ -20,58 +34,50 @@ def create_camera_settings_panel(camera: ConfigurableCameraMixin) -> None:
             with ui.card().classes():
                 with ui.row():
                     ui.label("FPS: ").classes('text-s')
-                    ui.select(options=list(range(1, 31)), on_change=lambda event: camera.update_parameters({'fps': event.value})).classes(
-                        'text-s').bind_value_from(camera, backward=lambda cam: cam.get_parameters()['fps'])
+                    ui.select(options=list(range(1, 31)), on_change=lambda event: camera.set_parameters({'fps': event.value})).classes(
+                        'text-s').bind_value_from(camera, 'parameters', backward=lambda params: params['fps'])
         if 'jovision_profile' in parameter_names:
             with ui.card().classes():
                 with ui.row():
-                    ui.switch('High Quality', on_change=lambda event: camera.update_parameters(
+                    ui.switch('High Quality', on_change=lambda event: camera.set_parameters(
                         {'jovision_profile': 0 if event.value else 1})).classes('text-s')
         if 'exposure' in parameter_names:
             with ui.card().classes():
                 with ui.row():
                     ui.label("Exposure: ").classes('text-s')
-                    ui.select(options=list(range(0, 256)), on_change=lambda event: camera.update_parameters({'exposure': event.value})).classes(
-                        'text-s').bind_value_from(camera, backward=lambda cam: cam.get_parameters()['exposure'])
+                    ui.select(options=list(range(0, 255)), on_change=lambda event: camera.set_parameters({'exposure': event.value})).classes(
+                        'text-s').bind_value_from(camera, 'parameters', backward=lambda params: params['exposure'])
+
         if 'auto_exposure' in parameter_names:
             with ui.card().classes():
                 with ui.row():
-                    ui.switch('Auto Exposure', on_change=lambda event: camera.update_parameters(
-                        {'auto_exposure': event.value})).classes('text-s')
+                    ui.switch('Auto Exposure', on_change=lambda event: camera.set_parameters(
+                        {'auto_exposure': event.value})).bind_value_from(camera, 'parameters', backward=lambda params: params['auto_exposure']).classes('text-s')
         if 'color' in parameter_names:
             with ui.card().classes():
                 with ui.row():
                     ui.label("Color: ").classes('text-s')
                     with ui.button(icon='colorize'):
-                        ui.color_picker(on_pick=lambda event: camera.update_parameters(
+                        ui.color_picker(on_pick=lambda event: camera.set_parameters(
                             {'color': event.color})).classes('text-s')
 
 
-def create_ui(camera_provider):
-    for uid, camera in camera_provider.cameras.items():
-        assert isinstance(camera, Camera)
-        if uid not in streams:
-            with camera_grid:
-                with ui.card().tight():
-                    streams[uid] = ui.interactive_image()
-                    ui.label(uid).classes('m-2')
-                    with ui.row():
-                        ui.switch('stream').bind_value(camera, 'streaming')
-                        ui.button('capture', on_click=camera.capture_image)
-                    if isinstance(camera, ConfigurableCameraMixin):
-                        create_camera_settings_panel(camera)
+def update_camera_cards():
 
-        streams[uid].set_source(camera.get_latest_image_url())
-
-
-def refresh() -> None:
     for provider in [rtsp_camera_provider, usb_camera_provider, simulated_camera_provider]:
-        create_ui(provider)
+        for camera in provider.cameras.values():
+            add_card(camera)
 
 
 streams: dict[str, ui.interactive_image] = {}
+camera_cards: dict[str, ui.card] = {}
 camera_grid = ui.row()
-ui.timer(0.01, refresh)
+
+rtsp_camera_provider = rosys.vision.RtspCameraProvider()
+usb_camera_provider = rosys.vision.UsbCameraProvider()
+simulated_camera_provider = rosys.vision.SimulatedCameraProvider()
+
+ui.timer(.01, update_camera_cards)
 
 simulated_camera_provider.add_cameras(1)
 
