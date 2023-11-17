56461eaf4dfe127d30785d814f5ff9311202b9e2
Falko Schindler <falko@zauberzeug.com>
2023-11-17 11:59:03 +0100

    let @track decorator preserve signature

--- rosys/analysis/track.py
+++ rosys/analysis/track.py
@@ -1,6 +1,10 @@
 from functools import wraps
+from typing import Callable, ParamSpec, TypeVar
 
 from nicegui import ui
+
+_T = TypeVar('_T')
+_P = ParamSpec('_P')
 
 
 class Track:
@@ -8,12 +12,12 @@ class Track:
     def __init__(self) -> None:
         self.stack: list[str] = []
 
-    def __call__(self, f):
+    def __call__(self, f: Callable[_P, _T]) -> Callable[_P, _T]:
         @wraps(f)
-        async def wrap(*args, **kw):
+        async def wrap(*args, **kwargs):
             try:
                 self.stack.append(f.__name__)
-                return await f(*args, **kw)
+                return await f(*args, **kwargs)
             finally:
                 if self.stack and self.stack[-1] == f.__name__:
                     self.stack.pop()
