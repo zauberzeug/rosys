34f7092fa48d9c434166eca66f2bc87e7d17f4ad
Rodja Trappe <rodja@zauberzeug.com>
2023-12-11 06:01:17 +0100

    improved videos page

--- rosys/analysis/videos_page_.py
+++ rosys/analysis/videos_page_.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-from nicegui import app, ui
-from starlette import responses
+from nicegui import background_tasks, ui
+from watchfiles import awatch
 
-PATH = Path('~/.rosys/timelapse/videos').expanduser()
+VIDEO_FILES = Path('~/.rosys/timelapse/videos').expanduser()
 
 
 class VideosPage:
@@ -11,14 +11,17 @@ class VideosPage:
     def __init__(self) -> None:
         @ui.page('/videos', title='Videos')
         def page():
-            def update_list() -> None:
-                video_list.clear()
-                with video_list:
-                    for mp4 in sorted(PATH.glob('*.mp4'), reverse=True):
-                        ui.link(mp4.stem.replace('_', ' ').replace('-', ':'), f'/timelapse/{mp4.name}')
-            video_list = ui.column()
-            ui.timer(5, update_list)
 
-        @app.get('/timelapse/{filename}')
-        def get_timelapse(filename: str) -> responses.FileResponse:
-            return responses.FileResponse(PATH / filename)
+            @ui.refreshable
+            def videos():
+                for mp4 in sorted(VIDEO_FILES.glob('*.mp4'), reverse=True):
+                    with ui.row():
+                        ui.video(mp4).classes('w-[800px]')
+                        ui.button(on_click=lambda mp4=mp4: mp4.unlink()).props('icon=delete flat')
+
+            async def watch_videos():
+                async for _ in awatch(VIDEO_FILES):
+                    videos.refresh()
+
+            videos()
+            background_tasks.create(watch_videos(), name='watch videos files')
