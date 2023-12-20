0315b700c6e856da670c7d00f0fd490dd734e51a
Niklas Neugebauer <niklas@zauberzeug.com>
2023-12-20 16:42:43 +0100

    use cv2.imencode to convert back to jpeg

--- rosys/vision/image_processing.py
+++ rosys/vision/image_processing.py
@@ -35,9 +35,7 @@ def process_jpeg_image(data: bytes, rotation: ImageRotation, crop: Optional[Rect
     if crop is not None:
         image = image.crop((int(crop.x), int(crop.y), int(crop.x + crop.width), int(crop.y + crop.height)))
     image = image.rotate(int(rotation), expand=True)  # NOTE: PIL handles rotation with 90 degree steps efficiently
-    img_byte_arr = io.BytesIO()
-    image.save(img_byte_arr, format='JPEG')
-    return img_byte_arr.getvalue()
+    return cv2.imencode('.jpg', np.array(image))[1].tobytes()
 
 
 def process_ndarray_image(image: np.ndarray, rotation: ImageRotation, crop: Optional[Rectangle] = None) -> bytes:
