3b876e47b3551028b6caf074d32da2474791cfb7
Falko Schindler <falko@zauberzeug.com>
2024-01-05 16:37:04 +0100

    code review

--- rosys/analysis/videos_page_.py
+++ rosys/analysis/videos_page_.py
@@ -11,18 +11,16 @@ class VideosPage:
     def __init__(self) -> None:
         @ui.page('/videos', title='Videos')
         async def page():
-
             @ui.refreshable
             async def videos():
                 for mp4 in sorted(await run.io_bound(VIDEO_FILES.glob, '*.mp4'), reverse=True):
                     with ui.row():
                         with ui.card().tight():
                             video = ui.video(mp4).classes('w-[800px]')
+                            src = video._props['src']  # pylint: disable=protected-access
                         with ui.column():
-                            ui.button(on_click=lambda mp4=mp4: mp4.unlink()) \
-                                .props('icon=delete flat')
-                            ui.button(on_click=lambda mp4=mp4: ui.download(video._props['src'])) \
-                                .props('icon=download flat')
+                            ui.button(on_click=lambda mp4=mp4: mp4.unlink()).props('icon=delete flat')
+                            ui.button(on_click=lambda src=src: ui.download(src)).props('icon=download flat')
 
             async def watch_videos():
                 async for _ in watchfiles.awatch(VIDEO_FILES):
