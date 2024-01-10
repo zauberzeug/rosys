5c14da7d979b86465b0a5736709aceb447c244e9
Niklas Neugebauer <niklas@zauberzeug.com>
2024-01-10 16:39:53 +0100

    add mjpeg classes to module inits

--- rosys/vision/__init__.py
+++ rosys/vision/__init__.py
@@ -8,6 +8,7 @@ from .detector import Autoupload, Detector
 from .detector_hardware import DetectorHardware
 from .detector_simulation import DetectorSimulation, SimulatedObject
 from .image import Image, ImageSize
+from .mjpeg_camera import MjpegCamera, MjpegCameraProvider
 from .mjpeg_camera_provider_hardware import MJpegCameraProviderHardware
 from .multi_camera_provider import MultiCameraProvider
 from .rtsp_camera import RtspCamera, RtspCameraProvider
---
+++ rosys/vision/mjpeg_camera/__init__.py
@@ -0,0 +1,2 @@
+from .mjpeg_camera import MjpegCamera
+from .mjpeg_camera_provider import MjpegCameraProvider
