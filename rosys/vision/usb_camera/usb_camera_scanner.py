24b180d345ab3f96af206dc089884da93357abc8
Niklas Neugebauer <niklas@zauberzeug.com>
2024-01-18 13:23:42 +0100

    add case to only use devname if all else fails

--- rosys/vision/usb_camera/usb_camera_scanner.py
+++ rosys/vision/usb_camera/usb_camera_scanner.py
@@ -9,7 +9,7 @@ def uid_from_device(device: pyudev.Device) -> Optional[str]:
         device.get('ID_MODEL_ID'),
         device.get('ID_SERIAL_SHORT') or device.get('ID_PATH') or device.get('DEVNAME'),
     ]
-    return '-'.join(parts) if all(parts) else None
+    return '-'.join(parts) if all(parts) else device.get('DEVNAME') or None
 
 
 def scan_for_connected_devices() -> set[str]:
