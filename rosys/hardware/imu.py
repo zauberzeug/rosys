3815b7f5bbd91dc7cd14a63233d59459ca970fb8
Falko Schindler <falko@zauberzeug.com>
2024-02-09 15:41:36 +0100

    code review

--- rosys/hardware/imu.py
+++ rosys/hardware/imu.py
@@ -1,82 +1,66 @@
-from .module import ModuleHardware,Module,ModuleSimulation
-from .robot_brain import RobotBrain
-from dataclasses import dataclass
+from typing import Optional
+
 from ..event import Event
+from ..geometry import Rotation
+from .module import Module, ModuleHardware, ModuleSimulation
+from .robot_brain import RobotBrain
 
 
-@dataclass(slots=True, kw_only=True)
-class Eulerangle:
-    roll: float
-    pitch: float
-    yaw: float
+class Imu(Module):
 
-class IMU(Module):
-    def __init__(self, **kwargs) -> None:
-        self.ROBOT_ANGLES = Event()
+    def __init__(self, rotation_offset: Rotation, **kwargs) -> None:
         super().__init__(**kwargs)
 
-class ImuHardware(IMU,ModuleHardware):
+        self.rotation_offset = rotation_offset
+        self.rotation: Optional[Rotation] = None
+        self.gyro_calibration: float = 0.0
 
-    def __init__(self, robot_brain: RobotBrain,pitch_offset: float , roll_offset: float,name: str = 'imu',**kwargs) -> None:
+        self.NEW_MEASUREMENT = Event()
+        """a new measurement has been received (argument: rotation)"""
+
+    @property
+    def roll(self) -> Optional[float]:
+        return self.rotation.roll if self.rotation else None
+
+    @property
+    def pitch(self) -> Optional[float]:
+        return self.rotation.pitch if self.rotation else None
+
+    @property
+    def yaw(self) -> Optional[float]:
+        return self.rotation.yaw if self.rotation else None
+
+    def emit_measurement(self) -> None:
+        self.NEW_MEASUREMENT.emit(self.rotation)
+
+
+class ImuHardware(Imu, ModuleHardware):
+
+    def __init__(self, robot_brain: RobotBrain, name: str = 'imu', **kwargs) -> None:
         self.name = name
-        self.pitch_offset = pitch_offset
-        self.roll_offset = roll_offset
-        self.yaw = 0
-        self.pitch = 0
-        self.roll = 0
-        self.cal_gyro = 0
         self.lizard_code = f'{name} = Imu()'
-        self.core_message_fields = [f'{name}.roll',
-                                f'{name}.pitch',
-                                f'{name}.yaw',
-                                f'{name}.cal_gyro',
-                                ]
-        super().__init__(robot_brain=robot_brain,lizard_code=self.lizard_code, core_message_fields=self.core_message_fields,**kwargs)
+        self.core_message_fields = [
+            f'{name}.roll',
+            f'{name}.pitch',
+            f'{name}.yaw',
+            f'{name}.cal_gyro',
+        ]
+        super().__init__(robot_brain=robot_brain, lizard_code=self.lizard_code, core_message_fields=self.core_message_fields, **kwargs)
 
     def handle_core_output(self, time: float, words: list[str]) -> None:
-        self.roll = float(words.pop(0))
-        self.pitch = float(words.pop(0))
-        self.yaw = float(words.pop(0))
-        self.cal_gyro = float(words.pop(0))
+        roll = float(words.pop(0))
+        pitch = float(words.pop(0))
+        yaw = float(words.pop(0))
+        self.gyro_calibration = float(words.pop(0))
+        if self.gyro_calibration < 1.0:
+            return
 
-        self.offset_correction()
-            
-        angles = Eulerangle(
-            roll = self.roll,
-            pitch = self.pitch,
-            yaw = self.yaw)
-            
-        if self.cal_gyro > 1.0:
-            self.ROBOT_ANGLES.emit(angles)
-    
-    def offset_correction(self)->None:
-        self.roll = self.roll + self.roll_offset
-        self.pitch = self.pitch + self.pitch_offset
+        self.rotation = self.rotation_offset * Rotation.from_euler(roll, pitch, yaw)
+        self.emit_measurement()
 
-class IMUSimulation(IMU,ModuleSimulation):
 
-    def __init__(self, *,
-                 name: str = 'imu') -> None:
-        self.name = name
-        self.yaw = 0
-        self.pitch = 0
-        self.roll = 0
-        self.cal_gyro = 3
-        super().__init__(**kwargs)
-    
-    async def set_euler_angles(self, yaw : float, pitch : float, roll : float):
-        self.yaw = yaw
-        self.pitch = pitch
-        self.roll = roll
-        angles = Eulerangle(
-            roll = self.roll,
-            pitch = self.pitch,
-            yaw = self.yaw)
-        self.ROBOT_ANGLES.emit(angles)
-    
-    async def testemit(self) -> None:
-        angles = Eulerangle(
-            roll = self.roll,
-            pitch = self.pitch,
-            yaw = self.yaw)
-        self.ROBOT_ANGLES.emit(angles)
+class ImuSimulation(Imu, ModuleSimulation):
+
+    def set_rotation(self, rotation: Rotation) -> None:
+        self.rotation = self.rotation_offset * rotation
+        self.emit_measurement()
