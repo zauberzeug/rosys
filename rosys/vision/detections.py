64d591b01ab6f7a5d8aad2fac356f635798452b6
Rodja Trappe <rodja@zauberzeug.com>
2024-02-10 05:53:31 +0100

    compute iou and distance for detections

--- rosys/vision/detections.py
+++ rosys/vision/detections.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from dataclasses import asdict, dataclass, field
 
+import numpy as np
+
 from ..geometry import Point
 
 
@@ -50,6 +52,23 @@ class BoxDetection(Detection):
         return f'<rect x="{x}" y="{y}" width="{self.width/shrink}" height="{self.height/shrink}" stroke-width="2" stroke="{color}" fill="none" />' \
             f'<text x="{x}" y="{y - 7}" text-anchor="start" stroke="{color}" fill="{color}" font-size="10">{self.category_name} ({int(self.confidence*100)}%)</text>'
 
+    def intersection_over_union(self, other_detection: 'BoxDetection') -> float:
+        # https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+        xA = max(self.x, other_detection.x)
+        yA = max(self.y, other_detection.y)
+        xB = min(self.x + self.width, other_detection.x + other_detection.width)
+        yB = min(self.y + self.height, other_detection.y +
+                 other_detection.height)
+        interArea = max(xB - xA, 0) * max(yB - yA, 0)
+        union = float(self._get_area() + other_detection._get_area() - interArea)  # pylint: disable=protected-access
+        if union == 0:
+            print("WARNING: something went wrong while calculating iou")
+            return 0
+        return interArea / union
+
+    def _get_area(self) -> float:
+        return self.width * self.height
+
 
 @dataclass(slots=True, kw_only=True)
 class Shape:
@@ -93,6 +112,9 @@ class PointDetection(Detection):
     @property
     def cy(self) -> float:
         return self.y
+
+    def distance(self, other: 'PointDetection') -> float:
+        return np.sqrt((other.x - self.x)**2 + (other.y - self.y)**2)
 
     def __str__(self) -> str:
         return f'x:{self.x:.0f} y:{self.y:.0f} cat:{self.category_name} conf:{self.confidence:.2f}'
