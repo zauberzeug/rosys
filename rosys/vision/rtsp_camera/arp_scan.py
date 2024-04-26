e5805d244b012b261f9f4d94acf3338f5e9108dd
pascalzauberzeug <pascal@zauberzeug.com>
2024-04-26 19:37:10 +0200

    filter unwanted lines in arp-scan

--- rosys/vision/rtsp_camera/arp_scan.py
+++ rosys/vision/rtsp_camera/arp_scan.py
@@ -34,6 +34,8 @@ async def find_cameras() -> AsyncIterator[tuple[str, str]]:
             continue
         mac = infos[1]
         ip = infos[0]
+        if mac in ['packets', 'arp-scan'] or ip == "Interface:":
+            continue
         yield mac, ip
 
 
