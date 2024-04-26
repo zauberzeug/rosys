77a0491b1b3d435db34d107c1628893992903430
Niklas Neugebauer <niklas@zauberzeug.com>
2024-04-26 14:49:09 +0200

    add fisheye calibration tests

--- tests/test_camera_calibration.py
+++ tests/test_camera_calibration.py
@@ -17,6 +17,20 @@ def demo_data() -> tuple[CalibratableCamera, list[Point3d]]:
     return cam, world_points
 
 
+def demo_fisheye_data() -> tuple[CalibratableCamera, list[Point3d]]:
+    cam = CalibratableCamera(id='1')
+    cam.set_perfect_calibration(x=0.1, y=0.2, z=3, roll=np.deg2rad(180+10), pitch=np.deg2rad(20), yaw=np.deg2rad(30))
+    cam.calibration.intrinsics.distortion = [0.1, 0.4, -0.5, 0.2]
+    cam.calibration.intrinsics.fisheye = True
+    world_points = [
+        Point3d(x=x, y=y, z=z)
+        for x in [-1.0, 0.0, 1.0]
+        for y in [-1.0, 0.0, 1.0]
+        for z in [-1.0, 0.0, 1.0]
+    ]
+    return cam, world_points
+
+
 def test_calibration_from_points():
     cam, world_points = demo_data()
     image_size = cam.calibration.intrinsics.size
@@ -32,8 +46,32 @@ def test_calibration_from_points():
     approx(calibration.extrinsics.rotation.R, cam.calibration.extrinsics.rotation.R, abs=1e-6)
 
 
+def test_fisheye_calibration_from_points():
+    cam, world_points = demo_fisheye_data()
+    image_size = cam.calibration.intrinsics.size
+
+    image_points = [cam.calibration.project_to_image(p) for p in world_points]
+    assert not any(p is None for p in image_points)
+    focal_length = cam.calibration.intrinsics.matrix[0][0]
+    calibration = Calibration.from_points(world_points, image_points, image_size, focal_length, fisheye=True)
+
+    approx(calibration.intrinsics.matrix, cam.calibration.intrinsics.matrix)
+    approx(calibration.intrinsics.rotation.R, cam.calibration.intrinsics.rotation.R)
+    approx(calibration.extrinsics.translation, cam.calibration.extrinsics.translation)
+    approx(calibration.extrinsics.rotation.R, cam.calibration.extrinsics.rotation.R, abs=1e-6)
+
+
 def test_projection():
     cam, world_points = demo_data()
+    for world_point in world_points:
+        image_point = cam.calibration.project_to_image(world_point)
+        assert image_point is not None
+        world_point_ = cam.calibration.project_from_image(image_point, target_height=world_point.z)
+        assert np.allclose(world_point.tuple, world_point_.tuple, atol=1e-6)
+
+
+def test_fisheye_projection():
+    cam, world_points = demo_fisheye_data()
     for world_point in world_points:
         image_point = cam.calibration.project_to_image(world_point)
         assert image_point is not None
@@ -54,8 +92,30 @@ def test_array_projection():
     assert np.allclose(world_point_array, world_point_array_, atol=1e-6)
 
 
+def test_fisheye_array_projection():
+    cam, world_points = demo_fisheye_data()
+    world_points = [p for p in world_points if p.z == 1]
+
+    world_point_array = np.array([p.tuple for p in world_points])
+    image_point_array = cam.calibration.project_to_image(world_point_array)
+    for i, world_point in enumerate(world_points):
+        image_point = cam.calibration.project_to_image(world_point)
+        assert np.allclose(image_point.tuple, image_point_array[i])  # pylint: disable=unsubscriptable-object
+    world_point_array_ = cam.calibration.project_from_image(image_point_array, target_height=1)
+    assert np.allclose(world_point_array, world_point_array_, atol=1e-6)
+
+
 def test_project_from_behind():
     cam = CalibratableCamera(id='1')
     cam.set_perfect_calibration(z=1, roll=np.deg2rad(180 + 10))
+    assert cam.calibration.project_to_image(Point3d(x=0, y=1, z=1)) is not None
+    assert cam.calibration.project_to_image(Point3d(x=0, y=-1, z=1)) is None
+
+
+def test_fisheye_project_from_behind():
+    cam = CalibratableCamera(id='1')
+    cam.set_perfect_calibration(z=1, roll=np.deg2rad(180 + 10))
+    cam.calibration.intrinsics.distortion = [0.1, 0.2, 0.3, 0.4]
+    cam.calibration.intrinsics.fisheye = True
     assert cam.calibration.project_to_image(Point3d(x=0, y=1, z=1)) is not None
     assert cam.calibration.project_to_image(Point3d(x=0, y=-1, z=1)) is None
