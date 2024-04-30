37bc158ea8058613e1a159cefde81a5e8d36f310
Niklas Neugebauer <niklas@zauberzeug.com>
2024-04-30 11:10:01 +0200

    remove import of to_bytes

--- rosys/vision/usb_camera/usb_camera.py
+++ rosys/vision/usb_camera/usb_camera.py
@@ -9,7 +9,7 @@ from ... import rosys
 from ..camera.configurable_camera import ConfigurableCamera
 from ..camera.transformable_camera import TransformableCamera
 from ..image import Image, ImageSize
-from ..image_processing import process_jpeg_image, process_ndarray_image, to_bytes
+from ..image_processing import process_jpeg_image, process_ndarray_image
 from ..image_rotation import ImageRotation
 from .usb_device import UsbDevice
 
