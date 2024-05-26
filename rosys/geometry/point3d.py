5ac5555e3b20a0a8d29ff56c912ba9169cbc8ac3
Rodja Trappe <rodja@zauberzeug.com>
2024-05-26 12:15:26 +0200

    Create Point3d from tuple and list. (#124)
    * Create point3d from tuple and list
    
    * using Sequence as input type

--- rosys/geometry/point3d.py
+++ rosys/geometry/point3d.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import math
 from dataclasses import dataclass
+from typing import Sequence
 
 from .point import Point
 
@@ -11,6 +12,11 @@ class Point3d:
     x: float
     y: float
     z: float
+
+    @staticmethod
+    def from_tuple(t: Sequence[float]) -> Point3d:
+        """Create a Point3d from the three first elements of a sequence."""
+        return Point3d(x=t[0], y=t[1], z=t[2])
 
     @property
     def tuple(self) -> tuple[float, float, float]:
