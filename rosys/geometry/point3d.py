b29fca1641afee6a4707fa0bb77b37294db1bdd4
Rodja Trappe <rodja@zauberzeug.com>
2024-05-26 04:33:00 +0200

    using Sequence as input type

--- rosys/geometry/point3d.py
+++ rosys/geometry/point3d.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import math
 from dataclasses import dataclass
+from typing import Sequence
 
 from .point import Point
 
@@ -13,12 +14,9 @@ class Point3d:
     z: float
 
     @staticmethod
-    def from_tuple(t: tuple[float, float, float]) -> Point3d:
+    def from_tuple(t: Sequence[float]) -> Point3d:
+        """Create a Point3d from the three first elements of a sequence."""
         return Point3d(x=t[0], y=t[1], z=t[2])
-
-    @staticmethod
-    def from_list(l: list[float]) -> Point3d:
-        return Point3d(x=l[0], y=l[1], z=l[2])
 
     @property
     def tuple(self) -> tuple[float, float, float]:
