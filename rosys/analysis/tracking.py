142984c54ed1fff3afbf50ff177c9e3457b5b112
Falko Schindler <falko@zauberzeug.com>
2024-07-15 16:57:08 +0200

    support tracking multiple tasks

--- rosys/analysis/tracking.py
+++ rosys/analysis/tracking.py
@@ -1,3 +1,5 @@
+import asyncio
+from collections import defaultdict
 from functools import wraps
 from typing import Callable, ParamSpec, TypeVar
 
@@ -10,26 +12,29 @@ _P = ParamSpec('_P')
 class Track:
 
     def __init__(self) -> None:
-        self.stack: list[str] = []
+        self.stacks: defaultdict[int, list[str]] = defaultdict(list)
 
     def __call__(self, f: Callable[_P, _T]) -> Callable[_P, _T]:
         @wraps(f)
         async def wrap(*args, **kwargs):
+            task_id = id(asyncio.current_task())
+            stack = self.stacks[task_id]
             try:
-                self.stack.append(f.__name__)
+                stack.append(f.__name__)
                 return await f(*args, **kwargs)
             finally:
-                if self.stack and self.stack[-1] == f.__name__:
-                    self.stack.pop()
+                assert stack
+                assert stack[-1] == f.__name__
+                stack.pop()
+                if not stack:
+                    del self.stacks[task_id]
         return wrap
 
-    def ui(self) -> ui.label:
-        label = ui.label()
-        ui.timer(0.5, lambda: label.set_text(' → '.join(self.stack)))
-        return label
-
-    def reset(self) -> None:
-        self.stack.clear()
+    def ui(self) -> ui.markdown:
+        markdown = ui.markdown()
+        ui.timer(0.1, lambda: markdown.set_content('\n'.join(' → '.join(stack).replace('_', '\_')
+                                                             for stack in self.stacks.values())))
+        return markdown
 
 
 track = Track()
