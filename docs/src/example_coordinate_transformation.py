b84781cc3e3fa4162261ad5bbb14dc89a599e7bd
Niklas Neugebauer <niklas@zauberzeug.com>
2024-07-24 11:28:29 +0200

    simplify example

--- docs/src/example_coordinate_transformation.py
+++ docs/src/example_coordinate_transformation.py
@@ -1,75 +1,38 @@
 #!/usr/bin/env python3
+import math
+
 from nicegui import ui
-from nicegui.elements.scene_objects import Extrusion
+from nicegui.elements.scene_objects import Box
 
-from rosys.driving import Odometer, Steerer, joystick, keyboard_control
-from rosys.geometry import Point3d, Pose3d, Prism, Rotation, scene_object
-from rosys.geometry.coordinate_frame import CoordinateFrame
-from rosys.hardware import RobotSimulation, WheelsSimulation
-from rosys.persistence.converters import to_dict
-from rosys.vision import CalibratableCamera, CameraSceneObject, SimulatedCamera
+import rosys
+from rosys.geometry import CoordinateFrame, Point3d, Pose3d, Rotation, scene_object
 
-wheels = WheelsSimulation()
-robot = RobotSimulation([wheels])
-odometer = Odometer(wheels)
-steerer = Steerer(wheels)
-
-keyboard_control(steerer)
-joystick(steerer, size=50, color='blue')
+frame = CoordinateFrame.from_pose(Pose3d.zero())
+pose = Pose3d.zero()
 
 
-robot_shape = Prism.default_robot_shape()
-robot_pose = CoordinateFrame.from_pose(pose=Pose3d.zero(), id='robot')
-
-hat_shape = Prism.default_robot_shape()
-hat_pose = CoordinateFrame(translation=Point3d(x=-0.5, y=0, z=1),
-                           rotation=Rotation.from_euler(0, -1.5, 0))
-hat_pose.parent_frame = robot_pose
-
-hat2_shape = Prism.default_robot_shape()
-hat2_pose = Pose3d(translation=Point3d(x=0.5, y=0, z=1),
-                   rotation=Rotation.from_euler(0, 1.5, 0))
-hat2_pose.parent_frame = robot_pose
+def step():
+    frame.rotation = Rotation.from_euler(0, 0, rosys.time() / 2.0)
+    pose.translation = Point3d(x=math.sin(rosys.time()), y=0, z=0.75)
 
 
-class CalibratableCameraSimulated(SimulatedCamera, CalibratableCamera):
-    pass
-
-
-camera = CalibratableCameraSimulated.create_calibrated(id='test', name='From Dict', x=0.75, y=0, z=0.5)
-assert camera.calibration is not None
-camera.calibration.extrinsics.parent_frame = hat_pose
-
-camera = CalibratableCameraSimulated.from_dict(camera.to_dict())
-
-
-def update_pose():
-    robot_pose.translation = Point3d(x=odometer.prediction.x, y=odometer.prediction.y, z=0)
-    robot_pose.rotation = Rotation.from_euler(0, 0, odometer.prediction.yaw)
-    return robot_pose
-
-
-def shape_object(shape: Prism, debug: bool = False) -> Extrusion:
-    outline = [list(point) for point in shape.outline]
-    return Extrusion(outline, shape.height, wireframe=debug)
-
+rosys.on_repeat(step, interval=0.01)
 
 with ui.scene():
-    scene_object(lambda: shape_object(robot_shape), robot_pose)
-    scene_object(lambda: shape_object(hat_shape, debug=True), hat_pose)
-    # scene_object(lambda: shape_object(hat2_shape, debug=True), hat2_pose)
-    scene_object(lambda camera=camera: CameraSceneObject(camera), camera.calibration.extrinsics)
-
-odometer.ROBOT_MOVED.register(update_pose)
+    scene_object(lambda: Box(width=1, height=1, depth=1).material(color='#0000ff'), pose=frame)
+    scene_object(lambda: Box(width=0.5, height=0.5, depth=0.5).material(color='#ff0000'), pose=pose)
 
 
-def update_rotation(x):
-    camera.calibration.extrinsics.rotation = Rotation.from_euler(x, x, x)
+def set_parent():
+    pose.parent_frame = frame
 
 
-ui.slider(min=-1.5, max=1.5, step=0.01).on_value_change(lambda event: update_rotation(event.value))
-ui.slider(min=-1, max=1, step=0.01).bind_value(camera.calibration.extrinsics.translation, 'x')
-ui.button('Camera Dict').on_click(lambda: ui.notify(camera.to_dict()))
-ui.button('Hat Pose Dict').on_click(lambda: ui.notify(to_dict(hat_pose)))
+def reset_parent():
+    pose.parent_frame = None
+
+
+with ui.row():
+    ui.button('Set parent', on_click=set_parent)
+    ui.button('Reset parent', on_click=reset_parent)
 
 ui.run(title='RoSys')
