137fa07535febba16e95228416f689d64a9c499d
Niklas Neugebauer <niklas@zauberzeug.com>
2024-07-24 10:22:34 +0200

    ignore "redifined builtin" pylint error and remove a comment

--- rosys/geometry/coordinate_frame.py
+++ rosys/geometry/coordinate_frame.py
@@ -15,14 +15,13 @@ class CoordinateFrame(Pose3d):
     """A 3D coordinate frame based on a 3D pose.
     This is a simple wrapper around a Pose3d with an additional id.
     It guarantees that the frame is registered in the coordinate_frame_registry."""
-    # id (uuid4 per default) of the frame
     id: str = field(default_factory=lambda: str(uuid4()))
 
     def __post_init__(self):
         coordinate_frame_registry[self.id] = self
 
     @classmethod
-    def from_pose(cls, pose: Pose3d, id: str | None = None) -> CoordinateFrame:
+    def from_pose(cls, pose: Pose3d, id: str | None = None) -> CoordinateFrame:  # pylint: disable=redefined-builtin
         """Creates a new frame from a given pose.
 
         :param pose: The pose of the new frame.
