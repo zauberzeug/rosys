32bfba9da012113521d8ac1fec5264418afd5ce5
Falko Schindler <falko@zauberzeug.com>
2024-08-06 16:44:26 +0200

    revert changes to persistence example

--- docs/src/example_persistence.py
+++ docs/src/example_persistence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from typing import Any
 
-from nicegui import app, ui
+from nicegui import ui
 
 from rosys import persistence
 
@@ -10,11 +10,7 @@ class Model(persistence.PersistentModule):
 
     def __init__(self) -> None:
         super().__init__()
-        self.value: float = 5.0
-
-    def set_value(self, value: float) -> None:
-        self.value = value
-        self.request_backup()
+        self.value: float = 1.0
 
     def restore(self, data: dict[str, Any]) -> None:
         self.value = data.get('value', 1.0)
@@ -25,13 +21,7 @@ class Model(persistence.PersistentModule):
         }
 
 
-def start():
-    model = Model()
-    model.set_value(3.0)
-    ui.slider(min=0, max=10.0, step=0.1, on_change=model.request_backup).bind_value(
-        model, 'value').props('label-always')
-
-
-app.on_startup(start)
+model = Model()
+ui.slider(min=0, max=10.0, step=0.1).bind_value(model, 'value').props('label-always')
 
 ui.run(title='RoSys')
