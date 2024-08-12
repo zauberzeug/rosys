6409fa47ad204243e3cd559b61902d8075dfd21b
Rodja Trappe <rodja@zauberzeug.com>
2024-08-12 06:21:11 +0200

    Add SimulatedCalibratableCamera

--- rosys/vision/simulated_camera/__init__.py
+++ rosys/vision/simulated_camera/__init__.py
@@ -1,7 +1,14 @@
+from ..camera import CalibratableCamera
 from .simulated_camera import SimulatedCamera
 from .simulated_camera_provider import SimulatedCameraProvider
+
+
+class SimulatedCalibratableCamera(SimulatedCamera, CalibratableCamera):
+    pass
+
 
 __all__ = [
     'SimulatedCamera',
     'SimulatedCameraProvider',
+    'SimulatedCalibratableCamera',
 ]
