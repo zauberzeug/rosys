0f504fe6948ec78844df650c492e0cea07a3839b
Falko Schindler <falko@zauberzeug.com>
2024-08-12 06:53:03 +0200

    code review

--- rosys/driving/odometer.py
+++ rosys/driving/odometer.py
@@ -28,7 +28,7 @@ class Odometer:
 
         wheels.VELOCITY_MEASURED.register(self.handle_velocities)
         self.prediction: Pose = Pose()
-        self.prediction_frame = Pose3d().as_frame('odometer')
+        self.prediction_frame = Pose3d().as_frame('odometer_prediction')
         self.detection: Pose | None = None
         self.current_velocity: Velocity | None = None
         self.last_movement: float = 0
@@ -69,7 +69,7 @@ class Odometer:
             self.prediction = deepcopy(detection)
         self._handle_movement()
 
-    def _handle_movement(self):
+    def _handle_movement(self) -> None:
         self.prediction_frame.x = self.prediction.x
         self.prediction_frame.y = self.prediction.y
         self.prediction_frame.rotation = Rotation.from_euler(0, 0, self.prediction.yaw)
