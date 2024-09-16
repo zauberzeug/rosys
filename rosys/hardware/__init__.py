a4034a9eacd631c97a764620c9ff5d3098af0370
Pascal Schade <pascal@zauberzeug.com>
2024-09-16 16:56:48 +0200

    fix import order

--- rosys/hardware/__init__.py
+++ rosys/hardware/__init__.py
@@ -4,6 +4,7 @@ from .bms import Bms, BmsHardware, BmsSimulation
 from .bumper import Bumper, BumperHardware, BumperSimulation
 from .can import CanHardware
 from .communication import Communication, SerialCommunication, WebCommunication
+from .esp_pins import ESPPins
 from .estop import EStop, EStopHardware, EStopSimulation
 from .expander import ExpanderHardware
 from .imu import Imu, ImuHardware, ImuSimulation
@@ -12,7 +13,6 @@ from .robot import Robot, RobotHardware, RobotSimulation
 from .robot_brain import RobotBrain
 from .serial import SerialHardware
 from .wheels import Wheels, WheelsHardware, WheelsSimulation
-from .esp_pins import ESPPins
 
 __all__ = [
     'BatteryControlHardware',
