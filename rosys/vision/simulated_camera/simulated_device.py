d5140a80e3b21e2b895f032a994ff3ca61a46419
Pascal Schade <pascal@zauberzeug.com>
2024-10-21 10:20:04 +0200

    add test check

--- rosys/vision/simulated_camera/simulated_device.py
+++ rosys/vision/simulated_camera/simulated_device.py
@@ -28,8 +28,10 @@ class SimulatedDevice:
         self.repeater = rosys.on_repeat(self._create_image, interval=1.0 / fps)
 
     async def _create_image(self) -> None:
-        # image_data = await rosys.run.cpu_bound(_create_image_data, self.id, self.size, self.color)
-        image_data = _create_image_data(self.id, self.size, self.color)
+        if rosys.is_test:
+            image_data = b'test data'
+        else:
+            image_data = await rosys.run.cpu_bound(_create_image_data, self.id, self.size, self.color)
         if not image_data:
             return
         result = self.on_new_image_data(image_data)
