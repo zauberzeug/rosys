efdb048456d4a7f24cc3d3bb66dd7d703d74e56c
Niklas Neugebauer <68709968+NiklasNeugebauer@users.noreply.github.com>
2024-11-04 18:28:05 +0100

    image route max_size parameter (#223)
    * feat: add and use max_size parameter
    
    * feature: image routes
    
    extend EP to allow floating point shrink factors.
    Default behaviour is unchanged.
    
    * formatting
    
    * code review
    
    ---------
    
    Co-authored-by: Dr. Dennis Wittich <denniswittich@hotmail.de>
    Co-authored-by: Falko Schindler <falko@zauberzeug.com>

--- rosys/vision/image_route.py
+++ rosys/vision/image_route.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import math
 from typing import TYPE_CHECKING
 
 import cv2
@@ -27,11 +28,30 @@ def create_image_route(camera: Camera) -> None:
     app.remove_route(timestamp_url)
     app.remove_route(undistorted_url)
 
-    async def get_camera_image(timestamp: str, shrink: int = 1) -> Response:
-        return await _get_image(camera, timestamp, shrink=shrink)
+    async def get_camera_image(timestamp: str,
+                               shrink: float = 1.0,
+                               max_dimension: float | None = None,
+                               fast: bool = True,
+                               compression: int = 60) -> Response:
+        return await _get_image(camera, timestamp,
+                                shrink=shrink,
+                                max_dimension=max_dimension,
+                                undistort=False,
+                                fast=fast,
+                                compression=compression)
 
-    async def get_camera_image_undistorted(timestamp: str, shrink: int = 1) -> Response:
-        return await _get_image(camera, timestamp, shrink=shrink, undistort=True)
+    async def get_camera_image_undistorted(timestamp: str,
+                                           shrink: float = 1.0,
+                                           max_dimension: float | None = None,
+                                           fast: bool = True,
+                                           compression: int = 60) -> Response:
+        return await _get_image(camera,
+                                timestamp,
+                                shrink=shrink,
+                                max_dimension=max_dimension,
+                                undistort=True,
+                                fast=fast,
+                                compression=compression)
 
     app.add_api_route(placeholder_url, _get_placeholder)
     app.add_api_route(timestamp_url, get_camera_image)
@@ -42,13 +62,23 @@ async def _get_placeholder(shrink: int = 1) -> Response:
     return Response(content=Image.create_placeholder('no image', shrink=shrink).data, media_type='image/jpeg')
 
 
-async def _get_image(camera: Camera, timestamp: str, *, shrink: int = 1, undistort: bool = False) -> Response:
+async def _get_image(camera: Camera,
+                     timestamp: str, *,
+                     shrink: float,
+                     max_dimension: float | None,
+                     undistort: bool,
+                     fast: bool,
+                     compression: int) -> Response:
     try:
-        if not camera:
-            return Response(content='Camera not found', status_code=404)
         if undistort and getattr(camera, 'calibration', None) is None:
             return Response(content='Camera is not calibrated', status_code=404)
-        jpeg = await _try_get_jpeg(camera, timestamp, shrink=shrink, undistort=undistort)
+        jpeg = await _try_get_jpeg(camera,
+                                   timestamp,
+                                   shrink=shrink,
+                                   max_dimension=max_dimension,
+                                   undistort=undistort,
+                                   fast=fast,
+                                   compression=compression)
         if not jpeg:
             return Response(content='Image not found', status_code=404)
         return Response(content=jpeg, headers={'cache-control': 'max-age=7776000'}, media_type='image/jpeg')
@@ -57,18 +87,34 @@ async def _get_image(camera: Camera, timestamp: str, *, shrink: int = 1, undisto
         raise
 
 
-async def _try_get_jpeg(camera: Camera, timestamp: str, *, shrink: int, undistort: bool) -> bytes | None:
+async def _try_get_jpeg(camera: Camera,
+                        timestamp: str, *,
+                        shrink: float,
+                        max_dimension: float | None,
+                        undistort: bool,
+                        fast: bool,
+                        compression: int) -> bytes | None:
     for image in reversed(camera.images):
         if str(image.time) == timestamp and image.data is not None:
-            if shrink == 1 and not undistort:
+            shrink_from_max = max(image.size.width, image.size.height) / max_dimension if max_dimension else shrink
+
+            shrink = max(1, shrink, shrink_from_max)
+
+            if shrink == 1 and not undistort and compression == 60:
                 return image.data
-            else:
-                calibration = camera.calibration if undistort else None  # type: ignore
-                return await run.cpu_bound(_process, image.data, calibration, shrink, undistort)
+
+            calibration = camera.calibration if undistort else None  # type: ignore
+            return await run.cpu_bound(_process, image.data, calibration, shrink, undistort, fast, compression)
+
     return None
 
 
-def _process(data: bytes, calibration: Calibration | None, shrink: int, undistort: bool) -> bytes | None:
+def _process(data: bytes,
+             calibration: Calibration | None,
+             shrink: float,
+             undistort: bool,
+             fast: bool,
+             compression: int) -> bytes | None:
     array = np.frombuffer(data, dtype=np.uint8)
     if array is None:
         return None
@@ -81,8 +127,15 @@ def _process(data: bytes, calibration: Calibration | None, shrink: int, undistor
             logging.warning('undistort_array returned an empty image')
             return None
 
-    if shrink != 1:
-        image_array = image_array[::shrink, ::shrink]
+    if shrink != 1.0:
+        if fast:
+            shrink_factor = math.ceil(shrink)
+            image_array = image_array[::shrink_factor, ::shrink_factor]
+        else:
+            new_width = int(image_array.shape[1] / shrink)
+            new_height = int(image_array.shape[0] / shrink)
+            # INTER_AREA is optimal for downsampling
+            image_array = cv2.resize(image_array, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
-    _, encoded_image = cv2.imencode('.jpg', image_array, [int(cv2.IMWRITE_JPEG_QUALITY), 60])
+    _, encoded_image = cv2.imencode('.jpg', image_array, [int(cv2.IMWRITE_JPEG_QUALITY), compression])
     return encoded_image.tobytes()
