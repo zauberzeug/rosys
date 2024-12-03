0442c3e7a5f069fda5446ca9d0ebcc53219eec91
Pascal Schade <pascal@zauberzeug.com>
2024-12-03 22:59:10 +0100

    remove quickfix for issue 66

--- rosys/hardware/battery_control.py
+++ rosys/hardware/battery_control.py
@@ -19,12 +19,6 @@ class BatteryControlHardware(ModuleHardware):
             {self.name}_reset = {expander.name + "." if expander else ""}Output({reset_pin})
             {self.name}_status = {expander.name + "." if expander else ""}Input({status_pin})
         ''')
-        if expander:
-            lizard_code += remove_indentation(f'''
-                # TODO: remove when lizard issue 66 is fixed.
-                {self.name}_status.level = 0
-                {self.name}_status.active = false
-            ''')
         core_message_fields = [f'{self.name}_status.level']
         super().__init__(robot_brain=robot_brain, lizard_code=lizard_code, core_message_fields=core_message_fields)
 
