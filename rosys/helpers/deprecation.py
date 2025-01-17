dfb00d75ead634c9dc789442e242e884962f9b17
Falko Schindler <falko@zauberzeug.com>
2025-01-17 16:57:57 +0100

    improve type annotation and docstring

--- rosys/helpers/deprecation.py
+++ rosys/helpers/deprecation.py
@@ -1,17 +1,17 @@
 import warnings
+from collections.abc import Callable
 from functools import wraps
 
 
-def deprecated_param(param_name):
-    """Usage:
-    @deprecated_param('my_param')
-    def my_function(param1, my_param=None):"""
-    def decorator(func):
+def deprecated_param(param_name: str) -> Callable:
+    """Mark a function parameter as deprecated."""
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args, **kwargs):
             if param_name in kwargs:
                 warnings.warn(
-                    f"The '{param_name}' parameter is deprecated and will be removed in future versions.",
+                    f'The parameter "{param_name}" is deprecated and will be removed in future versions.',
+                    category=DeprecationWarning,
                     stacklevel=2,
                 )
             return func(*args, **kwargs)
