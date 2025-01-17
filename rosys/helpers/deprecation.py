aaab564b7332d4a0b8445a33810d79680d516f58
Niklas Neugebauer <niklas@zauberzeug.com>
2025-01-17 14:39:25 +0100

    feat: add deprecation decorator

---
+++ rosys/helpers/deprecation.py
@@ -0,0 +1,19 @@
+import warnings
+from functools import wraps
+
+
+def deprecated_param(param_name):
+    """Usage:
+    @deprecated_param('my_param')
+    def my_function(param1, my_param=None):"""
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if param_name in kwargs:
+                warnings.warn(
+                    f"The '{param_name}' parameter is deprecated and will be removed in future versions.",
+                    DeprecationWarning,
+                )
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
