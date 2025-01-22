1025c1974bfc6125931db819aece1f83851c0665
Niklas Neugebauer <niklas@zauberzeug.com>
2025-01-22 16:42:38 +0100

    add wrapper to function warning and replace __init__ for classes

--- rosys/helpers/deprecation.py
+++ rosys/helpers/deprecation.py
@@ -23,20 +23,33 @@ def deprecated_param(param_name: str, *, remove_in_version: str | None = None, s
 def deprecated_function(*, remove_in_version: str | None = None, stacklevel: int = 2) -> Callable:
     """Mark a function as deprecated."""
     def decorator(func: Callable) -> Callable:
-        warnings.warn(f'The function "{func.__name__}" is deprecated and will be removed in '
-                      f'{("RoSys " + remove_in_version) if remove_in_version else "a future version"}.',
-                      category=DeprecationWarning,
-                      stacklevel=stacklevel)
-        return func
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            warnings.warn(
+                f'The function "{func.__name__}" is deprecated and will be removed in '
+                f'{("RoSys " + remove_in_version) if remove_in_version else "a future version"}.',
+                category=DeprecationWarning,
+                stacklevel=stacklevel,
+            )
+            return func(*args, **kwargs)
+        return wrapper
     return decorator
 
 
 def deprecated_class(*, remove_in_version: str | None = None, stacklevel: int = 2) -> Callable:
     """Mark a class as deprecated."""
     def decorator(cls: type) -> type:
-        warnings.warn(f'The class "{cls.__name__}" is deprecated and will be removed in '
-                      f'{("RoSys " + remove_in_version) if remove_in_version else "a future version"}.',
-                      category=DeprecationWarning,
-                      stacklevel=stacklevel)
+        original_init = cls.__init__
+
+        def new_init(self, *args, **kwargs):
+            warnings.warn(
+                f'The class "{cls.__name__}" is deprecated and will be removed in '
+                f'{("RoSys " + remove_in_version) if remove_in_version else "a future version"}.',
+                category=DeprecationWarning,
+                stacklevel=stacklevel,
+            )
+            original_init(self, *args, **kwargs)
+
+        cls.__init__ = new_init
         return cls
     return decorator
