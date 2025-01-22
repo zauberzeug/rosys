bb5dcc867a4791cbb16b49bc15ec8a1b2f0f6320
Pascal Schade <pascal@zauberzeug.com>
2025-01-22 01:14:46 +0100

    Expand current IMU

--- rosys/hardware/imu.py
+++ rosys/hardware/imu.py
@@ -1,7 +1,34 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+import numpy as np
+from nicegui import ui
+
+from .. import helpers, rosys
 from ..event import Event
 from ..geometry import Rotation
 from .module import Module, ModuleHardware, ModuleSimulation
 from .robot_brain import RobotBrain
+
+if TYPE_CHECKING:
+    from .wheels import WheelsSimulation
+
+
+@dataclass
+class ImuMeasurement:
+    """Imu measurement data with corrected and uncorrected angles and angular velocities in radians."""
+    time: float
+    roll: float
+    pitch: float
+    yaw: float
+    roll_corrected: float
+    pitch_corrected: float
+    yaw_corrected: float
+    roll_velocity: float | None
+    pitch_velocity: float | None
+    yaw_velocity: float | None
 
 
 class Imu(Module):
@@ -9,30 +36,53 @@ class Imu(Module):
     def __init__(self, offset_rotation: Rotation, **kwargs) -> None:
         super().__init__(**kwargs)
         self.offset_rotation = offset_rotation
-        self.rotation: Rotation | None = None
         self.gyro_calibration: float = 0.0
+        self.last_measurement: ImuMeasurement | None = None
 
         self.NEW_MEASUREMENT = Event()
-        """a new measurement has been received (argument: Tuple[Roll, Pitch, Yaw])"""
+        """a new measurement has been received (argument: ImuMeasurement)"""
 
-    @property
-    def roll(self) -> float | None:
-        return self.rotation.roll if self.rotation else None
+    def _emit_measurement(self, rotation: Rotation, time: float) -> None:
+        assert self.offset_rotation is not None
+        corrected_rotation = rotation * self.offset_rotation.T
+        corrected_euler = corrected_rotation.euler
+        new_measurement = ImuMeasurement(
+            time=time,
+            roll=rotation.roll,
+            pitch=rotation.pitch,
+            yaw=rotation.yaw,
+            roll_corrected=corrected_euler[0],
+            pitch_corrected=corrected_euler[1],
+            yaw_corrected=corrected_euler[2],
+            roll_velocity=None,
+            pitch_velocity=None,
+            yaw_velocity=None,
+        )
+        if self.last_measurement is not None:
+            d_t = time - self.last_measurement.time
+            d_roll = helpers.angle(self.last_measurement.roll, rotation.roll)
+            d_pitch = helpers.angle(self.last_measurement.pitch, rotation.pitch)
+            d_yaw = helpers.angle(self.last_measurement.yaw, rotation.yaw)
 
-    @property
-    def pitch(self) -> float | None:
-        return self.rotation.pitch if self.rotation else None
+            roll_velocity = d_roll / d_t
+            pitch_velocity = d_pitch / d_t
+            yaw_velocity = d_yaw / d_t
 
-    @property
-    def yaw(self) -> float | None:
-        return self.rotation.yaw if self.rotation else None
+            new_measurement.roll_velocity = roll_velocity
+            new_measurement.pitch_velocity = pitch_velocity
+            new_measurement.yaw_velocity = yaw_velocity
+            self.NEW_MEASUREMENT.emit(new_measurement)
+        self.last_measurement = new_measurement
 
-    @property
-    def euler(self) -> tuple[float, float, float] | None:
-        return self.rotation.euler if self.rotation else None
-
-    def emit_measurement(self) -> None:
-        self.NEW_MEASUREMENT.emit(self.euler)
+    def developer_ui(self) -> None:
+        ui.label('IMU').classes('text-center text-bold')
+        with ui.column().classes('gap-y-1'):
+            ui.label().bind_text_from(self, 'last_measurement',
+                                      lambda m: f'Roll: {np.rad2deg(m.roll):.2f}°' if m is not None else 'Roll: N/A')
+            ui.label().bind_text_from(self, 'last_measurement',
+                                      lambda m: f'Pitch: {np.rad2deg(m.pitch):.2f}°' if m is not None else 'Pitch: N/A')
+            ui.label().bind_text_from(self, 'last_measurement',
+                                      lambda m: f'Yaw: {np.rad2deg(m.yaw):.2f}°' if m is not None else 'Yaw: N/A')
 
 
 class ImuHardware(Imu, ModuleHardware):
@@ -60,17 +110,31 @@ class ImuHardware(Imu, ModuleHardware):
 
         if self.gyro_calibration < 1.0:
             return
-
-        # TODO: refactor
-        # self.rotation = rotation * self.offset_rotation.T
-        self.rotation = rotation
-        self.emit_measurement()
+        self._emit_measurement(rotation, time)
 
 
 class ImuSimulation(Imu, ModuleSimulation):
 
-    def simulate_measurement(self, rotation: Rotation) -> None:
-        # TODO: refactor
-        # self.rotation = rotation * self.offset_rotation.T
-        self.rotation = rotation
-        self.emit_measurement()
+    def __init__(self, *, wheels: WheelsSimulation, interval: float = 0.1, roll_noise: float = 0.0, pitch_noise: float = 0.0, yaw_noise: float = 0.0, **kwargs) -> None:
+        super().__init__(offset_rotation=Rotation.zero(), **kwargs)
+        self.wheels = wheels
+        self._roll_noise = roll_noise
+        self._pitch_noise = pitch_noise
+        self._yaw_noise = yaw_noise
+        rosys.on_repeat(self.simulate, interval)
+
+    def simulate(self) -> None:
+        roll = np.random.normal(0, self._roll_noise)
+        pitch = np.random.normal(0, self._pitch_noise)
+        yaw = self.wheels.pose.yaw + np.random.normal(0, self._yaw_noise)
+        self._emit_measurement(Rotation.from_euler(roll, pitch, yaw), rosys.time())
+
+    def developer_ui(self) -> None:
+        super().developer_ui()
+        with ui.column().classes('gap-y-1'):
+            ui.number(label='Roll Noise', format='%.3f', prefix='± ', suffix='°') \
+                .bind_value(self, '_roll_noise', forward=np.deg2rad, backward=np.rad2deg).classes('w-4/5')
+            ui.number(label='Pitch Noise', format='%.3f', prefix='± ', suffix='°') \
+                .bind_value(self, '_pitch_noise', forward=np.deg2rad, backward=np.rad2deg).classes('w-4/5')
+            ui.number(label='Yaw Noise', format='%.3f', prefix='± ', suffix='°') \
+                .bind_value(self, '_yaw_noise', forward=np.deg2rad, backward=np.rad2deg).classes('w-4/5')
