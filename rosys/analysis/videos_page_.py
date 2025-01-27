77eee817d4df2dc47498a02c379ec04309b94208
Paula Kammler <paula@zauberzeug.com>
2025-01-27 19:04:17 +0100

    update video page to only display times, dates and actions

--- rosys/analysis/videos_page_.py
+++ rosys/analysis/videos_page_.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from datetime import datetime
 from pathlib import Path
 
 import watchfiles
@@ -9,22 +11,62 @@ VIDEO_FILES = Path('~/.rosys/timelapse/videos').expanduser()
 class VideosPage:
 
     def __init__(self) -> None:
+
         @ui.page('/videos', title='Videos')
         async def page():
             @ui.refreshable
-            async def videos():
-                for mp4 in sorted(await run.io_bound(VIDEO_FILES.glob, '*.mp4'), reverse=True):
-                    with ui.row():
-                        with ui.card().tight():
-                            video = ui.video(mp4).classes('w-[800px]')
-                            src = video._props['src']  # pylint: disable=protected-access
-                        with ui.column():
-                            ui.button(on_click=lambda mp4=mp4: mp4.unlink()).props('icon=delete flat')
-                            ui.button(on_click=lambda src=src: ui.download(src)).props('icon=download flat')
+            async def videos(selected_date: str):
+                all_videos = await get_all_videos()
+                with ui.card().tight().props('flat bordered'):
+                    with ui.card_section().classes('w-full bg-primary text-white'):
+                        ui.label(_format_date(selected_date)).classes('text-xl')
+                    with ui.card_section().classes('w-full'):
+                        if selected_date not in all_videos:
+                            ui.label('No videos for this date')
+                        else:
+                            with ui.grid(columns='auto auto auto').classes('items-center'):
+                                for mp4 in all_videos[selected_date]:
+                                    video_date, video_duration = format_video_name(mp4)
+                                    ui.label(video_date)
+                                    ui.label(video_duration)
+                                    with ui.row().classes('gap-1'):
+                                        ui.button(on_click=lambda mp4=mp4: ui.download(mp4)) \
+                                            .props('icon=download flat') \
+                                            .tooltip('download')
+                                        ui.button(on_click=lambda mp4=mp4: mp4.unlink(),
+                                                  color='negative') \
+                                            .props('icon=delete flat') \
+                                            .tooltip('delete')
 
             async def watch_videos():
                 async for _ in watchfiles.awatch(VIDEO_FILES):
                     videos.refresh()
 
-            await videos()
+            ui.label('Timelapse Videos').classes('text-2xl')
+            with ui.row(wrap=False).classes('w-full'):
+                date_picker = ui.date(value=datetime.now().date().strftime('%Y%m%d'),
+                                      mask='YYYYMMDD',
+                                      on_change=lambda e: videos.refresh(e.value))
+                await videos(date_picker.value)
+
             background_tasks.create(watch_videos(), name='watch videos files')
+
+
+def _format_date(date: str) -> str:
+    return datetime.strptime(date, '%Y%m%d').strftime('%d.%m.%Y')
+
+
+def format_video_name(video: Path) -> tuple[str, str]:
+    parts = video.stem.split('_')
+    date = _format_date(parts[0])
+    time = parts[1].replace('-', ':')
+    return f'{date} {time}', f'{parts[2]} {parts[3]}'
+
+
+async def get_all_videos() -> dict[str, list[Path]]:
+    all_videos = sorted(await run.io_bound(VIDEO_FILES.glob, '*.mp4'), reverse=True)
+    video_date_dict: dict[str, list[Path]] = defaultdict(list)
+    for video in all_videos:
+        video_date = video.stem.split('_')[0]
+        video_date_dict[video_date].append(video)
+    return video_date_dict
