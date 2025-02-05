4ecf22254e2c6ca7e8ff5b2021bf9e2c9a51a1c4
Niklas Neugebauer <niklas@zauberzeug.com>
2025-02-05 16:49:45 +0100

    tests: test base parameters for all types tests: test calibratable subclasses

--- tests/test_cameras.py
+++ tests/test_cameras.py
@@ -9,7 +9,6 @@ from rosys.vision import (
     CalibratableCamera,
     Calibration,
     Camera,
-    ConfigurableCamera,
     MjpegCamera,
     RtspCamera,
     RtspCameraProvider,
@@ -61,12 +60,8 @@ async def test_rtsp_camera(rosys_integration):
     assert len(camera.images) >= 1
 
 
-async def test_storing_camera_as_dict(rosys_integration):
-    camera = Camera(id='test_cam',
-                    name='T3:5T',
-                    connect_after_init=False,
-                    base_path_overwrite='test_cam',
-                    image_history_length=10)
+async def test_storing_camera_as_dict(rosys_integration, camera_parameters):
+    camera = Camera(**camera_parameters)
     await camera.connect()
     camera_as_dict = camera.to_dict()
     restored_camera = Camera.from_dict(camera_as_dict)
@@ -76,9 +71,8 @@ async def test_storing_camera_as_dict(rosys_integration):
     assert restored_camera.images.maxlen == camera.images.maxlen
 
 
-async def test_storing_simulated_camera_as_dict(rosys_integration):
-    camera = SimulatedCamera(id='test_cam', name='T3:5T', connect_after_init=False,
-                             width=808, height=606, color='#010101', fps=1)
+async def test_storing_simulated_camera_as_dict(rosys_integration, camera_parameters):
+    camera = SimulatedCamera(width=808, height=606, color='#010101', fps=1, **camera_parameters)
     await camera.connect()
     camera_as_dict = camera.to_dict()
     restored_camera = SimulatedCamera.from_dict(camera_as_dict)
@@ -90,24 +84,9 @@ async def test_storing_simulated_camera_as_dict(rosys_integration):
     assert restored_camera.parameters == camera.parameters
 
 
-async def test_storing_configurable_camera_as_dict(rosys_integration):
-    camera = ConfigurableCamera(id='test_cam', name='T3:5T', connect_after_init=False,
-                                base_path_overwrite='test_cam')
-    await camera.connect()
-    camera_as_dict = camera.to_dict()
-    restored_camera = ConfigurableCamera.from_dict(camera_as_dict)
-    assert isinstance(restored_camera, ConfigurableCamera)
-    assert restored_camera.id == camera.id
-    assert restored_camera.name == camera.name
-    assert restored_camera.parameters == camera.parameters
-    assert restored_camera.connect_after_init == camera.connect_after_init
-    assert restored_camera.base_path == camera.base_path
-
-
-def test_storing_transformable_camera_as_dict(rosys_integration):
-    camera = TransformableCamera(id='test_cam', name='T3:5T',
-                                 crop=Rectangle(x=10, y=10, width=100, height=100),
-                                 rotation=90)
+def test_storing_transformable_camera_as_dict(rosys_integration, camera_parameters):
+    camera = TransformableCamera(crop=Rectangle(x=10, y=10, width=100, height=100),
+                                 rotation=90, **camera_parameters)
     camera_as_dict = camera.to_dict()
     restored_camera = TransformableCamera.from_dict(camera_as_dict)
     assert isinstance(restored_camera, TransformableCamera)
@@ -118,24 +97,9 @@ def test_storing_transformable_camera_as_dict(rosys_integration):
     assert restored_camera.rotation_angle == camera.rotation_angle
 
 
-def test_storing_calibratable_camera_as_dict(rosys_integration):
-    camera = CalibratableCamera.create_calibrated(id='test_cam', width=808, height=606,
-                                                  focal_length=577, x=1.0, y=2.0, z=3.0)
-    assert isinstance(camera.calibration, Calibration)
-    camera_as_dict = camera.to_dict()
-    restored_camera = CalibratableCamera.from_dict(camera_as_dict)
-    assert isinstance(restored_camera, CalibratableCamera)
-    assert isinstance(restored_camera.calibration, Calibration)
-    assert restored_camera.id == camera.id
-    assert restored_camera.name == camera.name
-    assert restored_camera.calibration == camera.calibration
-
-
-async def test_storing_mjpeg_camera_as_dict(rosys_integration):
-    camera = MjpegCamera(id='test_cam', name='T3:5T', connect_after_init=False,
-                         base_path_overwrite='test_cam', ip='192.168.1.1',
-                         username='admin', password='admin', fps=1,
-                         resolution=(808, 606), mirrored=True)
+async def test_storing_mjpeg_camera_as_dict(rosys_integration, camera_parameters):
+    camera = MjpegCamera(ip='192.168.1.1', username='admin', password='admin', fps=1,
+                         resolution=(808, 606), mirrored=True, **camera_parameters)
     await camera.connect()
     camera_as_dict = camera.to_dict()
     restored_camera = MjpegCamera.from_dict(camera_as_dict)
@@ -150,9 +114,8 @@ async def test_storing_mjpeg_camera_as_dict(rosys_integration):
     assert restored_camera.mac == camera.mac
 
 
-def test_storing_rtsp_camera_as_dict(rosys_integration):
-    camera = RtspCamera(id='test_cam', name='T3:5T', connect_after_init=False,
-                        fps=1, substream=2, bitrate=4097, ip='192.168.1.1')
+def test_storing_rtsp_camera_as_dict(rosys_integration, camera_parameters):
+    camera = RtspCamera(fps=1, substream=2, bitrate=4097, ip='192.168.1.1', **camera_parameters)
     camera_as_dict = camera.to_dict()
     restored_camera = RtspCamera.from_dict(camera_as_dict)
     assert isinstance(restored_camera, RtspCamera)
@@ -160,3 +123,53 @@ def test_storing_rtsp_camera_as_dict(rosys_integration):
     assert restored_camera.name == camera.name
     assert restored_camera.parameters == camera.parameters
     assert restored_camera.ip == camera.ip
+
+
+def test_storing_usb_camera_as_dict(rosys_integration, camera_parameters):
+    camera = UsbCamera(auto_exposure=False, exposure=100, width=808, height=606, fps=1, **camera_parameters)
+    camera_as_dict = camera.to_dict()
+    restored_camera = UsbCamera.from_dict(camera_as_dict)
+    assert isinstance(restored_camera, UsbCamera)
+    assert restored_camera.id == camera.id
+    assert restored_camera.name == camera.name
+    assert restored_camera.parameters == camera.parameters
+
+
+def test_storing_calibratable_camera_as_dict(rosys_integration, camera_parameters):
+    camera = CalibratableCamera.create_calibrated(id='test_cam', width=808, height=606,
+                                                  focal_length=577, x=1.0, y=2.0, z=3.0)
+    assert isinstance(camera.calibration, Calibration)
+    camera_as_dict = camera.to_dict()
+    restored_camera = CalibratableCamera.from_dict(camera_as_dict)
+    assert isinstance(restored_camera, CalibratableCamera)
+    assert isinstance(restored_camera.calibration, Calibration)
+    assert restored_camera.id == camera.id
+    assert restored_camera.name == camera.name
+    assert restored_camera.calibration == camera.calibration
+
+
+def test_storing_calibratable_camera_subclasses_as_dict(rosys_integration):
+    calibration = CalibratableCamera.create_calibrated(id='test_cam', width=808, height=606, focal_length=577,
+                                                       x=1.0, y=2.0, z=3.0).calibration
+    assert calibration is not None
+
+    class CalibratableRtspCamera(CalibratableCamera, RtspCamera):
+        pass
+
+    class CalibratableUsbCamera(CalibratableCamera, UsbCamera):
+        pass
+
+    class CalibratableMjpegCamera(CalibratableCamera, MjpegCamera):
+        pass
+
+    class CalibratableSimulatedCamera(CalibratableCamera, SimulatedCamera):
+        pass
+
+    for camera_class in [CalibratableRtspCamera, CalibratableUsbCamera, CalibratableMjpegCamera, CalibratableSimulatedCamera]:
+        camera = camera_class(id='test_cam')
+        camera.calibration = calibration
+        camera_as_dict = camera.to_dict()
+        restored_camera = camera_class.from_dict(camera_as_dict)
+        assert isinstance(restored_camera, camera_class)
+        assert restored_camera.id == camera.id
+        assert restored_camera.calibration == camera.calibration
