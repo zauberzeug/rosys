6f8e4a671e8948f3b17bdf0d6002918da3fc3634
Falko Schindler <falko@zauberzeug.com>
2025-03-11 12:07:51 +0100

    add docstrings; fix linter/typing issues

--- rosys/persistence/persistable.py
+++ rosys/persistence/persistable.py
@@ -1,7 +1,8 @@
 import abc
 import json
 from pathlib import Path
-from typing import Any, ClassVar, Self
+from typing import Any, ClassVar
+from typing_extensions import Self
 from nicegui import app, run
 
 
@@ -15,18 +16,30 @@ class Persistable(abc.ABC):
 
     def persistent(self, *,
                    key: str | None = None,
-                   path: str | Path = Path('~/.rosys').expanduser(),
+                   path: str | Path | None = None,
                    restore: bool = True,  # TODO: which default?
                    backup_check_interval: float | None = 10.0,  # TODO: which default?
                    backup_interval: float | None = None,  # TODO: which default?
                    ) -> Self:
+        """Make this object persistent.
+
+        If ``restore`` is ``True``, the object will be restored from the persistence file.
+        If ``backup_check_interval`` is not ``None`` or 0.0, the object will be backed up at the given interval _if a backup has been requested_.
+        If ``backup_interval`` is not ``None`` or 0.0, the object will be backed up at the given interval _independently of any backup requests_.
+
+        :param key: The key to use for the persistence file (default: module name)
+        :param path: The path to the persistence file (default: "~/.rosys")
+        :param restore: Whether to restore the object from the persistence file (default: True)
+        :param backup_check_interval: The interval to check for backups (default: 10.0 seconds)
+        :param backup_interval: The interval to backup the object (default: None)
+        """
         if self._filepath is not None:
             raise RuntimeError('This object is already persistent')
         key = key or self.__module__
         if key in self.used_keys:
             raise ValueError(f'Key "{key}" is already used by another persistent object')
         self.used_keys.append(key)
-        self._filepath = Path(path) / f'{key}.json'
+        self._filepath = Path(path or '~/.rosys').expanduser() / f'{key}.json'
         if restore:
             self.sync_restore()
         if backup_check_interval:
@@ -38,6 +51,10 @@ class Persistable(abc.ABC):
         return self
 
     def request_backup(self) -> None:
+        """Request a backup of the object.
+
+        This will trigger a backup at the check defined by ``backup_check_interval``.
+        """
         self._needs_backup = True
 
     async def _backup_if_requested(self) -> None:
@@ -45,18 +62,38 @@ class Persistable(abc.ABC):
             await self.backup()
 
     async def backup(self) -> None:
+        """Backup the object.
+
+        This will immediately write the object to the persistence file.
+        The file is written in an I/O bound task to avoid blocking the main thread.
+        """
         await run.io_bound(self.sync_backup)
 
     def sync_backup(self) -> None:
+        """Backup the object.
+
+        This will immediately write the object to the persistence file.
+        The file is written synchronously.
+        To avoid blocking the main thread, use ``request_backup()`` instead.
+        """
         if self._filepath is None:
             raise RuntimeError('Backup failed: This object is not persistent.')
         self._filepath.write_text(json.dumps(self.backup_to_dict()))
         self._needs_backup = False
 
     async def restore(self) -> None:
+        """Restore the object from the persistence file.
+
+        The file is read in an I/O bound task to avoid blocking the main thread.
+        """
         await run.io_bound(self.sync_restore)
 
     def sync_restore(self) -> None:
+        """Restore the object from the persistence file.
+
+        The file is read synchronously.
+        To avoid blocking the main thread, use ``restore()`` instead.
+        """
         if self._filepath is None:
             raise RuntimeError('Restore failed: This object is not persistent.')
         if not self._filepath.exists():
@@ -64,6 +101,7 @@ class Persistable(abc.ABC):
         self.restore_from_dict(json.loads(self._filepath.read_text()))
 
     def delete_persistence(self) -> None:
+        """Delete the persistence file."""
         if self._filepath is None:
             raise RuntimeError('Delete persistence failed: This object is not persistent.')
         self._filepath.unlink()
@@ -71,8 +109,8 @@ class Persistable(abc.ABC):
 
     @abc.abstractmethod
     def backup_to_dict(self) -> dict[str, Any]:
-        pass
+        """Convert the object to a dictionary."""
 
     @abc.abstractmethod
     def restore_from_dict(self, data: dict[str, Any]) -> None:
-        pass
+        """Restore the object from a dictionary."""
