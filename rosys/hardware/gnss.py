7a102c40663e8e4b232563d96ab25a79354ffa6b
Pascal Schade <pascal@zauberzeug.com>
2025-08-29 01:34:42 +0200

    simulate gnss delay

--- rosys/hardware/gnss.py
+++ rosys/hardware/gnss.py
@@ -281,6 +281,8 @@ class GnssSimulation(Gnss):
                  lat_std_dev: float = 0.01,
                  lon_std_dev: float = 0.01,
                  heading_std_dev: float = 0.01,
+                 interval: float = 1.0,
+                 delay: float = 0.0,
                  gps_quality: GpsQuality = GpsQuality.RTK_FIXED) -> None:
         """
         :param wheels: the wheels to use for the simulation
@@ -288,6 +290,8 @@ class GnssSimulation(Gnss):
         :param lon_std_dev: the standard deviation of the longitude in meters
         :param heading_std_dev: the standard deviation of the heading in degrees
         :param gps_quality: the quality of the GPS signal
+        :param interval: the interval between measurements in seconds
+        :param delay: the simulated measurement delay in seconds
         """
         super().__init__()
         self.wheels = wheels
@@ -296,8 +300,9 @@ class GnssSimulation(Gnss):
         self._lon_std_dev = lon_std_dev
         self._heading_std_dev = heading_std_dev
         self._gps_quality = gps_quality
+        self._delay = delay
         self.last_measurement: GnssMeasurement | None = None
-        rosys.on_repeat(self.simulate, 1.0)
+        rosys.on_repeat(self.simulate, interval)
 
     @property
     def is_connected(self) -> bool:
@@ -307,7 +312,7 @@ class GnssSimulation(Gnss):
     def is_connected(self, value: bool) -> None:
         self._is_connected = value
 
-    def simulate(self) -> None:
+    async def simulate(self) -> None:
         if not self.is_connected:
             return
         geo_pose = GeoPose.from_pose(self.wheels.pose)
@@ -320,6 +325,8 @@ class GnssSimulation(Gnss):
         noise_heading = np.random.normal(geo_pose.heading, math.radians(self._heading_std_dev))
         noise_pose = GeoPose(lat=noise_point.lat, lon=noise_point.lon, heading=noise_heading)
         timestamp = rosys.time()
+        if self._delay > 0.0:
+            await rosys.sleep(self._delay)
         self.last_measurement = GnssMeasurement(
             time=timestamp,
             gnss_time=timestamp,
@@ -345,3 +352,5 @@ class GnssSimulation(Gnss):
                     .bind_value(self, '_heading_std_dev').classes('w-4/5')
                 ui.select({quality: quality.name for quality in GpsQuality}, label='Quality') \
                     .bind_value(self, '_gps_quality').classes('w-4/5')
+                ui.number(label='Delay', format='%.2f', suffix='s') \
+                    .bind_value(self, '_delay').classes('w-4/5')
