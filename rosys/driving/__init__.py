5394970437770bf2f63e5dad9abe0585c64b069f
Pascal Schade <pascal@zauberzeug.com>
2025-09-23 10:20:10 +0200

    Revert "wip: persistable driver"
    This reverts commit b9f91a26c57cc62e1736c7cbdfeefe33c24b04a2.

--- rosys/driving/__init__.py
+++ rosys/driving/__init__.py
@@ -1,5 +1,5 @@
 from .drivable import Drivable
-from .driver import DriveParameters, Driver, DrivingAbortedException
+from .driver import Driver, DrivingAbortedException
 from .driver_object import DriverObject as driver_object
 from .joystick_ import Joystick as joystick
 from .keyboard_control_ import KeyboardControl as keyboard_control
@@ -10,7 +10,6 @@ from .steerer import Steerer
 
 __all__ = [
     'Drivable',
-    'DriveParameters',
     'Driver',
     'DrivingAbortedException',
     'Odometer',
