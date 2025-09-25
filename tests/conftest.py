0a242560beb2066cb004707e35842feb1c244f3f
Niklas Neugebauer <niklas@zauberzeug.com>
2025-09-25 14:56:51 +0200

    fix: use tempfile in fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
+import tempfile
+from pathlib import Path
+
+import pytest
+
 from rosys.testing.fixtures import *  # pylint: disable=wildcard-import,unused-wildcard-import # noqa: F403
+
+
+@pytest.fixture
+def recordings_dir():
+    with tempfile.TemporaryDirectory(prefix='rosys-test-', ignore_cleanup_errors=True, delete=False) as tmp_path:
+        d = Path(tmp_path) / 'recordings'
+        d.mkdir()
+        yield d
