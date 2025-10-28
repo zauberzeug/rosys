fba9c657cdac0c3dd7b888d0dfc5b0cc1911c29f
Dominik Drees <dominik@zauberzeug.com>
2025-10-28 17:28:06 +0100

    Remove test_image_without_data
    Images now must have data (via array attribute).

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,6 +1,5 @@
 import numpy as np
 import PIL.Image
-import pytest
 
 from rosys.vision import Image, ImageSize
 
@@ -28,11 +27,3 @@ def test_image_pil_conversion():
     assert isinstance(converted_pil, PIL.Image.Image)
     assert converted_pil.size == (640, 480)
     assert np.allclose(np.array(original_pil), np.array(converted_pil), atol=1)
-
-
-def test_image_without_data():
-    img = Image(camera_id='test_cam', time=123.45, size=ImageSize(width=640, height=480))
-    with pytest.raises(ValueError, match='Cannot convert image to array because it has no data.'):
-        img.to_array()
-    with pytest.raises(ValueError, match='Cannot convert image to PIL image because it has no data.'):
-        img.to_pil()
