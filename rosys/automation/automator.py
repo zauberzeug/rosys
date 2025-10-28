3b168110d7b0e30a5bd533fbc934e85d3a3a513f
Pascal Schade <pascal@zauberzeug.com>
2025-10-28 22:11:26 +0100

    remove unused argument

--- rosys/automation/automator.py
+++ rosys/automation/automator.py
@@ -193,7 +193,7 @@ class Automator:
         """
         self.default_automation = default_automation
 
-    def _handle_exception(self, e: Exception) -> None:
+    def _handle_exception(self, _: Exception) -> None:
         self.abort(because='an exception occurred in an automation')
         if rosys.is_test:
             self.log.exception('automation failed')
