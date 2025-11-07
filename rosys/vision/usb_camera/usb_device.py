4fe5659f1a767a202354bb43c5abe6118bc7f99c
ftilde <ftilde@tamepointer.de>
2025-11-07 10:40:32 +0100

    Avoid copy in BGR->RGB conversion

--- rosys/vision/usb_camera/usb_device.py
+++ rosys/vision/usb_camera/usb_device.py
@@ -99,7 +99,7 @@ class UsbDevice:
                 result = self._on_new_image_data(bytes_, timestamp)
             else:
                 # convert bgr to rgb
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = frame[:,:,::-1]
                 result = self._on_new_image_data(frame, timestamp)
             if isinstance(result, Awaitable):
                 await result
