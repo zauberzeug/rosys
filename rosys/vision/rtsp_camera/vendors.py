036a37f2fe2dfec2fd6760a6b795f6ac69393bf5
Niklas Neugebauer <68709968+NiklasNeugebauer@users.noreply.github.com>
2025-12-17 09:41:16 +0100

    Add Anjoy and OpenIPC cameras to vendor list (#376)
    ### Motivation
    
    We want to use two new camera firmwares with rosys.
    
    ### Implementation
    
    This adds anjoy cameras with their (observed) mac range and OpenIPC
    cameras with the mac range that we set for them.
    The functions for determining vendor and mac were also refactored to use
    dicts. Otherwise, we would run into a "too many return statements"
    Pylint error now.

--- rosys/vision/rtsp_camera/vendors.py
+++ rosys/vision/rtsp_camera/vendors.py
@@ -7,29 +7,42 @@ class VendorType(Enum):
     AXIS = 3
     REOLINK = 4
     UNIARCH = 5
+    ANJOY = 6
+    OPENIPC = 7
     OTHER = -1
 
 
+mac_prefix_to_vendor: dict[str, VendorType] = {
+    'e0:62:90': VendorType.JOVISION,
+    'e4:24:6c': VendorType.DAHUA,
+    '3c:e3:6b': VendorType.DAHUA,
+    'd4:43:0e': VendorType.DAHUA,
+    'fc:5f:49': VendorType.DAHUA,
+    '30:dd:aa': VendorType.DAHUA,
+    'ec:71:db': VendorType.REOLINK,
+    '6c:f1:7e': VendorType.UNIARCH,
+    'f0:00:06': VendorType.ANJOY,
+    '7a:7a:21': VendorType.OPENIPC,
+}
+
+
+vendor_to_url: dict[VendorType, str] = {
+    VendorType.JOVISION: 'rtsp://admin:admin@{ip}/profile{substream}',
+    VendorType.DAHUA: 'rtsp://admin:Adminadmin@{ip}/cam/realmonitor?channel=1&subtype={substream}',
+    VendorType.REOLINK: 'rtsp://admin:Adminadmin@{ip}/Preview_01_{"sub" if substream else "main"}',
+    VendorType.UNIARCH: 'rtsp://admin:Admin_adm1n@{ip}/media/video{2 if substream else 1}',
+    VendorType.ANJOY: 'rtsp://admin:123456@{ip}/h264/ch{2 if substream else 1}',
+    VendorType.OPENIPC: 'rtsp://root:Adminadmin@{ip}/stream={substream}',
+}
+
+
 def mac_to_vendor(mac: str) -> VendorType:
-    if mac.startswith('e0:62:90'):
-        return VendorType.JOVISION
-    if mac.startswith(('e4:24:6c', '3c:e3:6b', 'd4:43:0e', 'fc:5f:49', '30:dd:aa')):
-        return VendorType.DAHUA
-    if mac.startswith('ec:71:db'):
-        return VendorType.REOLINK
-    if mac.startswith('6c:f1:7e'):
-        return VendorType.UNIARCH
-    return VendorType.OTHER
+    prefix = mac[:8]
+    return mac_prefix_to_vendor.get(prefix, VendorType.OTHER)
 
 
 def mac_to_url(mac: str, ip: str, substream: int = 0) -> str | None:
     vendor = mac_to_vendor(mac)
-    if vendor == VendorType.JOVISION:
-        return f'rtsp://admin:admin@{ip}/profile{substream}'
-    if vendor == VendorType.DAHUA:
-        return f'rtsp://admin:Adminadmin@{ip}/cam/realmonitor?channel=1&subtype={substream}'
-    if vendor == VendorType.REOLINK:
-        return f'rtsp://admin:Adminadmin@{ip}/Preview_01_{"sub" if substream else "main"}'
-    if vendor == VendorType.UNIARCH:
-        return f'rtsp://admin:Admin_adm1n@{ip}/media/video{2 if substream else 1}'
-    return None
+    if vendor == VendorType.OTHER:
+        return None
+    return vendor_to_url[vendor].format(ip=ip, substream=substream)
