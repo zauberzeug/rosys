1c7b47894805621f28c52348957807683c546f86
ftilde <dominik@zauberzeug.com>
2025-12-19 10:55:29 +0100

    Fix vendor_to_url mapping in rtsp camera (#378)
    ### Motivation
    
    #376 broke the vendor-to-url mapping for device types with more complex
    f-string expressions since `.format()` (apparently) cannot handle those,
    resulting in: `KeyError: '"sub" if substream else "main"'`
    
    ### Implementation
    
    Use a function with match instead of a dict.

--- rosys/vision/rtsp_camera/vendors.py
+++ rosys/vision/rtsp_camera/vendors.py
@@ -26,14 +26,24 @@ mac_prefix_to_vendor: dict[str, VendorType] = {
 }
 
 
-vendor_to_url: dict[VendorType, str] = {
-    VendorType.JOVISION: 'rtsp://admin:admin@{ip}/profile{substream}',
-    VendorType.DAHUA: 'rtsp://admin:Adminadmin@{ip}/cam/realmonitor?channel=1&subtype={substream}',
-    VendorType.REOLINK: 'rtsp://admin:Adminadmin@{ip}/Preview_01_{"sub" if substream else "main"}',
-    VendorType.UNIARCH: 'rtsp://admin:Admin_adm1n@{ip}/media/video{2 if substream else 1}',
-    VendorType.ANJOY: 'rtsp://admin:123456@{ip}/h264/ch{2 if substream else 1}',
-    VendorType.OPENIPC: 'rtsp://root:Adminadmin@{ip}/stream={substream}',
-}
+def _vendor_to_url(vendor_type: VendorType, ip: str, substream: int) -> str | None:
+    # pylint: disable=too-many-return-statements
+    match vendor_type:
+        case VendorType.JOVISION:
+            return f'rtsp://admin:admin@{ip}/profile{substream}'
+        case VendorType.DAHUA:
+            return f'rtsp://admin:Adminadmin@{ip}/cam/realmonitor?channel=1&subtype={substream}'
+        case VendorType.REOLINK:
+            return f'rtsp://admin:Adminadmin@{ip}/Preview_01_{"sub" if substream else "main"}'
+        case VendorType.UNIARCH:
+            return f'rtsp://admin:Admin_adm1n@{ip}/media/video{2 if substream else 1}'
+        case VendorType.ANJOY:
+            return f'rtsp://admin:123456@{ip}/h264/ch{2 if substream else 1}'
+        case VendorType.OPENIPC:
+            return f'rtsp://root:Adminadmin@{ip}/stream={substream}'
+        case VendorType.OTHER:
+            return None
+    raise AssertionError('unreachable')  # Just for mypy
 
 
 def mac_to_vendor(mac: str) -> VendorType:
@@ -43,6 +53,4 @@ def mac_to_vendor(mac: str) -> VendorType:
 
 def mac_to_url(mac: str, ip: str, substream: int = 0) -> str | None:
     vendor = mac_to_vendor(mac)
-    if vendor == VendorType.OTHER:
-        return None
-    return vendor_to_url[vendor].format(ip=ip, substream=substream)
+    return _vendor_to_url(vendor, ip=ip, substream=substream)
