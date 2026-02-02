cf33fe35d96bd1d9e6162f8482934af2b967e574
Niklas Neugebauer <niklas@zauberzeug.com>
2026-02-02 17:29:15 +0100

    fix: test without initial connect

--- tests/test_reconnecting_camera.py
+++ tests/test_reconnecting_camera.py
@@ -27,8 +27,15 @@ async def test_reconnecting_camera_reconnects_after_disconnect(rosys_integration
     assert camera.is_connected
 
 
+async def test_reconnect_task_running_after_initial_connect(rosys_integration):
+    camera = ReconnectingSimulatedCamera(id='test_cam', width=800, height=600, fps=1, connect_after_init=True)
+    await forward(seconds=1.0)
+    assert camera.is_activated
+    assert camera.is_connected
+
+
 async def test_reconnecting_camera_stops_on_disconnect(rosys_integration):
-    camera = ReconnectingSimulatedCamera(id='test_cam', width=800, height=600, fps=1)
+    camera = ReconnectingSimulatedCamera(id='test_cam', width=800, height=600, fps=1, connect_after_init=False)
     await camera.connect()
     assert camera.is_activated
 
