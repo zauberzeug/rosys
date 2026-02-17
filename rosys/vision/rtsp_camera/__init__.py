d18e4ceaf4bc2859a64aeaa10ef6f4a490ba513c
Niklas Neugebauer <niklas@zauberzeug.com>
2026-02-17 14:26:48 +0100

    fix: remove some leftover imports

--- rosys/vision/rtsp_camera/__init__.py
+++ rosys/vision/rtsp_camera/__init__.py
@@ -1,17 +1,7 @@
-from .hardware_acceleration import (
-    HardwareCapabilities,
-    JetsonModel,
-    detect_jetson_hardware,
-    get_hardware_info_string,
-)
 from .rtsp_camera import RtspCamera
 from .rtsp_camera_provider import RtspCameraProvider
 
 __all__ = [
-    'HardwareCapabilities',
-    'JetsonModel',
     'RtspCamera',
     'RtspCameraProvider',
-    'detect_jetson_hardware',
-    'get_hardware_info_string',
 ]
