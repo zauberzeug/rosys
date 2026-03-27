a5d673f18f53a0ee233e36ec74f1dfa84ea9430d
Pascal Schade <pascal@zauberzeug.com>
2026-03-27 12:29:46 +0100

    update PoseProvider

--- rosys/driving/pose_provider.py
+++ rosys/driving/pose_provider.py
@@ -1,10 +1,10 @@
 from typing import Protocol
 
+from nicegui import Event
+
 from ..geometry import Pose
 
 
 class PoseProvider(Protocol):
-
-    @property
-    def pose(self) -> Pose:
-        ...
+    POSE_UPDATED: Event
+    pose: Pose
