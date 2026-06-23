b6f32e45557b6654e96af52fd4dca2122975cce3
NiklasNeugebauer <niklas@neugebauer.dev>
2026-06-23 10:34:22 +0200

    refactr: precompute vendor once

--- rosys/vision/mjpeg_camera/mjpeg_device_factory.py
+++ rosys/vision/mjpeg_camera/mjpeg_device_factory.py
@@ -16,22 +16,24 @@ class MjpegDeviceFactory:
                control_port: int | None = None,
                on_new_image_data: Callable[[bytes, float], Awaitable | None]) -> MjpegDevice:
 
-        if mac_to_vendor(mac) == VendorType.AXIS:
+        vendor = mac_to_vendor(mac)
+
+        if vendor == VendorType.AXIS:
             return AxisMjpegDevice(mac, ip,
                                    index=index, username=username,
                                    password=password, on_new_image_data=on_new_image_data)
 
-        if mac_to_vendor(mac) == VendorType.MOTEC:
+        if vendor == VendorType.MOTEC:
             return MotecMjpegDevice(mac, ip,
                                     username=username, password=password,
                                     control_port=control_port, on_new_image_data=on_new_image_data)
 
-        if mac_to_vendor(mac) == VendorType.ARKVISION:
+        if vendor == VendorType.ARKVISION:
             return ArkVisionMjpegDevice(mac, ip,
                                         index=index, username=username,
                                         password=password, on_new_image_data=on_new_image_data)
 
-        if mac_to_vendor(mac) in {VendorType.GOODCAM, VendorType.OPENIPC}:
+        if vendor in {VendorType.GOODCAM, VendorType.OPENIPC}:
             return MjpegDevice(mac, ip,
                                username=username, password=password,
                                on_new_image_data=on_new_image_data)
