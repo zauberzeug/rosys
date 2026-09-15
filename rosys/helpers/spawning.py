48725d41d66e34682ca4c399c686fc63a51ad3d6
Niklas Neugebauer <niklas@zauberzeug.com>
2026-09-15 16:29:01 +0200

    text: improve comments

--- rosys/helpers/spawning.py
+++ rosys/helpers/spawning.py
@@ -1,4 +1,4 @@
 import multiprocessing
 
-# spawn, not fork, regardless of the global start method
+# fork would copy the parent's threads and event loop into the child; spawn starts it clean
 SPAWN_CONTEXT = multiprocessing.get_context('spawn')
